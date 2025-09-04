90e19358176fb910ee58e6cad273a73720263bf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:24 +0000

    Add package my-sample-pkg/0.1.2/my_sample_pkg-0.1.2-py3-none-any.whl

---
+++ packages/my-sample-pkg/my_sample_pkg-0.1.2-py3-none-any.whl/my_sample_pkg/__init__.py
@@ -0,0 +1,9 @@
+"""A simple sample Python package for PyPI."""
+
+__version__ = "0.1.0"
+__author__ = "kimseungtae"
+__email__ = "monogatree@gmail.com"
+
+from .main import hello
+
+__all__ = ["hello"]
