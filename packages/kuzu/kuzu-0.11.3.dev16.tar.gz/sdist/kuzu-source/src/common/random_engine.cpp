ddabb90bf4f68ed327ef8ff87b7f83d9ac9b33ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 16:05:27 +0000

    Add package kuzu/0.11.3.dev16/kuzu-0.11.3.dev16.tar.gz

---
+++ packages/kuzu/kuzu-0.11.3.dev16.tar.gz/sdist/kuzu-source/src/common/random_engine.cpp
@@ -0,0 +1,32 @@
+#include "common/random_engine.h"
+
+#include <random>
+
+namespace kuzu {
+namespace common {
+
+RandomEngine::RandomEngine() : randomState(RandomState()) {
+    randomState.pcg.seed(pcg_extras::seed_seq_from<std::random_device>());
+}
+
+RandomEngine::RandomEngine(uint64_t seed, uint64_t stream) : randomState(RandomState()) {
+    randomState.pcg.seed(seed, stream);
+}
+
+void RandomEngine::setSeed(uint64_t seed) {
+    std::unique_lock xLck{mtx};
+    randomState.pcg.seed(seed);
+}
+
+uint32_t RandomEngine::nextRandomInteger() {
+    std::unique_lock xLck{mtx};
+    return randomState.pcg();
+}
+
+uint32_t RandomEngine::nextRandomInteger(uint32_t upper) {
+    std::unique_lock xLck{mtx};
+    return randomState.pcg(upper);
+}
+
+} // namespace common
+} // namespace kuzu
