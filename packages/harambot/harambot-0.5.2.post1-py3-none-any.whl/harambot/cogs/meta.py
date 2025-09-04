4525106522bff7ee3593e15991047404d71d4c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 07:04:35 +0000

    Add package harambot/0.5.2.post1/harambot-0.5.2.post1-py3-none-any.whl

---
+++ packages/harambot/harambot-0.5.2.post1-py3-none-any.whl/harambot/cogs/meta.py
@@ -0,0 +1,159 @@
+from discord.ext import commands
+from discord import app_commands
+
+import discord
+import logging
+
+from harambot.ui.views import ConfigView, LeagueConfigView, LeagueSelect, ReportConfigView
+from harambot.database.models import Guild
+
+
+logger = logging.getLogger("discord.harambot.cogs.meta")
+logger.setLevel(logging.INFO)
+
+
+class Meta(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="help", description="View available commands")
+    async def help(self, interaction: discord.Interaction):
+        embed = discord.Embed(
+            title="Harambot",
+            description="Yahoo Fantasy Sports Bot for Discord",
+            color=0xEEE657,
+        )
+        embed.add_field(
+            name="/ping", value="Gives the latency of harambot", inline=False
+        )
+        embed.add_field(
+            name="/rip", value="Pay respects to Harambe", inline=False
+        )
+        embed.add_field(
+            name="/standings",
+            value="Returns the current standings of your league",
+            inline=False,
+        )
+        embed.add_field(
+            name="/roster team_name",
+            value="Returns the roster of the given team",
+            inline=False,
+        )
+        embed.add_field(
+            name="/stats player_name",
+            value="Returns the details of the given player",
+            inline=False,
+        )
+        embed.add_field(
+            name="/trade",
+            value="Create poll for latest trade for league approval",
+            inline=False,
+        )
+        embed.add_field(
+            name="/matchups",
+            value="Returns the current weeks matchups",
+            inline=False,
+        )
+        embed.add_field(
+            name="/waivers days",
+            value="Returns the waiver wire transactions for the previous number of days",
+            inline=False,
+        )
+        embed.add_field(
+            name="/configure",
+            value="Configure your guild for Harambot",
+            inline=False,
+        )
+        embed.add_field(
+            name="/reports",
+            value="Configure automatic transaction and matchup reporting",
+            inline=False,
+        )
+        embed.add_field(
+            name="/league",
+            value="Set which league harambot should use for commands",
+            inline=False,
+        )
+        await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(
+        name="ping", description="Gives the latency of harambot"
+    )
+    async def ping(self, interaction: discord.Interaction):
+        await interaction.response.send_message(self.bot.latency)
+
+    @app_commands.command(
+        name="configure",
+        description="Configure your guild for Harambot",
+    )
+    @app_commands.checks.has_permissions(administrator=True)
+    async def configure(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            """
+            Lets setup your guild
+            1. Login into Yahoo and copy you authentication token
+2. Configure harambot with your league information
+            """,
+            view=ConfigView(),
+            ephemeral=True,
+        )
+
+    @configure.error
+    async def configure_check_error(
+        self, interaction: discord.Interaction, error
+    ):
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(
+                "You do not have the required permissions to run this command."
+            )
+
+    def webhook_permissions(interaction: discord.Interaction):
+        return interaction.guild.me.guild_permissions.manage_webhooks
+
+    def guild_is_configured(interaction: discord.Interaction):
+        return (
+            Guild.select()
+            .where(Guild.guild_id == str(interaction.guild_id))
+            .exists()
+        )
+
+    @app_commands.command(
+        name="reports",
+        description="Configure automatic transaction and matchup reporting",
+    )
+    @app_commands.check(webhook_permissions)
+    @app_commands.check(guild_is_configured)
+    async def reports(
+        self,
+        interaction: discord.Interaction,
+    ):
+        message = "Set what channel transaction reports should be sent to."
+        await interaction.response.send_message(
+            message, view=ReportConfigView(), ephemeral=True
+        )
+
+    @reports.error
+    async def reports_check_error(
+        self, interaction: discord.Interaction, error
+    ):
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(
+                "Grant Harambot the Manage Webhooks permission to use this command"
+            )
+        elif isinstance(error, app_commands.CheckFailure):
+            await interaction.response.send_message(
+                "This guild is not configured for Harambot. Please run `/configure` first."
+            )
+
+    @app_commands.command(
+            name="league",
+            description="Set which league harambot should use for commands"
+            )
+    @app_commands.check(guild_is_configured)
+    async def league(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+        message = "Select which league you would like to use for commands"
+        view = LeagueConfigView()
+        view.add_item(LeagueSelect(interaction.guild_id))
+        await interaction.followup.send(message, view=view, ephemeral=True)
+
---
+++ packages/harambot/harambot-0.5.2.post1-py3-none-any.whl/harambot/utils.py
@@ -0,0 +1,158 @@
+import base64
+import requests
+import time
+import logging
+
+from cachetools import keys
+
+from harambot.config import settings
+from harambot import yahoo_api
+from discord import Embed
+
+YAHOO_API_URL = "https://api.login.yahoo.com/oauth2/"
+YAHOO_AUTH_URI = "request_auth?redirect_uri=oob&response_type=code&client_id="
+
+logger = logging.getLogger("discord.harambot.utils")
+
+
+def yahoo_auth(code):
+    encoded_creds = base64.b64encode(
+        ("{0}:{1}".format(settings.yahoo_key, settings.yahoo_secret)).encode(
+            "utf-8"
+        )
+    )
+    response = requests.post(
+        url="{}get_token".format(YAHOO_API_URL),
+        data={
+            "code": code,
+            "redirect_uri": "oob",
+            "grant_type": "authorization_code",
+        },
+        headers={
+            "User-Agent": "HaramBot",
+            "Authorization": "Basic {0}".format(encoded_creds.decode("utf-8")),
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    )
+    if response.status_code != 200:
+        logger.error(
+            "Failed to authenticate with Yahoo API: {}".format(response.json)
+        )
+        return {}
+    details = response.json()
+
+    details["token_time"] = time.time()
+    return details
+
+
+def create_add_embed(transaction):
+    embed = Embed(title="Player Added")
+    add_player_fields_to_embed(embed, transaction["players"]["0"]["player"][0])
+    embed.add_field(
+        name="Owner",
+        value=transaction["players"]["0"]["player"][1]["transaction_data"][0][
+            "destination_team_name"
+        ],
+    )
+    if "faab_bid" in transaction:
+        embed.add_field(
+            name="Bid", value=transaction["faab_bid"], inline=False
+        )
+    return embed
+
+
+def create_drop_embed(transaction):
+    embed = Embed(title="Player Dropped")
+    add_player_fields_to_embed(embed, transaction["players"]["0"]["player"][0])
+    embed.add_field(
+        name="Owner",
+        value=transaction["players"]["0"]["player"][1]["transaction_data"][
+            "source_team_name"
+        ],
+    )
+    return embed
+
+
+def create_add_drop_embed(transaction):
+    embed = Embed(title="Player Added / Player Dropped")
+    embed.add_field(
+        name="Owner",
+        value=transaction["players"]["0"]["player"][1]["transaction_data"][0][
+            "destination_team_name"
+        ],
+    )
+    if "faab_bid" in transaction:
+        embed.add_field(
+            name="Bid", value=transaction["faab_bid"], inline=False
+        )
+    embed.add_field(
+        name="Player Added", value="=====================", inline=False
+    )
+    add_player_fields_to_embed(embed, transaction["players"]["0"]["player"][0])
+    embed.add_field(
+        name="Player Dropped", value="=====================", inline=False
+    )
+    add_player_fields_to_embed(embed, transaction["players"]["1"]["player"][0])
+    return embed
+
+
+def add_player_fields_to_embed(embed, player):
+    embed.add_field(
+        name="Player", value=player[2]["name"]["full"], inline=True
+    )
+    embed.add_field(
+        name="Team", value=player[3]["editorial_team_abbr"], inline=True
+    )
+    embed.add_field(
+        name="Position", value=player[4]["display_position"], inline=True
+    )
+
+
+def get_avatar_bytes():
+    # return the image from the settings.webhook_avatar_url variable as bytes
+    return requests.get(settings.webhook_avatar_url).content
+
+
+def get_cache_key(*args, **kwargs):
+    function_name = args[0]
+    guild_id = str(kwargs.get("guild_id"))
+    return keys.hashkey(function_name, guild_id)
+
+def clear_guild_cache(guild_id):
+    guild_cache_keys = [
+        get_cache_key("get_settings", guild_id=guild_id),
+        get_cache_key("get_teams", guild_id=guild_id),
+        get_cache_key("get_players", guild_id=guild_id),
+        get_cache_key("get_standings", guild_id=guild_id),
+        get_cache_key("get_roster", guild_id=guild_id),
+        get_cache_key("get_matchups", guild_id=guild_id),
+        get_cache_key("get_latest_trade", guild_id=guild_id),
+        get_cache_key("get_transactions", guild_id=guild_id)
+    ]
+    for key in guild_cache_keys:
+        yahoo_api.cache.pop(key, None)
+
+def get_player_embed(player):
+    embed = Embed(
+        title=player["name"]["full"],
+        description="#" + player["uniform_number"],
+        color=0xEEE657,
+    )
+    embed.add_field(name="Postion", value=player["primary_position"])
+    embed.add_field(name="Team", value=player["editorial_team_abbr"])
+    if "bye_weeks" in player:
+        embed.add_field(name="Bye", value=player["bye_weeks"]["week"])
+    embed.add_field(name="Owner", value=player["owner"])
+    embed.set_thumbnail(url=player["image_url"])
+    if "total_points" in player["stats"]:
+        embed.add_field(
+            name="Total Points",
+            value=player["stats"]["total_points"],
+            inline=False,
+        )
+    if len(player["stats"].items()) < 20:
+        for key, value in player["stats"].items():
+            if key == "total_points":
+                continue
+            embed.add_field(name=key, value=value)
+    return embed
