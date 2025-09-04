25d8e854d2db3f84b2ca67fe6fbd4c535389db4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 16:05:08 +0000

    Add package pytest-mergify/2025.9.4.1/pytest_mergify-2025.9.4.1.tar.gz

---
+++ packages/pytest-mergify/pytest_mergify-2025.9.4.1.tar.gz/pytest_mergify-2025.9.4.1/pytest_mergify/resources/mergify.py
@@ -0,0 +1,15 @@
+from opentelemetry.sdk.resources import Resource, ResourceDetector
+
+from pytest_mergify import utils
+
+
+class MergifyResourceDetector(ResourceDetector):
+    """Detects OpenTelemetry Resource attributes for Mergify fields."""
+
+    OPENTELEMETRY_MERGIFY_MAPPING = {
+        "mergify.test.job.name": (str, "MERGIFY_TEST_JOB_NAME"),
+    }
+
+    def detect(self) -> Resource:
+        attributes = utils.get_attributes(self.OPENTELEMETRY_MERGIFY_MAPPING)
+        return Resource(attributes)
