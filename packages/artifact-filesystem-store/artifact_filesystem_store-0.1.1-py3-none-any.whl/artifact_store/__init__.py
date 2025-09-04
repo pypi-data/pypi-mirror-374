7d7ae45ec4404c5c0183127fd3cc8d03809f5074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:41 +0000

    Add package artifact-filesystem-store/0.1.1/artifact_filesystem_store-0.1.1-py3-none-any.whl

---
+++ packages/artifact-filesystem-store/artifact_filesystem_store-0.1.1-py3-none-any.whl/artifact_store/__init__.py
@@ -0,0 +1,18 @@
+import json
+
+__version__ = "0.1.1"
+
+
+class ArtifactMetaData:
+    """Class to handle metadata for artifacts."""
+
+    def __init__(self, meta=None):
+        self.meta = meta if meta else {}
+
+    def add(self, key, value):
+        """Add a key-value pair to the metadata."""
+        self.meta[key] = value
+
+    def __str__(self):
+        """Return a string representation of the metadata."""
+        return json.dumps(self.meta, indent=2, sort_keys=True)
