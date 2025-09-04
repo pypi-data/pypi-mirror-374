7ff73d976952ecbaeb9b885926db49ebb3ad8c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:34 +0000

    Add package scmpompg/0.1.0/scmpompg-0.1.0.tar.gz

---
+++ packages/scmpompg/scmpompg-0.1.0.tar.gz/scmpompg-0.1.0/scmpompg/pommodule1.py
@@ -0,0 +1,7 @@
+SCM1_PI = 3.14
+
+def f1(n):
+    return n**2
+
+def f2(n):
+    return n**3
---
+++ packages/scmpompg/scmpompg-0.1.0.tar.gz/scmpompg-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scmpompg",      # Must be unique on PyPI!
+    version="0.1.0",      # Follow semantic versioning
+    description="This is my first package. This package is related to number theory.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="A. Karnbanjong",
+    author_email="kpadisak@gmail.com",
+    url="https://github.com/kadisak/scmpompg",  # optional
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[],
+)
