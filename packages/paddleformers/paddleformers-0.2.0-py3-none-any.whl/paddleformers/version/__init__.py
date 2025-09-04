55bffbb14230a480ebb0b30e26b49cae26ebff7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:04:23 +0000

    Add package paddleformers/0.2.0/paddleformers-0.2.0-py3-none-any.whl

---
+++ packages/paddleformers/paddleformers-0.2.0-py3-none-any.whl/paddleformers/version/__init__.py
@@ -0,0 +1,25 @@
+# THIS FILE IS GENERATED FROM PADDLEFORMERS SETUP.PY
+commit           = 'c905bd9cabcc963e1ecdb74c082ad6cf88f04b94'
+
+__all__ = ['show']
+
+def show():
+    """Get the corresponding commit id of paddleformers.
+
+    Returns:
+        The commit-id of paddleformers will be output.
+
+        full_version: version of paddleformers
+
+
+    Examples:
+        .. code-block:: python
+
+            import paddleformers
+
+            paddleformers.version.show()
+            # commit: 1ef5b94a18773bb0b1bba1651526e5f5fc5b16fa
+
+    """
+    print("commit:", commit)
+
