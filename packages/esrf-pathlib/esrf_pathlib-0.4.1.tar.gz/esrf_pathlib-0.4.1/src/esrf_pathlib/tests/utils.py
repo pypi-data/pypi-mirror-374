855aff48ac3ada1b369c8d84cd5a08647e49022d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:08:28 +0000

    Add package esrf-pathlib/0.4.1/esrf_pathlib-0.4.1.tar.gz

---
+++ packages/esrf-pathlib/esrf_pathlib-0.4.1.tar.gz/esrf_pathlib-0.4.1/src/esrf_pathlib/tests/utils.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+import sys
+
+IS_WINDOWS = sys.platform == "win32"
+_BASE_PATH = "X:\\" if IS_WINDOWS else "/data"
+
+
+def make_path(*parts) -> str:
+    return os.path.join(_BASE_PATH, *parts)
+
+
+def safe_repr(path: pathlib.Path) -> str:
+    if IS_WINDOWS:
+        return repr(path).replace("/", "\\")
+    else:
+        return repr(path)
