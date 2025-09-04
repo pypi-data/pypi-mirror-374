fc0fd0edddbafd466fdd6bd67a846eca1d170e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:52 +0000

    Add package pysdrlib/0.0.1/pysdrlib-0.0.1-py3-none-any.whl

---
+++ packages/pysdrlib/pysdrlib-0.0.1-py3-none-any.whl/pysdrlib/__init__.py
@@ -0,0 +1,12 @@
+"""
+pysdrlib
+
+wrapper for hardware SDRs
+
+Github: https://github.com/Anonoei/pysdrlib
+
+PyPI: https://pypi.org/project/pysdrlib/
+"""
+
+__version__ = "0.0.1"
+__author__ = "Anonoei <to+dev@an0.cx>"
