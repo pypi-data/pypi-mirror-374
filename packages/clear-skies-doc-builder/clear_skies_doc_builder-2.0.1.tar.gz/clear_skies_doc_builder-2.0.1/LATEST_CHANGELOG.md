e2ef05eafa91629bfea849043e34e1b3b666b406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:12:34 +0000

    Add package clear-skies-doc-builder/2.0.1/clear_skies_doc_builder-2.0.1.tar.gz

---
+++ packages/clear-skies-doc-builder/clear_skies_doc_builder-2.0.1.tar.gz/clear_skies_doc_builder-2.0.1/LATEST_CHANGELOG.md
@@ -0,0 +1,16 @@
+## [2.0.1] - 2025-09-03
+
+### Added
+- Add include_path for custom includes
+- Add copier template
+
+### Changed
+- Merge pull request #1 from clearskies-py/copier by @tnijboer in [#1](https://github.com/clearskies-py/doc-builder/pull/1)
+- Update for tooling by @cmancone
+- WIP by @cmancone
+- Initial commit by @cmancone
+
+## New Contributors
+* @ made their first contribution
+* @tnijboer made their first contribution in [#1](https://github.com/clearskies-py/doc-builder/pull/1)
+* @cmancone made their first contribution
---
+++ packages/clear-skies-doc-builder/clear_skies_doc_builder-2.0.1.tar.gz/clear_skies_doc_builder-2.0.1/src/clearskies_doc_builder/prepare_doc_space.py
@@ -0,0 +1,25 @@
+import pathlib
+import shutil
+
+
+def prepare_doc_space(project_root):
+    project_path = pathlib.Path(project_root)
+    build_path = project_path / "build"
+    doc_path = build_path / "docs"
+    jekyll_path = project_path / "jekyll"
+    includes_path = jekyll_path / "_includes"
+
+    if doc_path.is_dir():
+        shutil.rmtree(doc_path)
+    build_path.mkdir(parents=True, exist_ok=True)
+
+    for file in jekyll_path.glob("*"):
+        if not file.is_file():
+            continue
+        shutil.copy2(str(file), str(build_path / file.name))
+
+    shutil.copytree(str(jekyll_path / "docs"), str(build_path / "docs"))
+    if includes_path.is_dir():
+        shutil.copytree(str(includes_path), str(build_path / "_includes"))
+
+    return str(doc_path)
