97a1e3afc5537e22b47273b97809bf769e290238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:08:11 +0000

    Add package crewmaster/0.1.15b0/crewmaster-0.1.15b0-py3-none-any.whl

---
+++ packages/crewmaster/crewmaster-0.1.15b0-py3-none-any.whl/crewmaster/features/http_driver/__init__.py
@@ -0,0 +1,6 @@
+from .crew_router_base import (
+    CrewRouterBase
+)
+__all__ = [
+    "CrewRouterBase"
+]
---
+++ packages/crewmaster/crewmaster-0.1.15b0-py3-none-any.whl/crewmaster/features/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages # type: ignore
+
+setup(
+    name="crewmaster$",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Manejador de agentes",
+    author=[
+        {'name': "Imolko", "email": "info@imolko.com"},
+        {'name': "Carlos N", "email": "machinery.e@gmail.com"}
+    ],
+    contributors=[
+        {'name': "Carlos N", "email": "machinery.e@gmail.com"}
+    ]
+)
