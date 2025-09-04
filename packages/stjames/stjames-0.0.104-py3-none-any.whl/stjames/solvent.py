320ad48cb7f6b7ad2b317763831b93acaf5ea3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 07:04:40 +0000

    Add package stjames/0.0.104/stjames-0.0.104-py3-none-any.whl

---
+++ packages/stjames/stjames-0.0.104-py3-none-any.whl/stjames/solvent.py
@@ -0,0 +1,44 @@
+from .base import Base, LowercaseStrEnum
+
+
+class Solvent(LowercaseStrEnum):
+    WATER = "water"
+    NITROMETHANE = "nitromethane"
+    NITROBENZENE = "nitrobenzene"
+    TOLUENE = "toluene"
+    BENZENE = "benzene"
+    CHLOROBENZENE = "chlorobenzene"
+    CARBONTETRACHLORIDE = "carbontetrachloride"
+    DICHLOROETHANE = "dichloroethane"
+    DICHLOROMETHANE = "dichloromethane"
+    CHLOROFORM = "chloroform"
+    DIETHYLETHER = "diethylether"
+    DIISOPROPYLETHER = "diisopropylether"
+    DIMETHYLSULFOXIDE = "dimethylsulfoxide"
+    TETRAHYDROFURAN = "tetrahydrofuran"
+    CYCLOHEXANE = "cyclohexane"
+    OCTANE = "octane"
+    ACETICACID = "aceticacid"
+    HEXANE = "hexane"
+    ETHYLACETATE = "ethylacetate"
+    ACETONE = "acetone"
+    ACETONITRILE = "acetonitrile"
+    METHANOL = "methanol"
+    ETHANOL = "ethanol"
+    ISOPROPANOL = "isopropanol"
+    DIMETHYLACETAMIDE = "dimethylacetamide"
+    DIMETHYLFORMAMIDE = "dimethylformamide"
+
+
+class SolventModel(LowercaseStrEnum):
+    PCM = "pcm"
+    CPCM = "cpcm"
+    ALPB = "alpb"
+    COSMO = "cosmo"
+    GBSA = "gbsa"
+    CPCMX = "cpcmx"
+
+
+class SolventSettings(Base):
+    solvent: Solvent
+    model: SolventModel
