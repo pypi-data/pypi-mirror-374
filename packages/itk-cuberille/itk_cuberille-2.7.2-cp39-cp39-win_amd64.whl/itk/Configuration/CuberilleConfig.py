405498c247c4f9a044ca3d11d5f848bc5b983300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 07:04:35 +0000

    Add package itk-cuberille/2.7.2/itk_cuberille-2.7.2-cp39-cp39-win_amd64.whl

---
+++ packages/itk-cuberille/itk_cuberille-2.7.2-cp39-cp39-win_amd64.whl/itk/Configuration/CuberilleConfig.py
@@ -0,0 +1,18 @@
+depends = ('ITKPyBase', 'ITKMesh', 'ITKImageGradient', 'ITKImageFunction', 'ITKConnectedComponents', 'ITKCommon', )
+templates = (  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterISS2MSS2', True, 'itk::Image< signed short,2 >, itk::Mesh< signed short,2 >, itk::InterpolateImageFunction< itk::Image< signed short,2 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUC2MUC2', True, 'itk::Image< unsigned char,2 >, itk::Mesh< unsigned char,2 >, itk::InterpolateImageFunction< itk::Image< unsigned char,2 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUS2MUS2', True, 'itk::Image< unsigned short,2 >, itk::Mesh< unsigned short,2 >, itk::InterpolateImageFunction< itk::Image< unsigned short,2 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIF2MF2', True, 'itk::Image< float,2 >, itk::Mesh< float,2 >, itk::InterpolateImageFunction< itk::Image< float,2 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterID2MD2', True, 'itk::Image< double,2 >, itk::Mesh< double,2 >, itk::InterpolateImageFunction< itk::Image< double,2 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterISS3MSS3', True, 'itk::Image< signed short,3 >, itk::Mesh< signed short,3 >, itk::InterpolateImageFunction< itk::Image< signed short,3 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUC3MUC3', True, 'itk::Image< unsigned char,3 >, itk::Mesh< unsigned char,3 >, itk::InterpolateImageFunction< itk::Image< unsigned char,3 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUS3MUS3', True, 'itk::Image< unsigned short,3 >, itk::Mesh< unsigned short,3 >, itk::InterpolateImageFunction< itk::Image< unsigned short,3 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIF3MF3', True, 'itk::Image< float,3 >, itk::Mesh< float,3 >, itk::InterpolateImageFunction< itk::Image< float,3 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterID3MD3', True, 'itk::Image< double,3 >, itk::Mesh< double,3 >, itk::InterpolateImageFunction< itk::Image< double,3 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterISS4MSS4', True, 'itk::Image< signed short,4 >, itk::Mesh< signed short,4 >, itk::InterpolateImageFunction< itk::Image< signed short,4 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUC4MUC4', True, 'itk::Image< unsigned char,4 >, itk::Mesh< unsigned char,4 >, itk::InterpolateImageFunction< itk::Image< unsigned char,4 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIUS4MUS4', True, 'itk::Image< unsigned short,4 >, itk::Mesh< unsigned short,4 >, itk::InterpolateImageFunction< itk::Image< unsigned short,4 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIF4MF4', True, 'itk::Image< float,4 >, itk::Mesh< float,4 >, itk::InterpolateImageFunction< itk::Image< float,4 >, double >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterID4MD4', True, 'itk::Image< double,4 >, itk::Mesh< double,4 >, itk::InterpolateImageFunction< itk::Image< double,4 >, double >'),
+)
+factories = ()
