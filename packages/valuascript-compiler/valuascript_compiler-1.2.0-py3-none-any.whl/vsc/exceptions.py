68153db6cd7ab17debec5d445051c96e0009f267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:41 +0000

    Add package valuascript-compiler/1.2.0/valuascript_compiler-1.2.0-py3-none-any.whl

---
+++ packages/valuascript-compiler/valuascript_compiler-1.2.0-py3-none-any.whl/vsc/exceptions.py
@@ -0,0 +1,9 @@
+"""
+Custom exception types for the ValuaScript compiler.
+"""
+
+
+class ValuaScriptError(Exception):
+    """Custom exception for semantic or validation errors in a ValuaScript file."""
+
+    pass
