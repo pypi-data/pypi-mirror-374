015f9d619aee1a2d87342ab0ba8541e7ce4a7e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:41 +0000

    Add package tentacletk/0.9.40/tentacletk-0.9.40.tar.gz

---
+++ packages/tentacletk/tentacletk-0.9.40.tar.gz/tentacletk-0.9.40/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+from tentacle import __package__, __version__
+import pythontk as ptk
+
+long_description = ptk.get_file_contents("docs/README.md")
+description = ptk.get_text_between_delimiters(
+    long_description,
+    "<!-- short_description_start -->",
+    "<!-- short_description_end -->",
+    as_string=True,
+)
+
+setuptools.setup(
+    name="tentacletk",
+    version=__version__,
+    author="Ryan Simpson",
+    author_email="m3trik@outlook.com",
+    license="LGPLv3",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/m3trik/{__package__}",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=ptk.update_requirements(
+        exc=["Pillow", "qtpy", "numpy", "shiboken6", "pymel"]
+    ),
+    data_files=ptk.get_dir_contents(
+        __package__,
+        "filepath",
+        exc_files=["*.py", "*.pyc", "*.json", "*.bak"],
+        recursive=True,
+    ),
+)
+
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
