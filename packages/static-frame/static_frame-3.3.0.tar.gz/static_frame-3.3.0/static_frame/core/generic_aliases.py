a1478ecae32ea66aa61992e62cd6cf397d5432fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:03:59 +0000

    Add package static-frame/3.3.0/static_frame-3.3.0.tar.gz

---
+++ packages/static-frame/static_frame-3.3.0.tar.gz/static_frame-3.3.0/static_frame/core/generic_aliases.py
@@ -0,0 +1,25 @@
+import numpy as np
+import typing_extensions as tp
+
+from static_frame.core.bus import Bus
+from static_frame.core.frame import Frame, FrameGO, FrameHE
+from static_frame.core.index import Index
+from static_frame.core.index_hierarchy import IndexHierarchy
+from static_frame.core.series import Series, SeriesHE
+
+TIndexAny = Index[tp.Any]
+TIndexIntDefault = Index[np.int64]
+TIndexHierarchyAny = IndexHierarchy[tp.Unpack[tp.Tuple[tp.Any, ...]]]
+
+TSeriesAny = Series[tp.Any, tp.Any]
+TSeriesObject = Series[tp.Any, np.object_]
+TSeriesHEAny = SeriesHE[tp.Any, tp.Any]
+
+
+TFrameAny = Frame[tp.Any, tp.Any, tp.Unpack[tp.Tuple[tp.Any, ...]]]  # pragma: no cover
+TFrameGOAny = FrameGO[tp.Any, tp.Any]  # pragma: no cover
+TFrameHEAny = FrameHE[
+    tp.Any, tp.Any, tp.Unpack[tp.Tuple[tp.Any, ...]]
+]  # pragma: no cover
+
+TBusAny = Bus[tp.Any]  # pragma: no cover
