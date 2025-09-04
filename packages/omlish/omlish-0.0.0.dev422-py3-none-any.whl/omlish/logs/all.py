c5207e92e9e3b44b9f5508fddb2f98e0223d9666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:05:25 +0000

    Add package omlish/0.0.0.dev422/omlish-0.0.0.dev422-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev422-py3-none-any.whl/omlish/logs/all.py
@@ -0,0 +1,68 @@
+# rufF: noqa: I001
+from .. import lang as _lang
+
+
+with _lang.auto_proxy_init(globals()):
+    ##
+
+    from .callers import (  # noqa
+        LoggingCaller,
+    )
+
+    from .color import (  # noqa
+        ColorLogFormatter,
+    )
+
+    from .filters import (  # noqa
+        TidLogFilter,
+    )
+
+    from .handlers import (  # noqa
+        ListHandler,
+    )
+
+    from .json import (  # noqa
+        JsonLogFormatter,
+    )
+
+    from .levels import (  # noqa
+        LogLevel,
+    )
+
+    from .noisy import (  # noqa
+        silence_noisy_loggers,
+    )
+
+    from .protocol import (  # noqa
+        AnyLogging,
+        Logging,
+        AsyncLogging,
+
+        AnyAbstractLogging,
+        AbstractLogging,
+        AbstractAsyncLogging,
+
+        AnyNopLogging,
+        NopLogging,
+        NopAsyncLogging,
+
+        StdlibLogging,
+    )
+
+    from .proxy import (  # noqa
+        ProxyLogFilterer,
+        ProxyLogHandler,
+    )
+
+    from .standard import (  # noqa
+        STANDARD_LOG_FORMAT_PARTS,
+        StandardLogFormatter,
+
+        StandardConfiguredLogHandler,
+
+        configure_standard_logging,
+    )
+
+    from .utils import (  # noqa
+        error_logging,
+    )
---
+++ packages/omlish/omlish-0.0.0.dev422-py3-none-any.whl/omlish/logs/utils.py
@@ -0,0 +1,23 @@
+import functools
+import logging
+
+
+##
+
+
+_log = logging.getLogger(__name__)
+
+
+def error_logging(log=_log):  # noqa
+    def outer(fn):
+        @functools.wraps(fn)
+        def inner(*args, **kwargs):
+            try:
+                return fn(*args, **kwargs)
+            except Exception:
+                log.exception('Error in %r', fn)
+                raise
+
+        return inner
+
+    return outer
