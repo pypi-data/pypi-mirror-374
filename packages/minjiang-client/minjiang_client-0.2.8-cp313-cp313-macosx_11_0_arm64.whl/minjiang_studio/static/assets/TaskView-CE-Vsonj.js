d7676ce0a525b52c0f84b9e7d6db1020452d1ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:28 +0000

    Add package minjiang-client/0.2.8/minjiang_client-0.2.8-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/minjiang-client/minjiang_client-0.2.8-cp313-cp313-macosx_11_0_arm64.whl/minjiang_studio/static/assets/TaskView-CE-Vsonj.js
@@ -0,0 +1,1 @@
+import{E as o,D as r,c as t,e as c,h as n}from"./index-Bl6WO0OO.js";const s={};function a(_,i){const e=r("router-view");return c(),t("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
