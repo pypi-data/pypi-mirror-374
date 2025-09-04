71207a7708e2a74dcca703e6e36b4a351292f65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:36 +0000

    Add package lockss-pyclient/0.1.0.dev2/lockss_pyclient-0.1.0.dev2-py3-none-any.whl

---
+++ packages/lockss-pyclient/lockss_pyclient-0.1.0.dev2-py3-none-any.whl/lockss/pyclient/md/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lockss.pyclient.md.api.mdupdates_api import MdupdatesApi
+from lockss.pyclient.md.api.metadata_api import MetadataApi
+from lockss.pyclient.md.api.status_api import StatusApi
+from lockss.pyclient.md.api.urls_api import UrlsApi
---
+++ packages/lockss-pyclient/lockss_pyclient-0.1.0.dev2-py3-none-any.whl/lockss/pyclient/rs/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lockss.pyclient.rs.api.artifacts_api import ArtifactsApi
+from lockss.pyclient.rs.api.aus_api import AusApi
+from lockss.pyclient.rs.api.repo_api import RepoApi
+from lockss.pyclient.rs.api.status_api import StatusApi
+from lockss.pyclient.rs.api.wayback_api import WaybackApi
