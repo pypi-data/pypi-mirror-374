78917acc6a3689855c3fb8c8ead81a952e6f5060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:36 +0000

    Add package lockss-pyclient/0.1.0.dev2/lockss_pyclient-0.1.0.dev2.tar.gz

---
+++ packages/lockss-pyclient/lockss_pyclient-0.1.0.dev2.tar.gz/lockss_pyclient-0.1.0.dev2/src/lockss/pyclient/config/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lockss.pyclient.config.api.aus_api import AusApi
+from lockss.pyclient.config.api.config_api import ConfigApi
+from lockss.pyclient.config.api.plugins_api import PluginsApi
+from lockss.pyclient.config.api.status_api import StatusApi
+from lockss.pyclient.config.api.tdb_api import TdbApi
+from lockss.pyclient.config.api.users_api import UsersApi
+from lockss.pyclient.config.api.utils_api import UtilsApi
---
+++ packages/lockss-pyclient/lockss_pyclient-0.1.0.dev2.tar.gz/lockss_pyclient-0.1.0.dev2/src/lockss/pyclient/crawler/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lockss.pyclient.crawler.api.crawlers_api import CrawlersApi
+from lockss.pyclient.crawler.api.crawls_api import CrawlsApi
+from lockss.pyclient.crawler.api.jobs_api import JobsApi
+from lockss.pyclient.crawler.api.status_api import StatusApi
+from lockss.pyclient.crawler.api.ws_api import WsApi
