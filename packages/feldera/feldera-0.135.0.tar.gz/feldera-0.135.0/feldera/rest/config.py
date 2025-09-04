e1b5f7f2ca2653eddea7efd56e38a1f68bc20476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:06 +0000

    Add package feldera/0.135.0/feldera-0.135.0.tar.gz

---
+++ packages/feldera/feldera-0.135.0.tar.gz/feldera-0.135.0/feldera/rest/config.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+
+class Config:
+    """
+    :class:`.FelderaClient`'s credentials and configuration parameters
+    """
+
+    def __init__(
+        self,
+        url: str,
+        api_key: Optional[str] = None,
+        version: Optional[str] = None,
+        timeout: Optional[float] = None,
+        connection_timeout: Optional[float] = None,
+        requests_verify: bool = True,
+    ) -> None:
+        """
+        :param url: The url to the Feldera API (ex: https://try.feldera.com)
+        :param api_key: The optional API key to access Feldera
+        :param version: The version of the API to use
+        :param timeout: The timeout for the HTTP requests
+        :param connection_timeout: The connection timeout for the HTTP requests
+        :param requests_verify: The `verify` parameter passed to the requests
+            library. `True` by default.
+        """
+
+        self.url: str = url
+        self.api_key: Optional[str] = api_key
+        self.version: Optional[str] = version or "v0"
+        self.timeout: Optional[float] = timeout
+        self.connection_timeout: Optional[float] = connection_timeout
+        self.requests_verify: bool = requests_verify
