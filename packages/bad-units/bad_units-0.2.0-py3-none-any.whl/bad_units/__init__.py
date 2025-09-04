f2c493d76da2479a0031c696e5224e75ff56de1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:47 +0000

    Add package bad-units/0.2.0/bad_units-0.2.0-py3-none-any.whl

---
+++ packages/bad-units/bad_units-0.2.0-py3-none-any.whl/bad_units/__init__.py
@@ -0,0 +1,4 @@
+from .module import Unit
+from .builtin_units import Meter, Kilometer, Centimeter, Foot, Inch, Kilogram, Gram, Pound, Second, Minute
+
+__all__ = ["Unit", "Meter", "Kilometer", "Centimeter", "Foot", "Inch", "Kilogram", "Gram", "Pound", "Second", "Minute"]
