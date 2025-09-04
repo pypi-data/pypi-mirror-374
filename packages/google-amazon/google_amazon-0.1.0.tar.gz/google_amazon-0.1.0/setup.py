42dbb1862e6f6640896aae3eb64bdb4b5d3d72e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:07:52 +0000

    Add package google-amazon/0.1.0/google_amazon-0.1.0.tar.gz

---
+++ packages/google-amazon/google_amazon-0.1.0.tar.gz/google_amazon-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="google-amazon",          # must be unique on PyPI
+    version="0.1.0",
+    description="Placeholder for our future package",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],               # list dependencies here
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
