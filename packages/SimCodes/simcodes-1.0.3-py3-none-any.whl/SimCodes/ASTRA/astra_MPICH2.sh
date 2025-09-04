cd05e5fd10487e090e72145bdc049f1b8c18bddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:07:58 +0000

    Add package SimCodes/1.0.3/simcodes-1.0.3-py3-none-any.whl

---
+++ packages/SimCodes/simcodes-1.0.3-py3-none-any.whl/SimCodes/ASTRA/astra_MPICH2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/ASTRA/lib
+/opt/ASTRA/astra_r62M_Linux_x86_64_MPICH2_1.3.2p1 $1
---
+++ packages/SimCodes/simcodes-1.0.3-py3-none-any.whl/SimCodes/ASTRA/generator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/xkc85723/Documents/simcodes/SimCodes/ASTRA/generator $1 > /dev/null
