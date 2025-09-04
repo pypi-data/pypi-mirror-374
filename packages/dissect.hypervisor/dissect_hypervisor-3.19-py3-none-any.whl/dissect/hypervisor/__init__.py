54ff193ccaf6ec1a86a7b77739b0b12cba1647fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:04:28 +0000

    Add package dissect.hypervisor/3.19/dissect_hypervisor-3.19-py3-none-any.whl

---
+++ packages/dissect.hypervisor/dissect_hypervisor-3.19-py3-none-any.whl/dissect/hypervisor/__init__.py
@@ -0,0 +1,19 @@
+from dissect.hypervisor.descriptor import hyperv, ovf, pvs, vbox, vmx
+from dissect.hypervisor.disk import hdd, qcow2, vdi, vhd, vhdx, vmdk
+from dissect.hypervisor.util import envelope, vmtar
+
+__all__ = [
+    "envelope",
+    "hdd",
+    "hyperv",
+    "ovf",
+    "pvs",
+    "qcow2",
+    "vbox",
+    "vdi",
+    "vhd",
+    "vhdx",
+    "vmdk",
+    "vmtar",
+    "vmx",
+]
