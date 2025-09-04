01517c31df2eb670588eeb405b88582e8b7dc3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 16:05:01 +0000

    Add package daft/0.6.0/daft-0.6.0-cp39-abi3-win_amd64.whl

---
+++ packages/daft/daft-0.6.0-cp39-abi3-win_amd64.whl/daft/ai/_expressions.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from daft import Series
+    from daft.ai.protocols import ImageEmbedder, ImageEmbedderDescriptor, TextEmbedder, TextEmbedderDescriptor
+    from daft.ai.typing import Embedding
+
+
+class _TextEmbedderExpression:
+    """Function expression implementation for a TextEmbedder protocol."""
+
+    text_embedder: TextEmbedder
+
+    def __init__(self, text_embedder: TextEmbedderDescriptor):
+        self.text_embedder = text_embedder.instantiate()
+
+    def __call__(self, text_series: Series) -> list[Embedding]:
+        text = text_series.to_pylist()
+        return self.text_embedder.embed_text(text) if text else []
+
+
+class _ImageEmbedderExpression:
+    """Function expression implementation for an ImageEmbedder protocol."""
+
+    image_embedder: ImageEmbedder
+
+    def __init__(self, image_embedder: ImageEmbedderDescriptor):
+        self.image_embedder = image_embedder.instantiate()
+
+    def __call__(self, image_series: Series) -> list[Embedding]:
+        image = image_series.to_pylist()
+        return self.image_embedder.embed_image(image) if image else []
---
+++ packages/daft/daft-0.6.0-cp39-abi3-win_amd64.whl/daft/execution/ray_actor_pool_udf.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING
+
+from daft.expressions.expressions import Expression, ExpressionsProjection
+from daft.recordbatch.micropartition import MicroPartition
+
+if TYPE_CHECKING:
+    from daft.daft import PyExpr, PyMicroPartition
+
+try:
+    import ray
+except ImportError:
+    raise
+
+MAX_UDFACTOR_ACTOR_RESTARTS = 4
+MAX_UDFACTOR_ACTOR_TASK_RETRIES = 4
+
+
+@ray.remote(
+    max_restarts=MAX_UDFACTOR_ACTOR_RESTARTS,
+    max_task_retries=MAX_UDFACTOR_ACTOR_TASK_RETRIES,
+)
+class UDFActor:
+    def __init__(self, uninitialized_projection: ExpressionsProjection) -> None:
+        self.projection = ExpressionsProjection([e._initialize_udfs() for e in uninitialized_projection])
+
+    def get_node_id(self) -> str:
+        return ray.get_runtime_context().get_node_id()
+
+    def eval_input(self, input: PyMicroPartition) -> PyMicroPartition:
+        mp = MicroPartition._from_pymicropartition(input)
+        res = mp.eval_expression_list(self.projection)
+        return res._micropartition
+
+
+class UDFActorHandle:
+    def __init__(self, node_id: str, actor_ref: ray.ObjectRef) -> None:
+        self.node_id = node_id
+        self.actor = actor_ref
+
+    async def eval_input(self, input: PyMicroPartition) -> PyMicroPartition:
+        return await self.actor.eval_input.remote(input)
+
+    def is_on_current_node(self) -> bool:
+        return self.node_id == ray.get_runtime_context().get_node_id()
+
+    def teardown(self) -> None:
+        ray.kill(self.actor)
+
+
+async def start_udf_actors(
+    projection: list[PyExpr],
+    num_actors: int,
+    num_gpus_per_actor: float,
+    num_cpus_per_actor: float,
+    memory_per_actor: float,
+) -> list[UDFActorHandle]:
+    expr_projection = ExpressionsProjection([Expression._from_pyexpr(expr) for expr in projection])
+
+    actors = [
+        UDFActor.options(  # type: ignore
+            scheduling_strategy="SPREAD",
+            num_gpus=num_gpus_per_actor,
+            num_cpus=num_cpus_per_actor,
+            memory=memory_per_actor,
+        ).remote(expr_projection)
+        for _ in range(num_actors)
+    ]
+    node_ids = await asyncio.gather(*[actor.get_node_id.remote() for actor in actors])
+    handles = [UDFActorHandle(node_id, actor) for actor, node_id in zip(actors, node_ids)]
+    return handles
