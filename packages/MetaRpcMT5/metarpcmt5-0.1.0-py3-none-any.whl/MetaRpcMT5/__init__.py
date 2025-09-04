14166618887158ce4eec65a6be9bc1c9b874ff4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 16:05:36 +0000

    Add package MetaRpcMT5/0.1.0/metarpcmt5-0.1.0-py3-none-any.whl

---
+++ packages/MetaRpcMT5/metarpcmt5-0.1.0-py3-none-any.whl/MetaRpcMT5/__init__.py
@@ -0,0 +1,32 @@
+from . import mrpc_mt5_error_pb2
+from . import mrpc_mt5_error_pb2_grpc
+
+from . import mt5_term_api_account_helper_pb2
+from . import mt5_term_api_account_helper_pb2_grpc
+
+from . import mt5_term_api_account_information_pb2
+from . import mt5_term_api_account_information_pb2_grpc
+
+from . import mt5_term_api_charts_pb2
+from . import mt5_term_api_charts_pb2_grpc
+
+from . import mt5_term_api_connection_pb2
+from . import mt5_term_api_connection_pb2_grpc
+
+from . import mt5_term_api_health_check_pb2
+from . import mt5_term_api_health_check_pb2_grpc
+
+from . import mt5_term_api_internal_charts_pb2
+from . import mt5_term_api_internal_charts_pb2_grpc
+
+from . import mt5_term_api_market_info_pb2
+from . import mt5_term_api_market_info_pb2_grpc
+
+from . import mt5_term_api_subscriptions_pb2
+from . import mt5_term_api_subscriptions_pb2_grpc
+
+from . import mt5_term_api_trade_functions_pb2
+from . import mt5_term_api_trade_functions_pb2_grpc
+
+from . import mt5_term_api_trading_helper_pb2
+from . import mt5_term_api_trading_helper_pb2_grpc
