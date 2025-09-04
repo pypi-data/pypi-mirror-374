98017a9d8fb9cd4294b9fa4eaae0b5d9fe89e62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:08:18 +0000

    Add package torchic/0.5.32/torchic-0.5.32.tar.gz

---
+++ packages/torchic/torchic-0.5.32.tar.gz/torchic-0.5.32/torchic/core/api.py
@@ -0,0 +1,21 @@
+from torchic.core.dataset import (
+    Dataset,
+)
+
+from torchic.core import histogram
+from torchic.core.histogram import (
+    AxisSpec,
+    HistLoadInfo
+)
+
+from torchic.core.plotter import (
+    Plotter
+)
+
+__all__ = [
+    'Dataset',
+    'AxisSpec',
+    'HistLoadInfo',
+    'histogram',
+    'Plotter'
+]
