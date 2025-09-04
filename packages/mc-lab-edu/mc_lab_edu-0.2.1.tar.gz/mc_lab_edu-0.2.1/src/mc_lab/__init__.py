2cdb89bed6c2a5cb3d7088a3d6f5ae1c2f2fd373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:04:05 +0000

    Add package mc-lab-edu/0.2.1/mc_lab_edu-0.2.1.tar.gz

---
+++ packages/mc-lab-edu/mc_lab_edu-0.2.1.tar.gz/mc_lab_edu-0.2.1/src/mc_lab/__init__.py
@@ -0,0 +1,11 @@
+from .multivariate_gaussian import sample_multivariate_gaussian
+
+
+def hello() -> str:
+    return "Hello from mc-lab!"
+
+
+__all__ = [
+    "sample_multivariate_gaussian",
+    "hello",
+]
