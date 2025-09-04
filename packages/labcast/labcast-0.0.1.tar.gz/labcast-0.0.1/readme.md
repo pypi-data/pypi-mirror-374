5e5fd13d9275ade51f53f82f828258d5b5ee0700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:34 +0000

    Add package labcast/0.0.1/labcast-0.0.1.tar.gz

---
+++ packages/labcast/labcast-0.0.1.tar.gz/labcast-0.0.1/readme.md
@@ -0,0 +1,12 @@
+<!--
+- SPDX-License-Identifier: Apache-2.0
+- Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+-->
+
+# LabCast
+
+Labcast is a Gitlab CI/CD Catalog allowing one to  trigger pipelines on another
+GitLab instance, monitoring  the status of the triggered pipelines, and then
+incorporating the results back into the original pipeline.
+
+
---
+++ packages/labcast/labcast-0.0.1.tar.gz/labcast-0.0.1/src/labcast/metadata.py
@@ -0,0 +1,8 @@
+# --------------------------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# --------------------------------------------------------------------------------------------------
+
+import semver as SemVer
+__version__ = "0.0.1"
+semver = SemVer.VersionInfo.parse(__version__)
