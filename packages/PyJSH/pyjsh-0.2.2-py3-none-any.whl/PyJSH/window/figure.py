a140546334f6b395850b7f02c767e514e8235aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:42 +0000

    Add package PyJSH/0.2.2/pyjsh-0.2.2-py3-none-any.whl

---
+++ packages/PyJSH/pyjsh-0.2.2-py3-none-any.whl/PyJSH/window/figure.py
@@ -0,0 +1,20 @@
+#project2/PyJSH/window/figure.py
+from os import environ
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
+import pygame
+from pygame.locals import FULLSCREEN
+
+class Figure:
+    def __init__(self, model, color, scale, position):
+        self.model = model
+        self.color = color
+        self.scale = scale
+        self.position = position
+
+        # Терезе ашамыз
+        self.screen = pygame.display.set_mode((0, 0), FULLSCREEN)
+
+    def draw(self):
+        rect = pygame.Rect(self.position[0], self.position[1], self.scale[0], self.scale[1])
+        pygame.draw.rect(self.screen, self.color, rect)  # тіктөртбұрыш мысал үшін
+        pygame.display.flip()
