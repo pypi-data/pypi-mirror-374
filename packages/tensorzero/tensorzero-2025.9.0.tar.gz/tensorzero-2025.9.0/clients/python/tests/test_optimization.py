2a1f27fc038ecf166c455eabb77fff14de16ed4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 07:04:34 +0000

    Add package tensorzero/2025.9.0/tensorzero-2025.9.0.tar.gz

---
+++ packages/tensorzero/tensorzero-2025.9.0.tar.gz/tensorzero-2025.9.0/clients/python/tests/test_optimization.py
@@ -0,0 +1,284 @@
+from time import sleep
+from typing import List
+
+import pytest
+from tensorzero import (
+    AsyncTensorZeroGateway,
+    DiclOptimizationConfig,
+    FireworksSFTConfig,
+    OpenAISFTConfig,
+    OptimizationJobStatus,
+    RenderedSample,
+    TensorZeroGateway,
+    TogetherSFTConfig,
+)
+
+
+def test_sync_dicl_chat(
+    embedded_sync_client: TensorZeroGateway,
+    chat_function_rendered_samples: List[RenderedSample],
+):
+    optimization_config = DiclOptimizationConfig(
+        embedding_model="text-embedding-3-small",
+        variant_name="test_dicl",
+        function_name="basic_test",
+        dimensions=None,
+        batch_size=None,
+        max_concurrency=None,
+        k=None,
+        model=None,
+        credentials=None,
+    )
+    optimization_job_handle = embedded_sync_client.experimental_launch_optimization(
+        train_samples=chat_function_rendered_samples,
+        val_samples=None,
+        optimization_config=optimization_config,
+    )
+    while True:
+        job_info = embedded_sync_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+def test_sync_dicl_json(
+    embedded_sync_client: TensorZeroGateway,
+    json_function_rendered_samples: List[RenderedSample],
+):
+    optimization_config = DiclOptimizationConfig(
+        embedding_model="text-embedding-3-small",
+        variant_name="test_dicl_json",
+        function_name="json_success",
+        dimensions=None,
+        batch_size=None,
+        max_concurrency=None,
+        k=None,
+        model=None,
+        credentials=None,
+    )
+    optimization_job_handle = embedded_sync_client.experimental_launch_optimization(
+        train_samples=json_function_rendered_samples,
+        val_samples=None,
+        optimization_config=optimization_config,
+    )
+    while True:
+        job_info = embedded_sync_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+def test_sync_openai_sft(
+    embedded_sync_client: TensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = OpenAISFTConfig(
+        model="gpt-4o-mini", api_base="http://localhost:3030/openai/"
+    )
+    optimization_job_handle = embedded_sync_client.experimental_launch_optimization(
+        train_samples=mixed_rendered_samples,
+        val_samples=None,
+        optimization_config=optimization_config,
+    )
+    while True:
+        job_info = embedded_sync_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+def test_sync_fireworks_sft(
+    embedded_sync_client: TensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = FireworksSFTConfig(
+        model="gpt-4o-mini",
+        api_base="http://localhost:3030/fireworks/",
+        account_id="test",
+        epochs=1,
+    )
+    optimization_job_handle = embedded_sync_client.experimental_launch_optimization(
+        train_samples=mixed_rendered_samples,
+        val_samples=None,
+        optimization_config=optimization_config,
+    )
+    while True:
+        job_info = embedded_sync_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+def test_sync_together_sft(
+    embedded_sync_client: TensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = TogetherSFTConfig(
+        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Reference",
+        api_base="http://localhost:3030/together/",
+        n_epochs=1,
+        training_type={"type": "Lora", "lora_r": 8, "lora_alpha": 16},
+        batch_size="max",
+    )
+    optimization_job_handle = embedded_sync_client.experimental_launch_optimization(
+        train_samples=mixed_rendered_samples,
+        val_samples=None,
+        optimization_config=optimization_config,
+    )
+    while True:
+        job_info = embedded_sync_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_async_dicl_chat(
+    embedded_async_client: AsyncTensorZeroGateway,
+    chat_function_rendered_samples: List[RenderedSample],
+):
+    optimization_config = DiclOptimizationConfig(
+        embedding_model="text-embedding-3-small",
+        variant_name="test_dicl",
+        function_name="basic_test",
+        dimensions=None,
+        batch_size=None,
+        max_concurrency=None,
+        k=None,
+        model=None,
+        credentials=None,
+    )
+    optimization_job_handle = (
+        await embedded_async_client.experimental_launch_optimization(
+            train_samples=chat_function_rendered_samples,
+            val_samples=None,
+            optimization_config=optimization_config,
+        )
+    )
+    while True:
+        job_info = await embedded_async_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_async_dicl_json(
+    embedded_async_client: AsyncTensorZeroGateway,
+    json_function_rendered_samples: List[RenderedSample],
+):
+    optimization_config = DiclOptimizationConfig(
+        embedding_model="text-embedding-3-small",
+        variant_name="test_dicl_json",
+        function_name="json_success",
+        dimensions=None,
+        batch_size=None,
+        max_concurrency=None,
+        k=None,
+        model=None,
+        credentials=None,
+    )
+    optimization_job_handle = (
+        await embedded_async_client.experimental_launch_optimization(
+            train_samples=json_function_rendered_samples,
+            val_samples=None,
+            optimization_config=optimization_config,
+        )
+    )
+    while True:
+        job_info = await embedded_async_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_async_openai_sft(
+    embedded_async_client: AsyncTensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = OpenAISFTConfig(
+        model="gpt-4o-mini", api_base="http://localhost:3030/openai/"
+    )
+    optimization_job_handle = (
+        await embedded_async_client.experimental_launch_optimization(
+            train_samples=mixed_rendered_samples,
+            val_samples=None,
+            optimization_config=optimization_config,
+        )
+    )
+    while True:
+        job_info = await embedded_async_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+
+
+@pytest.mark.asyncio
+async def test_async_fireworks_sft(
+    embedded_async_client: AsyncTensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = FireworksSFTConfig(
+        model="gpt-4o-mini",
+        api_base="http://localhost:3030/fireworks/",
+        account_id="test",
+        epochs=1,
+    )
+    optimization_job_handle = (
+        await embedded_async_client.experimental_launch_optimization(
+            train_samples=mixed_rendered_samples,
+            val_samples=None,
+            optimization_config=optimization_config,
+        )
+    )
+    while True:
+        job_info = await embedded_async_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
+
+
+@pytest.mark.asyncio
+async def test_async_together_sft(
+    embedded_async_client: AsyncTensorZeroGateway,
+    mixed_rendered_samples: List[RenderedSample],
+):
+    optimization_config = TogetherSFTConfig(
+        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Reference",
+        api_base="http://localhost:3030/together/",
+        n_epochs=1,
+        training_type={"type": "Lora", "lora_r": 8, "lora_alpha": 16},
+        batch_size="max",
+    )
+    optimization_job_handle = (
+        await embedded_async_client.experimental_launch_optimization(
+            train_samples=mixed_rendered_samples,
+            val_samples=None,
+            optimization_config=optimization_config,
+        )
+    )
+    while True:
+        job_info = await embedded_async_client.experimental_poll_optimization(
+            job_handle=optimization_job_handle
+        )
+        if job_info.status == OptimizationJobStatus.Completed:
+            break
+        sleep(1)
