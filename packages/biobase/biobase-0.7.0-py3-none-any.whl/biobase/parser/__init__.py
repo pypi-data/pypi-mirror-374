a1dcd3422cc8187d361ac6b8feed3aa611e87655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:03:57 +0000

    Add package biobase/0.7.0/biobase-0.7.0-py3-none-any.whl

---
+++ packages/biobase/biobase-0.7.0-py3-none-any.whl/biobase/parser/__init__.py
@@ -0,0 +1,8 @@
+from biobase.parser import fasta
+
+# Fasta parser functions
+fasta_parser = fasta.fasta_parser
+fasta_file_parser = fasta.fasta_file_parser
+
+# Record Class
+FastaRecord = fasta.FastaRecord
