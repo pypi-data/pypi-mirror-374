a7a1b7526d063c894507296863c915313e192872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:08:46 +0000

    Add package codegrade/17.0.5/codegrade-17.0.5.tar.gz

---
+++ packages/codegrade/codegrade-17.0.5.tar.gz/codegrade-17.0.5/docs/services/transaction.rst
@@ -0,0 +1,16 @@
+
+TransactionService
+==================
+
+.. currentmodule:: codegrade._api.transaction
+
+.. autoclass:: TransactionService
+   :members: get, refund
+
+	 .. rubric:: Methods
+
+	 .. autosummary::
+	    :nosignatures:
+
+			~TransactionService.get
+			~TransactionService.refund
---
+++ packages/codegrade/codegrade-17.0.5.tar.gz/codegrade-17.0.5/src/codegrade/_api/transaction.py
@@ -0,0 +1,117 @@
+"""The endpoints for transaction objects.
+
+SPDX-License-Identifier: AGPL-3.0-only OR BSD-3-Clause-Clear
+"""
+
+from __future__ import annotations
+
+import os
+import typing as t
+
+import cg_request_args as rqa
+from cg_maybe import Maybe, Nothing
+
+from .. import parsers, utils
+
+if t.TYPE_CHECKING or os.getenv("CG_EAGERIMPORT", False):
+    from .. import client
+    from ..models.course_purchase import CoursePurchase
+    from ..models.user_access_pass import UserAccessPass
+
+
+_ClientT = t.TypeVar("_ClientT", bound="client._BaseClient")
+
+
+class TransactionService(t.Generic[_ClientT]):
+    __slots__ = ("__client",)
+
+    def __init__(self, client: _ClientT) -> None:
+        self.__client = client
+
+    def get(
+        self: TransactionService[client.AuthenticatedClient],
+        *,
+        transaction_id: str,
+    ) -> t.Union[CoursePurchase, UserAccessPass]:
+        """Get a transaction by id.
+
+        :param transaction_id: The id of the transaction you want to get.
+
+        :returns: The found transaction.
+        """
+
+        url = "/api/v1/transactions/{transactionId}".format(
+            transactionId=transaction_id
+        )
+        params = None
+
+        with self.__client as client:
+            resp = client.http.get(url=url, params=params)
+        utils.log_warnings(resp)
+
+        if utils.response_code_matches(resp.status_code, 200):
+            from ..models.course_purchase import CoursePurchase
+            from ..models.user_access_pass import UserAccessPass
+
+            return parsers.JsonResponseParser(
+                parsers.make_union(
+                    parsers.ParserFor.make(CoursePurchase),
+                    parsers.ParserFor.make(UserAccessPass),
+                )
+            ).try_parse(resp)
+
+        from ..models.any_error import AnyError
+
+        raise utils.get_error(
+            resp,
+            (
+                (
+                    (400, 409, 401, 403, 404, 429, 500),
+                    utils.unpack_union(AnyError),
+                ),
+            ),
+        )
+
+    def refund(
+        self: TransactionService[client.AuthenticatedClient],
+        *,
+        transaction_id: str,
+    ) -> t.Union[CoursePurchase, UserAccessPass]:
+        """Refund a transaction.
+
+        :param transaction_id: The id of the transaction you want to get.
+
+        :returns: The found transaction.
+        """
+
+        url = "/api/v1/transactions/{transactionId}/refund".format(
+            transactionId=transaction_id
+        )
+        params = None
+
+        with self.__client as client:
+            resp = client.http.post(url=url, params=params)
+        utils.log_warnings(resp)
+
+        if utils.response_code_matches(resp.status_code, 200):
+            from ..models.course_purchase import CoursePurchase
+            from ..models.user_access_pass import UserAccessPass
+
+            return parsers.JsonResponseParser(
+                parsers.make_union(
+                    parsers.ParserFor.make(CoursePurchase),
+                    parsers.ParserFor.make(UserAccessPass),
+                )
+            ).try_parse(resp)
+
+        from ..models.any_error import AnyError
+
+        raise utils.get_error(
+            resp,
+            (
+                (
+                    (400, 409, 401, 403, 404, 429, 500),
+                    utils.unpack_union(AnyError),
+                ),
+            ),
+        )
