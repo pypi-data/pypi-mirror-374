81d995ceebe03f7005ba1519ed47e4f768c66245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:12:09 +0000

    Add package streamOneIonSDK/0.1.8/streamoneionsdk-0.1.8.tar.gz

---
+++ packages/streamOneIonSDK/streamoneionsdk-0.1.8.tar.gz/streamoneionsdk-0.1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='streamOneIonSDK',
+    version='0.1.8',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add command line scripts here
+        ],
+    },
+    long_description=long_description,
+    # or "text/x-rst" if using reStructuredText
+    long_description_content_type="text/markdown",
+)
