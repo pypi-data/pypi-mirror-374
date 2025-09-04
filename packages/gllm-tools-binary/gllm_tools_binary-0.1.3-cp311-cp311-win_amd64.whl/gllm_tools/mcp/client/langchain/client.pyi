fe6179d98a09c9f4a8bb5235aa9ad5c78488a26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:08:49 +0000

    Add package gllm-tools-binary/0.1.3/gllm_tools_binary-0.1.3-cp311-cp311-win_amd64.whl

---
+++ packages/gllm-tools-binary/gllm_tools_binary-0.1.3-cp311-cp311-win_amd64.whl/gllm_tools/mcp/client/langchain/client.pyi
@@ -0,0 +1,17 @@
+from gllm_tools.mcp.client.client import MCPClient as MCPClient
+from gllm_tools.mcp.client.config import MCPConfiguration as MCPConfiguration
+from gllm_tools.mcp.client.resource import MCPResource as MCPResource
+from gllm_tools.mcp.client.session import create_session as create_session
+from gllm_tools.mcp.client.tool import MCPTool as MCPTool
+from mcp.types import CallToolResult as CallToolResult, EmbeddedResource, ImageContent
+from pydantic import AnyUrl as AnyUrl
+
+NonTextContent = ImageContent | EmbeddedResource
+
+class LangchainMCPClient(MCPClient):
+    """Langchain MCP Client.
+
+    This client is a wrapper around the MCPClient that converts MCP tools and resources
+    into Langchain tools and resources. It is used to integrate MCP with Langchain.
+    """
+    RESOURCE_FETCH_TIMEOUT: int
