4d80eb3f4cfba3dfcccf006c814b35ce299d3c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:30 +0000

    Add package omlish/0.0.0.dev423/omlish-0.0.0.dev423.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev423.tar.gz/omlish-0.0.0.dev423/omlish/logs/handlers.py
@@ -0,0 +1,17 @@
+# ruff: noqa: UP006 UP007 UP045
+# @omlish-lite
+import logging
+import typing as ta
+
+
+##
+
+
+class ListHandler(logging.Handler):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.records: ta.List[logging.LogRecord] = []
+
+    def emit(self, record: logging.LogRecord) -> None:
+        self.records.append(record)
