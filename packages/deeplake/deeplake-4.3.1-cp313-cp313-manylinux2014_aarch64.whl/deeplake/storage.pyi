c5d054d72629d1b585fb061de514387a91fe511e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:13:03 +0000

    Add package deeplake/4.3.1/deeplake-4.3.1-cp313-cp313-manylinux2014_aarch64.whl

---
+++ packages/deeplake/deeplake-4.3.1-cp313-cp313-manylinux2014_aarch64.whl/deeplake/storage.pyi
@@ -0,0 +1,99 @@
+"""
+Storage readers and writers - s3, gcs, azure, filesystem, etc.
+"""
+
+from __future__ import annotations
+import datetime
+import deeplake._deeplake.core
+import typing
+from deeplake import Future
+
+__all__ = ["Reader", "Writer", "ResourceMeta", "concurrency", "set_concurrency"]
+
+class Reader:
+    def __getstate__(self) -> tuple: ...
+    def __setstate__(self, arg0: tuple) -> None: ...
+    def get(
+        self, path: str, start_bytes: int | None = None, end_bytes: int | None = None
+    ) -> deeplake._deeplake.core.MemoryBuffer: ...
+    def get_async(self, path: str, start_bytes: int | None = None, end_bytes: int | None = None) -> Future[deeplake._deeplake.core.MemoryBuffer]:
+        ...
+    def length(self, path: str) -> int: ...
+    def list(self, path: str = '') -> list[ResourceMeta]: ...
+    def subdir(self, subpath: str) -> Reader: ...
+    @property
+    def original_path(self) -> str: ...
+    @property
+    def path(self) -> str: ...
+    @property
+    def token(self) -> str: ...
+
+class Writer:
+    def __getstate__(self) -> tuple: ...
+    def __setstate__(self, arg0: tuple) -> None: ...
+    def remove(self, path: str) -> None: ...
+    def remove_directory(self, prefix: str = "") -> None: ...
+    def set(self, path: str, content: bytes) -> ResourceMeta: ...
+    def set_async(self, path: str, content: bytes) -> Future[ResourceMeta]: ...
+    def subdir(self, subpath: str) -> Writer: ...
+    @property
+    def original_path(self) -> str: ...
+    @property
+    def path(self) -> str: ...
+    @property
+    def token(self) -> str: ...
+
+class ResourceMeta:
+    __hash__: typing.ClassVar[None] = None
+    def __eq__(self, other: ResourceMeta) -> bool:
+        ...
+    def __lt__(self, other: ResourceMeta) -> bool:
+        ...
+    def __repr__(self) -> str:
+        ...
+    @property
+    def etag(self) -> str:
+        ...
+    @property
+    def last_modified(self) -> datetime.datetime:
+        ...
+    @property
+    def path(self) -> str:
+        ...
+    @property
+    def size(self) -> int:
+        ...
+
+def concurrency() -> int:
+    """
+    Returns the number of threads of storage readers and writers.
+
+    <!-- test-context
+    ```python
+    import deeplake
+    ```
+    -->
+
+    Examples:
+        ```python
+        deeplake.storage.concurrency()
+        ```
+    """
+    ...
+
+def set_concurrency(num_threads: int) -> None:
+    """
+    Sets the number of threads of storage readers and writers.
+
+    <!-- test-context
+    ```python
+    import deeplake
+    ```
+    -->
+
+    Examples:
+        ```python
+        deeplake.storage.set_concurrency(64)
+        ```
+    """
+
