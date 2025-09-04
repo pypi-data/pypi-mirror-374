feae23e1ea774d4965d22f1af1de6e810c0116f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:45 +0000

    Add package netboxlabs-netbox-branching/0.6.2/netboxlabs_netbox_branching-0.6.2.tar.gz

---
+++ packages/netboxlabs-netbox-branching/netboxlabs_netbox_branching-0.6.2.tar.gz/netboxlabs_netbox_branching-0.6.2/netbox_branching/database.py
@@ -0,0 +1,66 @@
+import warnings
+
+from netbox.registry import registry
+
+from .contextvars import active_branch
+
+
+__all__ = (
+    'BranchAwareRouter',
+)
+
+
+class BranchAwareRouter:
+    """
+    A Django database router that returns the appropriate connection/schema for
+    the active branch (if any).
+    """
+    connection_prefix = 'schema_'
+
+    def _get_connection(self, branch):
+        return f'{self.connection_prefix}{branch.schema_name}'
+
+    def _get_db(self, model, **hints):
+        # Warn & exit if branching support has not yet been initialized
+        if 'branching' not in registry['model_features']:
+            warnings.warn(f"Routing database query for {model} before branching support is initialized.")
+            return
+
+        # Bail if the model does not support branching
+        app_label, model_name = model._meta.label.lower().split('.')
+        if model_name not in registry['model_features']['branching'].get(app_label, []):
+            return
+
+        # Return the schema for the active branch (if any)
+        if branch := active_branch.get():
+            return self._get_connection(branch)
+
+    def db_for_read(self, model, **hints):
+
+        # Always use the active branch (if any) when retrieving changelog records
+        if model._meta.label == 'core.ObjectChange':
+            if branch := active_branch.get():
+                return self._get_connection(branch)
+            return
+
+        return self._get_db(model, **hints)
+
+    def db_for_write(self, model, **hints):
+        return self._get_db(model, **hints)
+
+    def allow_relation(self, obj1, obj2, **hints):
+        # Permit relations from the branch schema to the main schema
+        return True
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        # This router has no opinion on non-branch connections
+        if not db.startswith(self.connection_prefix):
+            return
+
+        # Disallow migrations for models from the plugin itself within a branch
+        if app_label == 'netbox_branching':
+            return False
+
+        # Disallow migrations for models which don't support branching
+        if model_name and model_name not in registry['model_features']['branching'].get(app_label, []):
+            return False
