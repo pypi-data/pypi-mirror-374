a0a371b35731acf320e1cf9bfa7d07bdecf5c56a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:08:18 +0000

    Add package entitysdk/7.9/entitysdk-7.9.tar.gz

---
+++ packages/entitysdk/entitysdk-7.9.tar.gz/entitysdk-7.9/tests/integration/test_searching.py
@@ -0,0 +1,50 @@
+import pytest
+
+from entitysdk.models import (
+    Contribution,
+    ElectricalCellRecording,
+    EModel,
+    IonChannelModel,
+    License,
+    MEModel,
+    MEModelCalibrationResult,
+    MTypeClass,
+    Organization,
+    Person,
+    ReconstructionMorphology,
+    Role,
+    SingleNeuronSimulation,
+    SingleNeuronSynaptome,
+    SingleNeuronSynaptomeSimulation,
+    Species,
+    Strain,
+    ValidationResult,
+)
+
+
+@pytest.mark.parametrize(
+    "entity_type",
+    [
+        Contribution,
+        IonChannelModel,
+        License,
+        MTypeClass,
+        Person,
+        ReconstructionMorphology,
+        Role,
+        Species,
+        Strain,
+        Organization,
+        EModel,
+        MEModel,
+        ElectricalCellRecording,
+        ValidationResult,
+        MEModelCalibrationResult,
+        SingleNeuronSimulation,
+        SingleNeuronSynaptomeSimulation,
+        SingleNeuronSynaptome,
+    ],
+)
+def test_is_searchable(entity_type, client):
+    res = client.search_entity(entity_type=entity_type, limit=1).one()
+    assert res.id
