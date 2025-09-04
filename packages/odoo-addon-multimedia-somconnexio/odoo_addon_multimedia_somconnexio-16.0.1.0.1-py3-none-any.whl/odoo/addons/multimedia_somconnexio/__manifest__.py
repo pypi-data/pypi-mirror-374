97a052ae61c9be8d914467bc4cf987210e99fff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:04:10 +0000

    Add package odoo-addon-multimedia-somconnexio/16.0.1.0.1/odoo_addon_multimedia_somconnexio-16.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo-addon-multimedia-somconnexio/odoo_addon_multimedia_somconnexio-16.0.1.0.1-py3-none-any.whl/odoo/addons/multimedia_somconnexio/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "multimedia_somconnexio",
+    "version": "16.0.1.0.1",
+    "depends": ["somconnexio", "product_product_sale_subscription_template"],
+    "summary": """
+        Sets a basic structure for multimedia subscriptions within the SomConnexio ERP
+    """,
+    "author": "Coopdevs Treball SCCL, " "Som Connexió SCCL",
+    "website": "https://coopdevs.org",
+    "license": "AGPL-3",
+    "category": "Cooperative management",
+    "data": [
+        "data/product_attribute.xml",
+        "data/product_attribute_value.xml",
+        "data/product_category.xml",
+        "data/product_template.xml",
+        "data/product_template_attribute_line.xml",
+        "data/service_technology.xml",
+        "security/ir.model.access.csv",
+        "views/contract_views.xml",
+        "views/product_views.xml",
+        "views/crm_lead.xml",
+        "views/crm_lead_line.xml",
+        "wizards/crm_lead_add_multimedia_line/crm_lead_add_multimedia_line.xml",
+    ],
+    "demo": [
+        "demo/service_supplier.xml",
+        "demo/service_technology_service_supplier.xml",
+    ],
+}
