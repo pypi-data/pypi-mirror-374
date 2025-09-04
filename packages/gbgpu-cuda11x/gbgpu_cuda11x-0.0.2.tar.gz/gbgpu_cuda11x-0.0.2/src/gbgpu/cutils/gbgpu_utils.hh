7975fc8f1c3693875e61dc047dfc62fef332eaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:04:16 +0000

    Add package gbgpu-cuda11x/0.0.2/gbgpu_cuda11x-0.0.2.tar.gz

---
+++ packages/gbgpu-cuda11x/gbgpu_cuda11x-0.0.2.tar.gz/gbgpu_cuda11x-0.0.2/src/gbgpu/cutils/gbgpu_utils.hh
@@ -0,0 +1,15 @@
+#ifndef __NEW_FASTGB_HH__
+#define __NEW_FASTGB_HH__
+
+#include "global.h"
+
+void fill_global_wrap(cmplx *A_glob, cmplx *E_glob, cmplx *A_template, cmplx *E_template,
+                      int *start_ind_all, int M, int num_bin, int *group_index, int data_length);
+
+void get_ll_wrap(cmplx *d_h, cmplx *h_h,
+                 cmplx *A_template, cmplx *E_template,
+                 cmplx *A_data, cmplx *E_data,
+                 double *A_psd, double *E_psd, double df,
+                 int *start_ind, int M, int num_bin, int *data_index, int *noise_index, int data_length);
+
+#endif // __NEW_FASTGB_HH__
