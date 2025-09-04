6c9bca87e7723f60db2c90f3cf96c463a2cf2881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:06 +0000

    Add package snappi-ixload/0.0.8/snappi_ixload-0.0.8-py2.py3-none-any.whl

---
+++ packages/snappi-ixload/snappi_ixload-0.0.8-py2.py3-none-any.whl/snappi_ixload/__init__.py
@@ -0,0 +1,1 @@
+from snappi_ixload.snappi_api import Api
---
+++ packages/snappi-ixload/snappi_ixload-0.0.8-py2.py3-none-any.whl/snappi_ixload/ports.py
@@ -0,0 +1,87 @@
+import json
+import re
+import time
+from .timer import Timer
+
+class port(object):
+    """
+    Args
+    ----
+    - Ixloadapi (Api): instance of the Api class
+
+    """
+        
+    def __init__(self, ixloadapi):
+        self._api = ixloadapi
+    
+    def config(self):
+        """T
+        """
+        self._config = self._api._l47config
+        with Timer(self._api, "Port Configuration"):
+            self._create_chassis()
+    
+    def _create_chassis(self):
+        """Add any scenarios to the api server that do not already exist
+        """
+        for device in self._config.devices:
+            ethernet = device.ethernets[0]
+            location = self._get_chasiss(ethernet.connection.port_name, self._config.ports)
+            #import pdb;pdb.set_trace()
+            if location :
+                if not self.is_chassis_connected(location):
+                    self._add_chassis(location)
+                if ethernet.ipv4_addresses:
+                    for ip in ethernet.ipv4_addresses:
+                        self._assign_ports(location, ip.name)
+                elif ethernet.ipv6_addresses:
+                    for ip in ethernet.ipv6_addresses:
+                        self._assign_ports(location, ip.name)
+            else:
+                pass
+    
+    def _get_chasiss(self, port_name, port_config):
+        '''
+        '''
+        for port in port_config:
+            if port.name == port_name:
+                return port.location
+        return False
+            
+    def _add_chassis(self, location):
+        '''
+        '''
+        #
+        chassis_name = location.split("/")[0]
+        chassis_list_url = "%s/ixload/chassisChain/chassisList" % (self._api._ixload)
+        payload = {"name": chassis_name}
+        response = self._api._request('POST', chassis_list_url, payload)
+        refresh_connection_url = "%s/%s/operations/refreshConnection" % (chassis_list_url, response)
+        response = self._api._request('POST', refresh_connection_url, {})
+        self._api._perform_generic_operation(refresh_connection_url, {})
+    
+    def is_chassis_connected(self, location):
+        chassis_list_url = "%s/ixload/chassisChain/chassisList" % (self._api._ixload)
+        chassis_name = location.split("/")[0]
+        chassis_list = self._api._request('GET', chassis_list_url)
+        for chassis in chassis_list:
+            if chassis['name'] == chassis_name and chassis['isConnected'] == True:
+                return True
+        return False
+    
+    def _assign_ports(self, location, ip_name):
+        '''
+        
+        '''
+        active_test_url = "%s/ixload/test/activeTest" % (self._api._ixload)
+        payload = {"enableForceOwnership": "true"}
+        self._api._request('PATCH', active_test_url, payload)
+        url = self._api._config_url.get(ip_name)
+        url = self._api.common.get_community_url(url)
+        port_list_url = url + "network/portList"
+        chassis_id = 1
+        chassis_ip, card_id, port_id = location.split("/")
+        payload = {"chassisId": chassis_id, "cardId": card_id, "portId": port_id}
+        self._api._request('POST', port_list_url, payload)
+            
+            
