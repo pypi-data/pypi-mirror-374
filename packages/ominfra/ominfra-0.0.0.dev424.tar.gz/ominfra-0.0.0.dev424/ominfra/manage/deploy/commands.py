09b2844c0a1273158b160f644a2cca84604bf26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:31 +0000

    Add package ominfra/0.0.0.dev424/ominfra-0.0.0.dev424.tar.gz

---
+++ packages/ominfra/ominfra-0.0.0.dev424.tar.gz/ominfra-0.0.0.dev424/ominfra/manage/deploy/commands.py
@@ -0,0 +1,34 @@
+# ruff: noqa: UP006 UP007 UP045
+import dataclasses as dc
+
+from omlish.lite.logs import log
+
+from ..commands.base import Command
+from ..commands.base import CommandExecutor
+from .deploy import DeployDriverFactory
+from .specs import DeploySpec
+
+
+##
+
+
+@dc.dataclass(frozen=True)
+class DeployCommand(Command['DeployCommand.Output']):
+    spec: DeploySpec
+
+    @dc.dataclass(frozen=True)
+    class Output(Command.Output):
+        pass
+
+
+@dc.dataclass(frozen=True)
+class DeployCommandExecutor(CommandExecutor[DeployCommand, DeployCommand.Output]):
+    _driver_factory: DeployDriverFactory
+
+    async def execute(self, cmd: DeployCommand) -> DeployCommand.Output:
+        log.info('Deploying! %r', cmd.spec)
+
+        with self._driver_factory(cmd.spec) as driver:
+            await driver.drive_deploy()
+
+        return DeployCommand.Output()
