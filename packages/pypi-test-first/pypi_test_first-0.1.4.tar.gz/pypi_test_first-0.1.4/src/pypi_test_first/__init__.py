ba0c019749a197667dec129eac2bab843b69e97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:32 +0000

    Add package pypi-test-first/0.1.4/pypi_test_first-0.1.4.tar.gz

---
+++ packages/pypi-test-first/pypi_test_first-0.1.4.tar.gz/pypi_test_first-0.1.4/src/pypi_test_first/__init__.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    """This function adds two numbers"""
+    return a + b
+
+def subtract(a, b):
+    """This function subtracts two numbers"""
+    return a - b
+
+def multiply(a, b):
+    """This function multiplies two numbers"""
+    return a * b
+
+def divide(a, b):
+    """This function divides two numbers"""
+    if b == 0:
+        raise ValueError("Cannot divide by zero!")
+    return a / b
+
+def power(a, b):
+    """This function raises a to the power of b"""
+    return a ** b
