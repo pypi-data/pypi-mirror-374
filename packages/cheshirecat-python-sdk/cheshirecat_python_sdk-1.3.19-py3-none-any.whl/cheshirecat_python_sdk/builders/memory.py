2a34b6266a4193f524e75a9a8f3ecf2e95cff3f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:36 +0000

    Add package cheshirecat-python-sdk/1.3.19/cheshirecat_python_sdk-1.3.19-py3-none-any.whl

---
+++ packages/cheshirecat-python-sdk/cheshirecat_python_sdk-1.3.19-py3-none-any.whl/cheshirecat_python_sdk/builders/memory.py
@@ -0,0 +1,50 @@
+from typing import Dict, Any
+
+from cheshirecat_python_sdk.builders.base import BaseBuilder
+from cheshirecat_python_sdk.models.dtos import Memory, MemoryPoint
+
+
+class MemoryBuilder(BaseBuilder):
+    def __init__(self):
+        self.declarative: Dict[str, Any] | None = None
+        self.procedural: Dict[str, Any] | None = None
+
+    @staticmethod
+    def create():
+        return MemoryBuilder()
+
+    def set_declarative(self, declarative: Dict[str, Any] | None = None):
+        self.declarative = declarative or {}
+        return self
+
+    def set_procedural(self, procedural: Dict[str, Any] | None = None):
+        self.procedural = procedural or {}
+        return self
+
+    def build(self):
+        memory = Memory()
+        memory.declarative = self.declarative
+        memory.procedural = self.procedural
+
+        return memory
+
+
+class MemoryPointBuilder(BaseBuilder):
+    def __init__(self):
+        self.content: str = ""
+        self.metadata: Dict[str, Any] = {}
+
+    @staticmethod
+    def create():
+        return MemoryPointBuilder()
+
+    def set_content(self, content: str):
+        self.content = content
+        return self
+
+    def set_metadata(self, metadata: Dict[str, Any]):
+        self.metadata = metadata
+        return self
+
+    def build(self):
+        return MemoryPoint(self.content, self.metadata)
