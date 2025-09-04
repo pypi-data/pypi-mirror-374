d5e414563524cf6142033b596d0af48ef2a0af4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 17:08:58 +0000

    Add package edsnlp/0.18.0/edsnlp-0.18.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.18.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tests/pipelines/core/test_terminology.py
@@ -0,0 +1,29 @@
+import pytest
+
+from edsnlp.core import PipelineProtocol
+from edsnlp.utils.examples import parse_example
+
+example = "1g de <ent kb_id=paracetamol>doliprane</ent>"
+
+
+@pytest.mark.parametrize("term_matcher", ["exact", "simstring"])
+def test_terminology(blank_nlp: PipelineProtocol, term_matcher: str):
+    blank_nlp.add_pipe(
+        "eds.terminology",
+        config=dict(
+            label="drugs",
+            terms=dict(paracetamol=["doliprane", "tylenol", "paracetamol"]),
+            attr="NORM",
+            term_matcher=term_matcher,
+        ),
+    )
+
+    text, entities = parse_example(example)
+
+    doc = blank_nlp(text)
+
+    assert len(entities) == len(doc.ents)
+
+    for ent, entity in zip(doc.ents, entities):
+        assert ent.text == text[entity.start_char : entity.end_char]
+        assert ent.kb_id_ == entity.modifiers[0].value
