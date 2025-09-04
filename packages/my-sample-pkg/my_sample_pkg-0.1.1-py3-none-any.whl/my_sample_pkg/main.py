cbf76a416c93a4f56fa8a9eafbc10486bc413556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:08:27 +0000

    Add package my-sample-pkg/0.1.1/my_sample_pkg-0.1.1-py3-none-any.whl

---
+++ packages/my-sample-pkg/my_sample_pkg-0.1.1-py3-none-any.whl/my_sample_pkg/main.py
@@ -0,0 +1,11 @@
+"""Main module for my-sample-pkg."""
+
+
+def hello():
+    """Print a greeting message to PyPI."""
+    print("Hello, PyPI!")
+    return "Hello, PyPI!"
+
+
+if __name__ == "__main__":
+    hello()
