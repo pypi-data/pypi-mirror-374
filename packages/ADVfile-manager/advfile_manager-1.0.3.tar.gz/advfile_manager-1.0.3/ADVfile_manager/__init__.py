02cb533d2d7be319a1474c3f875d6d8e90963b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:03:45 +0000

    Add package ADVfile-manager/1.0.3/advfile_manager-1.0.3.tar.gz

---
+++ packages/ADVfile-manager/advfile_manager-1.0.3.tar.gz/advfile_manager-1.0.3/ADVfile_manager/__init__.py
@@ -0,0 +1,31 @@
+"""
+ADVfile_manager
+===============
+
+Unified file abstractions with backups, context manager, and exit cleanup.
+
+Author: Avi Twil
+"""
+
+from .core import (
+    File,
+    TextFile,
+    JsonFile,
+    CsvFile,
+    YamlFile,
+    set_exit_cleanup,
+    cleanup_backups_for_all,
+)
+
+__all__ = [
+    "File",
+    "TextFile",
+    "JsonFile",
+    "CsvFile",
+    "YamlFile",
+    "set_exit_cleanup",
+    "cleanup_backups_for_all",
+]
+
+__version__ = "1.0.3"
+__author__ = "Avi Twil"
