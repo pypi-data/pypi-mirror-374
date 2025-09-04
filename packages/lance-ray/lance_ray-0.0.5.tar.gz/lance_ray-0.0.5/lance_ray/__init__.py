b9f8cc7a77d36a5ca70df3618f0dbc6292951ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:04:23 +0000

    Add package lance-ray/0.0.5/lance_ray-0.0.5.tar.gz

---
+++ packages/lance-ray/lance_ray-0.0.5.tar.gz/lance_ray-0.0.5/lance_ray/__init__.py
@@ -0,0 +1,19 @@
+"""
+Lance-Ray: Ray integration for Lance columnar format.
+
+This package provides integration between Ray and Lance for distributed
+columnar data processing.
+"""
+
+__version__ = "0.0.5"
+__author__ = "LanceDB Devs"
+__email__ = "dev@lancedb.com"
+
+# Main imports
+from .io import add_columns, read_lance, write_lance
+
+__all__ = [
+    "read_lance",
+    "write_lance",
+    "add_columns",
+]
