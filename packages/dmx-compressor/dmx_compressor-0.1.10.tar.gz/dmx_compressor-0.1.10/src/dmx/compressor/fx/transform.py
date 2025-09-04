f13c4378f19ee56346e4c5bc104e40951fcadc12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:03:53 +0000

    Add package dmx-compressor/0.1.10/dmx_compressor-0.1.10.tar.gz

---
+++ packages/dmx-compressor/dmx_compressor-0.1.10.tar.gz/dmx_compressor-0.1.10/src/dmx/compressor/fx/transform.py
@@ -0,0 +1,161 @@
+import torch
+import torch.nn as nn
+
+from dmx.compressor.fx.tracer import HFQuantTracer, hf_symbolic_trace
+from ..fx import QuantTracer, InputOutputTransformer, QdQTransformer
+from dmx.compressor.fx import (
+    ConfigurationTransformer,
+    DMXAwareTransformer,
+    RecordInputInterpreter,
+)
+from dmx.compressor.fx.transformer.utils import dmx_aware_mapping
+from torch.fx import GraphModule
+from typing import Any, Dict, List, Optional, Union
+from inspect import signature
+
+
+def prepare_tracing_inputs(_model, args, kwargs):
+    # remove kwargs with value None
+    kwargs = {k: v for k, v in kwargs.items() if v is not None}
+    # boolean inputs will affect tracing and need to be set as concrete args
+    bool_inputs = {k: v for k, v in kwargs.items() if isinstance(v, bool)}
+    kwargs = {k: v for k, v in kwargs.items() if not isinstance(v, bool)}
+
+    if hasattr(_model, "old_forward"):
+        input_names = (
+            signature(_model.old_forward).bind(*args, **kwargs).arguments.keys()
+        )
+    else:
+        input_names = signature(_model.forward).bind(*args, **kwargs).arguments.keys()
+    dummy_inputs = {}
+    i = 0
+    for k in input_names:
+        if k not in kwargs:
+            dummy_inputs[k] = args[i]
+            i += 1
+        else:
+            dummy_inputs[k] = kwargs[k]
+    return input_names, bool_inputs, dummy_inputs
+
+
+def substitute_transform(
+    root: torch.nn.Module,
+    concrete_args: Optional[Dict[str, Any]] = None,
+    input_names: Optional[List[str]] = None,
+    dummy_inputs: Optional[Dict[str, Any]] = None,
+    additional_mappings: Optional[Dict[str, Any]] = None,
+):
+    """
+    A function that transforms the model by substituting torch.nn.modules and activation functions to dmx.nn.modules.
+
+    Args:
+        root (torch.nn.Module): model/module to transform
+        concrete_args (Dict[str,Any], optional): concrete arguments to be used for tracer. Defaults to None.
+        hf (bool, optional): True if root is a huggingface model. Defaults to False
+
+    Returns:
+        transformed model
+    """
+    mod_type = type(root).__module__ + "." + type(root).__name__
+    if mod_type in dmx_aware_mapping:
+        transformed = dmx_aware_mapping[mod_type].from_raw(root)
+        return transformed
+
+    gm, tracer = hf_symbolic_trace(
+        root,
+        input_names,
+        concrete_args=concrete_args,
+        dummy_inputs=dummy_inputs,
+    )
+
+    gi = RecordInputInterpreter(gm)
+    gi.run(*list(dummy_inputs.values()))
+    transformer = DMXAwareTransformer(
+        gm,
+        tracer.node_name_to_scope,
+        root._gm if root.transformed else None,
+        gi.nodeInputs,
+    )
+    if additional_mappings:
+        for target, dmx_module in additional_mappings.items():
+            new_target = target.replace("torch.ops.", "")
+            transformer.add_dmx_aware_functional_mapping(new_target, dmx_module)
+    transformed = transformer.transform()
+
+    return transformed
+
+
+def qDq_transform(
+    root: torch.fx.GraphModule,
+):
+    """
+    A function that transforms the model by substituting CastTos with Q/dQ ops
+
+    Args:
+        root (torch.nn.Module): model/module to transform
+
+    Returns:
+        transformed model
+    """
+    # import compiler so q/dq ops are registered
+
+    transformer = QdQTransformer(root)
+    transformed = transformer.transform()
+    transformed.recompile()
+    return transformed
+
+
+make_compiler_graph = qDq_transform
+
+
+def cast_input_output_transform(
+    root: torch.nn.Module,
+    tracer: Union[QuantTracer, HFQuantTracer] = QuantTracer(),
+    concrete_args: Optional[Dict[str, Any]] = None,
+    cfg: Optional[str] = None,
+) -> nn.Module:
+    """
+    A function that transforms the module by adding additional ops, which includes:
+    - casting
+    - approximator
+    - sparsifier
+    An optional config file can be passed to specify the formats for the additional ops, dummy formats would be used otherwise.
+
+    Args:
+        root (torch.nn.Module): model/module to transform
+        tracer (Union[QuantTracer, HFQuantTracer], optional): tracer used for tracing the root. Defaults to QuantTracer.
+        concrete_args (Dict[str,Any], optional): concrete arguments to be used for tracer. Defaults to None.
+        cfg (Optional[str]): config file for setting the added ops formats. Defaults to None.
+
+    Returns:
+        transformed model
+    """
+    graph = tracer.trace(root, concrete_args)
+    name = (
+        root.__class__.__name__ if isinstance(root, torch.nn.Module) else root.__name__
+    )
+    gm = GraphModule(tracer.root, graph, name)
+    transformer = InputOutputTransformer(gm, tracer.node_name_to_scope, cfg)
+    transformed = transformer.transform()
+    transformed.scopeDict = transformer.scopeDict
+    return transformed
+
+
+def configure_transform(gm: torch.fx.GraphModule, scopeDict: dict, cfg: str):
+    """
+    A function that changes the format of the ops according to the cfg file
+    Note:
+        Configure_transform will only change existing ops and will not add any additional ops.
+        Hence it is recommened to pass in a cfg file for cast_input_output_transform to make sure all necessary ops are added.
+
+    Args:
+        gm (torch.fx.GraphModule): Graphmodule to apply changes on
+        scopeDict (dict): Dictionary that maps node name to scope
+        cfg (str): config file for setting the added ops formats.
+
+    Returns:
+        Graphmodule with updated formats
+    """
+    transformer = ConfigurationTransformer(gm, scopeDict, cfg)
+    transformer.transform()
+    return gm
---
+++ packages/dmx-compressor/dmx_compressor-0.1.10.tar.gz/dmx_compressor-0.1.10/tests/test_submod_forward.py
@@ -0,0 +1,96 @@
+import torch
+import pytest
+
+from dmx.compressor.modeling.model import DmxModel, DmxConfigRule
+from dmx.compressor.modeling import nn as dmxnn
+
+# Add parent directory to Python path
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+
+class Submod(torch.nn.Module):
+    def __init__(self, indim, hiddim, outdim) -> None:
+        super().__init__()
+        self.lin1 = torch.nn.Linear(indim, hiddim)
+        self.act = torch.nn.ReLU()
+        self.lin2 = torch.nn.Linear(hiddim, outdim)
+
+    def forward(self, x, y):
+        out = self.lin1(x + y)
+        out = self.act(out)
+        out = self.lin2(out)
+        return out
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sm1 = Submod(160, 6400, 6400)
+        self.act = torch.nn.GELU()
+        self.sm2 = Submod(6400, 12800, 10)
+
+    def forward(self, x):
+        out = self.sm1(x, x)
+        out = self.act(out)
+        out = self.sm2(out, out)
+        return out
+
+
+@pytest.fixture
+def setup_model():
+    model = DmxModel.from_torch(Model())
+    inp = torch.rand((1, 160))
+    out_full_0 = model(inp)
+    
+    # creating transformed submod forward
+    DmxModel.create_submod_transform_forward(model, "sm1")
+    DmxModel.create_submod_transform_forward(model, "sm2")
+    
+    return model, inp, out_full_0
+
+
+def test_unquantized(setup_model):
+    model, inp, _ = setup_model
+    out1 = model.sm1.transformed_forward(inp, inp)
+    out0 = model.sm1.forward(inp, inp)
+    # check sm1 output is same between original model and unquantized dmx model
+    assert torch.all(
+        out1 == out0
+    ), "unquantized submodule produced different output from original submodule, should be same"
+
+
+def test_quantized(setup_model):
+    model, inp, out_full_0 = setup_model
+    bfp16 = "BFP[8|8]{64}(SN)"
+    rules = (
+        DmxConfigRule(
+            module_types=(dmxnn.Linear,),
+            module_config=dict(
+                input_format=bfp16,
+                weight_format=bfp16,
+                bias_format=bfp16,
+                output_format=bfp16,
+            ),
+        ),
+    )
+    model.configure(None, *rules)
+    out1 = model.sm1.transformed_forward(inp, inp)
+    out0 = model.sm1.forward(inp, inp)
+    # check sm1 output is different between original model and quantized dmx model
+    assert torch.any(
+        out1 != out0
+    ), "quantized submodule produced same output as original submodule, should be different"
+    out_full = model(inp)
+    out_sub = model.sm1.transformed_forward(inp, inp)
+    out_sub = torch.nn.GELU()(out_sub)
+    out_sub = model.sm2.transformed_forward(out_sub, out_sub)
+    # check model output is same between running whole quantized model vs running quantized submodules step-by-step
+    assert torch.all(
+        out_full == out_sub
+    ), "quantized whole model produced different output from chained quantized submodules, should be same"
+    # check model output is different between dmx quantized model and original model
+    assert torch.any(
+        out_full != out_full_0
+    ), "quantized whole model produced same output as unquantized whole model, should be different"
