cbbe24c55c3c801fbcba480a343350a4b3433309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:28 +0000

    Add package notificationz-sdk-python/0.1.0/notificationz_sdk_python-0.1.0-py3-none-any.whl

---
+++ packages/notificationz-sdk-python/notificationz_sdk_python-0.1.0-py3-none-any.whl/notificationz_sdk_python/notification.py
@@ -0,0 +1,25 @@
+from enum import Enum
+import requests
+from notificationz_sdk_python.models import *
+
+class Notification:
+    def __init__(self, api_url):
+        self.api_url = api_url
+        self.api_telegram_url = api_url + "telegram"
+        self.api_email_url = api_url + "email"
+
+
+    def send(self, destination: notification_destination, address, text, tag = None, source = None):
+        # sends request to send notification container with details
+        # subprocess.run(["pkill", "openvpn"])
+        # print("killing vpn")
+        # sleep(10)
+        if destination == notification_destination.telegram:
+            data = {"username": address, "message": text, "tag": tag, "source": source}    
+            res = requests.post(self.api_telegram_url, json=data)
+            print(res)
+            return res
+
+        if destination == notification_destination.email:
+            pass
+    
