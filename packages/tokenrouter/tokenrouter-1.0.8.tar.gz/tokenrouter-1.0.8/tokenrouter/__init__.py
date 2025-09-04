35dce5da436ed38b9a48e91b535805e2af02deac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:05:23 +0000

    Add package tokenrouter/1.0.8/tokenrouter-1.0.8.tar.gz

---
+++ packages/tokenrouter/tokenrouter-1.0.8.tar.gz/tokenrouter-1.0.8/tokenrouter/__init__.py
@@ -0,0 +1,38 @@
+"""
+TokenRouter SDK - Intelligent LLM Routing Client
+
+A Python SDK for the TokenRouter API that provides intelligent routing
+to multiple LLM providers with cost optimization.
+"""
+
+from .client import TokenRouter, AsyncTokenRouter
+from .models import (
+    ChatCompletion,
+    ChatCompletionMessage,
+    ChatCompletionChoice,
+    Usage,
+)
+from .exceptions import (
+    TokenRouterError,
+    AuthenticationError,
+    RateLimitError,
+    InvalidRequestError,
+    APIConnectionError,
+    APIStatusError,
+)
+
+__version__ = "1.0.8"
+__all__ = [
+    "TokenRouter",
+    "AsyncTokenRouter",
+    "ChatCompletion",
+    "ChatCompletionMessage",
+    "ChatCompletionChoice",
+    "Usage",
+    "TokenRouterError",
+    "AuthenticationError",
+    "RateLimitError",
+    "InvalidRequestError",
+    "APIConnectionError",
+    "APIStatusError",
+]
