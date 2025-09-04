2542ca828a94576bfe66e903228eb15278068b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 04:04:14 +0000

    Add package fast-task-api/1.2.2/fast_task_api-1.2.2.tar.gz

---
+++ packages/fast-task-api/fast_task_api-1.2.2.tar.gz/fast_task_api-1.2.2/fast_task_api/__init__.py
@@ -0,0 +1,6 @@
+from fast_task_api.fast_task_api import FastTaskAPI
+from fast_task_api.core.job.job_progress import JobProgress
+from fast_task_api.core.job.job_result import FileModel, JobResult
+from media_toolkit import MediaFile, ImageFile, AudioFile, VideoFile, MediaList, MediaDict
+
+__all__ = ["FastTaskAPI", "JobProgress", "FileModel", "JobResult", "MediaFile", "ImageFile", "AudioFile", "VideoFile", "MediaList", "MediaDict"]
