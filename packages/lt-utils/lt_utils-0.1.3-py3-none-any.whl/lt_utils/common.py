a84db03884f34dd012612c1369f835d51fb0fe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:56 +0000

    Add package lt-utils/0.1.3/lt_utils-0.1.3-py3-none-any.whl

---
+++ packages/lt-utils/lt_utils-0.1.3-py3-none-any.whl/lt_utils/common.py
@@ -0,0 +1,33 @@
+__all__ = [
+    "Any",
+    "Type",
+    "Path",
+    "Dict",
+    "List",
+    "Union",
+    "Tuple",
+    "Number",
+    "Literal",
+    "Sequence",
+    "Optional",
+    "Callable",
+    "PathLike",
+    "TypeAlias",
+]
+
+from typing import (
+    Any,
+    Dict,
+    List,
+    Tuple,
+    Type,
+    Union,
+    Literal,
+    Sequence,
+    Optional,
+    Callable,
+    TypeAlias,
+)
+from numbers import Number
+from pathlib import Path
+from os import PathLike
