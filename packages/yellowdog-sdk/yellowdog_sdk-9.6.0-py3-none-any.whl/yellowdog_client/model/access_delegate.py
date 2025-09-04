fcd89d9331b682af113411cc5cd02ebee8fa4a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 16:04:50 +0000

    Add package yellowdog-sdk/9.6.0/yellowdog_sdk-9.6.0-py3-none-any.whl

---
+++ packages/yellowdog-sdk/yellowdog_sdk-9.6.0-py3-none-any.whl/yellowdog_client/model/access_delegate.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List, Optional
+
+from .identified import Identified
+from .permission import Permission
+
+
+@dataclass
+class AccessDelegate(Identified):
+    """Represents a user or application delegate within the YellowDog Platform."""
+    id: Optional[str] = field(default=None, init=False)
+    creator: Optional[str] = None
+    subject: Optional[str] = None
+    description: Optional[str] = None
+    createdTime: Optional[datetime] = None
+    requiredPermissions: Optional[List[Permission]] = None
