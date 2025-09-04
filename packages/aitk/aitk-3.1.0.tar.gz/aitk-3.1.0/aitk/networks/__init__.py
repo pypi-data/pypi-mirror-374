910a74e8b1f71a3dd774d601bd95d803a72a6340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 06:04:16 +0000

    Add package aitk/3.1.0/aitk-3.1.0.tar.gz

---
+++ packages/aitk/aitk-3.1.0.tar.gz/aitk-3.1.0/aitk/networks/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# ******************************************************
+# aitk.networks: Keras model wrapper with visualizations
+#
+# Copyright (c) 2021 Douglas S. Blank
+#
+# https://github.com/ArtificialIntelligenceToolkit/aitk.networks
+#
+# ******************************************************
+
+from tensorflow.keras.layers import *
+
+from .network import Network, SequentialNetwork, SimpleNetwork  # noqa: F401
