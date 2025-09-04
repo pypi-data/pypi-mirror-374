024b69cefe88218d50fd7db2863670bc5fa444cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:05:27 +0000

    Add package omlish/0.0.0.dev422/omlish-0.0.0.dev422.tar.gz

---
+++ packages/omlish/omlish-0.0.0.dev422.tar.gz/omlish-0.0.0.dev422/omlish/logs/callers.py
@@ -0,0 +1,67 @@
+# ruff: noqa: UP006 UP007 UP045
+# @omlish-lite
+import io
+import os.path
+import sys
+import traceback
+import types
+import typing as ta
+
+
+##
+
+
+class LoggingCaller(ta.NamedTuple):
+    filename: str
+    lineno: int
+    func: str
+    sinfo: ta.Optional[str]
+
+    @classmethod
+    def is_internal_frame(cls, frame: types.FrameType) -> bool:
+        filename = os.path.normcase(frame.f_code.co_filename)
+
+        # https://github.com/python/cpython/commit/5ca6d7469be53960843df39bb900e9c3359f127f
+        if 'importlib' in filename and '_bootstrap' in filename:
+            return True
+
+        return False
+
+    @classmethod
+    def find_frame(cls, ofs: int = 0) -> types.FrameType:
+        f: ta.Optional[types.FrameType] = sys._getframe(2 + ofs)  # noqa
+
+        while f is not None and hasattr(f, 'f_code'):
+            if f.f_code.co_filename != __file__:
+                return f
+
+            f = f.f_back
+
+        raise RuntimeError
+
+    @classmethod
+    def find(
+            cls,
+            ofs: int = 0,
+            *,
+            stack_info: bool = False,
+    ) -> 'LoggingCaller':
+        f = cls.find_frame(ofs + 1)
+        # TODO: ('(unknown file)', 0, '(unknown function)', None) ?
+
+        sinfo = None
+        if stack_info:
+            sio = io.StringIO()
+            sio.write('Stack (most recent call last):\n')
+            traceback.print_stack(f, file=sio)
+            sinfo = sio.getvalue()
+            sio.close()
+            if sinfo[-1] == '\n':
+                sinfo = sinfo[:-1]
+
+        return cls(
+            f.f_code.co_filename,
+            f.f_lineno,
+            f.f_code.co_name,
+            sinfo,
+        )
