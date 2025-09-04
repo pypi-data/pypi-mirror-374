141bbf7fc273de7a6906ade3f2f72101619082f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:05:02 +0000

    Add package langchain-kusto/0.0.1/langchain_kusto-0.0.1-py3-none-any.whl

---
+++ packages/langchain-kusto/langchain_kusto-0.0.1-py3-none-any.whl/langchain_kusto/__init__.py
@@ -0,0 +1,6 @@
+"""LangChain vector store implementation for Azure Data Explorer (Kusto)."""
+
+from langchain_kusto.vectorstore import KustoVectorStore
+
+__version__ = "0.0.1"
+__all__ = ["KustoVectorStore"]
