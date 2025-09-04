00c9db9c301620ec618ce4d2a2e705465945e7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 03:12:23 +0000

    Add package prune-norminette/2.0.0/prune_norminette-2.0.0.tar.gz

---
+++ packages/prune-norminette/prune_norminette-2.0.0.tar.gz/prune_norminette-2.0.0/prune_norminette/main.py
@@ -0,0 +1,16 @@
+from prune_norminette.utils.display_result import display_results
+from prune_norminette.utils.run_checks import run_checks
+from prune_norminette.utils.setup_django import initialize_django
+
+
+def main():
+    if not initialize_django():
+        return
+
+    errors = run_checks()
+
+    display_results(errors)
+
+
+if __name__ == "__main__":
+    main()
