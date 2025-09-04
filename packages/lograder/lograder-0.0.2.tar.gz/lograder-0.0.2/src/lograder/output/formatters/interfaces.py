8aefa30bc1ae84ffbad6feff8f48ca8a0a49dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 05:04:33 +0000

    Add package lograder/0.0.2/lograder-0.0.2.tar.gz

---
+++ packages/lograder/lograder-0.0.2.tar.gz/lograder-0.0.2/src/lograder/output/formatters/interfaces.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional, Sequence
+
+from ...dispatch.common.types import (
+    AssignmentMetadata,
+    BuilderOutput,
+    PreprocessorOutput,
+)
+from ...tests.test import ExecutableTestInterface
+from ...tests.test.analytics import (
+    CallgrindSummary,
+    ExecutionTimeSummary,
+    ValgrindLeakSummary,
+    ValgrindWarningSummary,
+)
+
+
+class MetadataFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, assignment_metadata: AssignmentMetadata) -> str:
+        pass
+
+
+class PreprocessorOutputFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, preprocessor_output: PreprocessorOutput) -> str:
+        pass
+
+
+class BuildOutputFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, build_output: BuilderOutput) -> str:
+        pass
+
+
+class RuntimeSummaryFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, test_cases: Sequence[ExecutableTestInterface]) -> str:
+        pass
+
+
+class ValgrindLeakSummaryFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, leak_summary: Optional[ValgrindLeakSummary]) -> str:
+        pass
+
+
+class ValgrindWarningSummaryFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, warning_summary: Optional[ValgrindWarningSummary]) -> str:
+        pass
+
+
+class ExecutionTimeSummaryFormatterInterface(ABC):
+    @abstractmethod
+    def format(
+        self,
+        callgrind_summary: Optional[List[CallgrindSummary]],
+        execution_time_summary: Optional[ExecutionTimeSummary],
+    ) -> str:
+        pass
+
+
+class ExecutableTestFormatterInterface(ABC):
+    @abstractmethod
+    def format(self, test_case: ExecutableTestInterface) -> str:
+        pass
