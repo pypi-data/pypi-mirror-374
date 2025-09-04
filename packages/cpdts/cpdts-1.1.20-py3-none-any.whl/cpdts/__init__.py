d339c6b9e3da453d420e4ec0b9e88fff6017909c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 15:07:50 +0000

    Add package cpdts/1.1.20/cpdts-1.1.20-py3-none-any.whl

---
+++ packages/cpdts/cpdts-1.1.20-py3-none-any.whl/cpdts/__init__.py
@@ -0,0 +1,18 @@
+
+
+import fire 
+from .fire_project import fire_create 
+
+
+
+class ENTRY(object):
+    """主入口类"""
+    
+    def fire(self,name) -> None:
+        fire_create(name)
+
+    
+def main() -> None:
+    """Main function to run the CLI."""
+    fire.Fire(ENTRY)
+
