a1429f30eac2d50697a2883c1ceaa103f3c6f9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-04 14:04:53 +0000

    Add package tosnativeclient/1.0.1/tosnativeclient-1.0.1.tar.gz

---
+++ packages/tosnativeclient/tosnativeclient-1.0.1.tar.gz/tosnativeclient-1.0.1/python/tosnativeclient/__init__.py
@@ -0,0 +1,21 @@
+from .tosnativeclient import TosClient, ListStream, ListObjectsResult, TosObject, TosError, TosException, TosRawClient, \
+    HeadObjectInput, HeadObjectOutput, GetObjectOutput, DeleteObjectInput, DeleteObjectOutput, GetObjectInput, \
+    PutObjectFromBufferInput, PutObjectFromFileInput, PutObjectOutput
+
+__all__ = ['TosClient',
+           'ListStream',
+           'ListObjectsResult',
+           'TosObject',
+           'TosError',
+           'TosException',
+           'TosRawClient',
+           'HeadObjectInput',
+           'HeadObjectOutput',
+           'DeleteObjectInput',
+           'DeleteObjectOutput',
+           'GetObjectInput',
+           'GetObjectOutput',
+           'PutObjectFromBufferInput',
+           'PutObjectFromFileInput',
+           'PutObjectOutput'
+           ]
