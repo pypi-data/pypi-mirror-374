af2dae4f27bf2e4fee0b8738f7e228e212188b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:05:37 +0000

    Add package genie-partner-sdk/1.0.11/genie_partner_sdk-1.0.11-py3-none-any.whl

---
+++ packages/genie-partner-sdk/genie_partner_sdk-1.0.11-py3-none-any.whl/genie_partner_sdk/auth.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from aiohttp import ClientSession, ClientResponse
+import logging
+
+
+class Auth(ABC):
+    """Class to make authenticated requests"""
+
+    def __init__(self, websession: ClientSession, host: str, access_token: str, api_key: str) -> None:
+        """Initialize the auth."""
+        self._logger = logging.getLogger(__name__)
+        self.websession = websession
+        self.host = host
+        self.access_token = access_token
+        self.api_key = api_key
+
+    @abstractmethod
+    async def async_get_access_token(self) -> str:
+        """Return a valid access token."""
+
+    async def request(self, method: str, path: str, **kwargs: Any) -> ClientResponse:
+        """Make a request."""
+        headers = kwargs.get("headers")
+
+        if headers is None:
+            headers = {}
+        else:
+            headers = dict(headers)
+
+        access_token = await self.async_get_access_token()
+        headers["authorization"] = f"Bearer {access_token}"
+        headers["x-api-key"] = self.api_key
+
+        return await self.websession.request(
+            method, f"{self.host}/{path}", **kwargs, headers=headers,
+        )
---
+++ packages/genie-partner-sdk/genie_partner_sdk-1.0.11-py3-none-any.whl/genie_partner_sdk/model.py
@@ -0,0 +1,27 @@
+"""Models for Aladdin connect cover platform."""
+
+from typing import TypedDict
+
+
+class GarageDoorData(TypedDict):
+    """Aladdin door data"""
+
+    device_id: str
+    door_number: int
+    name: str
+    status: str
+    link_status: str
+    battery_level: int
+
+
+class GarageDoor:
+    """Aladdin Garage Door Entity"""
+
+    def __init__(self, data: GarageDoorData) -> None:
+        self.device_id: str = data["device_id"]
+        self.door_number: int = data["door_number"]
+        self.unique_id: str = f"{self.device_id}-{self.door_number}"
+        self.name: str = data["name"]
+        self.status: str | None = data["status"]
+        self.link_status: str | None = data["link_status"]
+        self.battery_level: int | None = data["battery_level"]
