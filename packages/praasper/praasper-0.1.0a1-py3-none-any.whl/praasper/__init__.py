d7e65c8ce942a365bc6b8acf810735acb52c0db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:06:02 +0000

    Add package praasper/0.1.0a1/praasper-0.1.0a1-py3-none-any.whl

---
+++ packages/praasper/praasper-0.1.0a1-py3-none-any.whl/praasper/__init__.py
@@ -0,0 +1,42 @@
+try:
+    from .process import *
+except:
+    from process import *
+
+import os
+import whisper
+
+class init_model:
+    name = "large-v3-turbo"
+
+
+    def __init__(self):
+        available_models = whisper.available_models()
+        if self.name in available_models:
+            print(f"Choose Whisper model: {self.name}")
+        else:
+            raise ValueError(f"Model {self.name} is not in the available Whisper models. Available models are: {available_models}")
+
+
+    def annote(
+        self,
+        input_path: str
+    ):
+
+        fnames = [os.path.splitext(f)[0] for f in os.listdir(input_path) if f.endswith('.wav')]
+
+
+        for fname in fnames:
+            wav_path = os.path.join(input_path, fname + ".wav")
+            tg_path = wav_path.replace(".wav", "_whisper.TextGrid")
+            vad_path = wav_path.replace(".wav", "_VAD.TextGrid")
+
+            transcribe_wav_file(wav_path, vad=vad_path, model_name=self.name)
+            word_timestamp(wav_path, tg_path)
+
+
+if __name__ == "__main__":
+    model = init_model()
+    model.annote(
+        input_path=os.path.abspath("data")
+    )
