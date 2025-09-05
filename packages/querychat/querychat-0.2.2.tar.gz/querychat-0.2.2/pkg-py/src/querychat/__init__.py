ccaed9254513fbd2bdbc7d370ea85c4154ec3e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:06:15 +0000

    Add package querychat/0.2.2/querychat-0.2.2.tar.gz

---
+++ packages/querychat/querychat-0.2.2.tar.gz/querychat-0.2.2/pkg-py/src/querychat/__init__.py
@@ -0,0 +1,13 @@
+from querychat.querychat import (
+    init,
+    sidebar,
+    system_prompt,
+)
+from querychat.querychat import (
+    mod_server as server,
+)
+from querychat.querychat import (
+    mod_ui as ui,
+)
+
+__all__ = ["init", "server", "sidebar", "system_prompt", "ui"]
