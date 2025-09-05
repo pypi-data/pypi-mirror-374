82c115de970dbb10e5eb9b2f639fbcaa84c71771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:41 +0000

    Add package mc-netease-sdk-nyrev/1.0.4/mc-netease-sdk-nyrev-1.0.4.tar.gz

---
+++ packages/mc-netease-sdk-nyrev/mc-netease-sdk-nyrev-1.0.4.tar.gz/mc-netease-sdk-nyrev-1.0.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mc-netease-sdk-nyrev",
+    version="1.0.4",
+    description="网易我的世界ModSDK补全库修正版 by Nuoyan",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nuoyan",
+    author_email="1279735247@qq.com",
+    url="https://github.com/charminglee/mc-netease-sdk-nyrev",
+    license="MIT",
+    packages=find_packages(where="libs"),
+    package_dir={"": "libs"},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7, <4",
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
