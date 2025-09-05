bd82a87077ecf1c0988662c54b530415a53f8156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:05:20 +0000

    Add package perfect-hash/0.5.1/perfect_hash-0.5.1.tar.gz

---
+++ packages/perfect-hash/perfect_hash-0.5.1.tar.gz/perfect_hash-0.5.1/setup.py
@@ -0,0 +1,42 @@
+import re
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+kwds = {}
+kwds['long_description'] = open('README.rst').read()
+
+# Read version from perfect_hash/__init__.py
+pat = re.compile(r'__version__\s*=\s*(\S+)', re.M)
+data = open('perfect_hash.py').read()
+kwds['version'] = eval(pat.search(data).group(1))
+
+setup(
+    name = "perfect-hash",
+    author = "Ilan Schnell",
+    author_email = "ilanschnell@gmail.com",
+    url = "https://github.com/ilanschnell/perfect-hash",
+    license = "BSD",
+    classifiers = [
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Topic :: Utilities",
+    ],
+    description = "creating perfect minimal hash function",
+    py_modules = ["perfect_hash"],
+    scripts = ['perfect-hash'],
+    **kwds
+)
