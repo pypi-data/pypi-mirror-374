cff1abd7fab774d878f8a5da17303ec7f15d38cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:25 +0000

    Add package code-review-plus/0.2.1/code_review_plus-0.2.1-py3-none-any.whl

---
+++ packages/code-review-plus/code_review_plus-0.2.1-py3-none-any.whl/code_review/review/main.py
@@ -0,0 +1,53 @@
+import json
+from pathlib import Path
+
+import click
+
+from code_review.adapters.generics import parse_for_ticket
+from code_review.cli import cli
+from code_review.dependencies.pip.handlers import requirements_updated
+from code_review.git.adapters import is_rebased
+from code_review.git.handlers import _get_unmerged_branches, display_branches
+from code_review.handlers import ch_dir
+from code_review.review.adapters import build_code_review_schema
+from code_review.review.handlers import display_review, write_review_to_file
+from code_review.settings import OUTPUT_FOLDER, CLI_CONSOLE
+
+
+@cli.group()
+def review() -> None:
+    """Tools for code review."""
+    pass
+
+@review.command()
+@click.option("--folder", "-f", type=Path, help="Path to the git repository", default=None)
+def make(folder: Path) -> None:
+    """List branches in the specified Git repository."""
+    ch_dir(folder)
+    unmerged_branches = _get_unmerged_branches("master")
+    if not unmerged_branches:
+        click.echo("No unmerged branches found.")
+        return
+    display_branches(unmerged_branches)
+    branch_num = click.prompt("Select a branch by number", type=int)
+    selected_branch = unmerged_branches[branch_num - 1]
+    click.echo(f"You selected branch: {selected_branch.name}")
+    code_review_schema = build_code_review_schema(folder, selected_branch.name)
+    ticket_number = parse_for_ticket(selected_branch.name)
+
+    ticket = ticket_number or click.prompt("Select a ticket by number", type=str)
+
+    code_review_schema.ticket = ticket
+
+    code_review_schema.is_rebased = is_rebased(code_review_schema.target_branch.name, "develop")
+
+    display_review(code_review_schema)
+
+    updated = requirements_updated(folder)
+    if updated:
+        CLI_CONSOLE.print("[green]Updated packages:[/green]")
+        for pkg in updated:
+            CLI_CONSOLE.print(f"- {pkg['library']}: -> {pkg['file'].name}")
+
+    new_file, backup_file = write_review_to_file(review=code_review_schema, folder=OUTPUT_FOLDER)
+    CLI_CONSOLE.print("[bold blue]Code review written to:[/bold blue] " + str(new_file))
---
+++ packages/code-review-plus/code_review_plus-0.2.1-py3-none-any.whl/code_review/schemas.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+import logging
+logger = logging.getLogger(__name__)
+
+class SemanticVersion(BaseModel):
+    """Schema for semantic versioning."""
+
+    major: int
+    minor: int
+    patch: int
+    source: Path
+
+    def __str__(self):
+        return f"{self.major}.{self.minor}.{self.patch}"
+
+    @classmethod
+    def parse_version(cls, version: str, file_path: Path) -> "SemanticVersion":
+        """Parse a version string into a SemanticVersion object."""
+        logger.debug("Parsing version '%s' from file '%s'", version, file_path)
+        parts = version.split(".")
+
+        if len(parts) != 3:
+            raise ValueError(f"Invalid version format: {version}")
+        try:
+            major, minor, patch = map(int, parts)
+        except ValueError as e:
+            major, minor, patch = 0, 0, 0
+        return cls(major=major, minor=minor, patch=patch, source=file_path)
+
+
+class BranchSchema(BaseModel):
+    """Schema for branch information."""
+
+    name: str
+    author: str
+    email: str
+    hash: str
+    date: datetime | None = None
+    linting_errors: int = Field(default=-1, description="Number of linting errors found by ruff. -1 means not checked")
+    min_coverage: float | None = Field(default=None, description="Minimum coverage based on the Makefile")
+    version: SemanticVersion | None = Field(default=None, description="Semantic version from the version file")
+    changelog_versions: list[SemanticVersion] = Field(
+        default_factory=list, description="List of last 5 semantic versions found in the changelog"
+    )
+
+    def __lt__(self, other):
+        if not isinstance(other, BranchSchema):
+            return NotImplemented
+
+        # Handle cases where dates are None
+        self_date = self.date if self.date is not None else datetime.min
+        other_date = other.date if other.date is not None else datetime.min
+
+        return self_date < other_date
