8119cb3dcab6eccdf01690b9891cdade3c3eaa40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 18:13:20 +0000

    Add package multiolib/2.7.0.dev20250905/multiolib-2.7.0.dev20250905-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/multiolib/multiolib-2.7.0.dev20250905-cp311-cp311-macosx_13_0_arm64.whl/multiolib/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '2.7.0.dev20250905'
+__commit_hash__ = 'b6dc4cde45a9ae2e76978f2d56c2e90df9dc72be'
+findlibs_dependencies = ["eckitlib", "eccodeslib", "metkitlib", "atlaslib_ecmwf", "fdb5lib", "mirlib", "fckitlib"]
