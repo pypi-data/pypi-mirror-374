cec60f29352052338b8f170f131a936337daad84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 14:06:02 +0000

    Add package hassette/0.2.1/hassette-0.2.1-py3-none-any.whl

---
+++ packages/hassette/hassette-0.2.1-py3-none-any.whl/hassette/models/states/automation.py
@@ -0,0 +1,18 @@
+from typing import Literal
+
+from pydantic import Field
+
+from .base import AttributesBase, StringBaseState
+
+
+class AutomationState(StringBaseState):
+    class Attributes(AttributesBase):
+        id: str | None = Field(default=None)
+        last_triggered: str | None = Field(default=None)
+        mode: str | None = Field(default=None)
+        current: int | float | None = Field(default=None)
+        max: int | float | None = Field(default=None)
+
+    domain: Literal["automation"]
+
+    attributes: Attributes
