280aea7319cba5c16c34e249be5fc748bb622317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:05:37 +0000

    Add package phoebe/2.4.21/phoebe-2.4.21-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/phoebe/phoebe-2.4.21-cp310-cp310-macosx_11_0_arm64.whl/phoebe/dependencies/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+if os.getenv('PHOEBE_ENABLE_PLOTTING', 'TRUE').upper() == 'TRUE':
+    try:
+        from . import autofig
+    except ImportError as e:
+        print("autofig could not be imported with error: {}.  Plotting will be disabled.".format(e))
+
+from . import nparray
+from . import distl
+from . import crimpl
+from . import unitsiau2015
