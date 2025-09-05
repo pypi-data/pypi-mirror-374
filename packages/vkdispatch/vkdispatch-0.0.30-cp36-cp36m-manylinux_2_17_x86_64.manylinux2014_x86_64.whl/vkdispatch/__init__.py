f797ca1bd4046bf6da2b07007008ca61bdffe21c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 15:08:24 +0000

    Add package vkdispatch/0.0.30/vkdispatch-0.0.30-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/vkdispatch/vkdispatch-0.0.30-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/vkdispatch/__init__.py
@@ -0,0 +1,76 @@
+from .base.errors import check_for_errors
+from .base.errors import check_for_compute_stage_errors
+
+from .base.init import DeviceInfo
+from .base.init import LogLevel
+from .base.init import get_devices
+from .base.init import initialize
+from .base.init import is_initialized
+from .base.init import log, log_error, log_warning, log_info, log_verbose, set_log_level
+
+from .base.dtype import dtype
+from .base.dtype import float32, int32, uint32, complex64
+from .base.dtype import vec2, vec3, vec4, ivec2, ivec3, ivec4, uvec2, uvec3, uvec4
+from .base.dtype import mat2, mat4
+from .base.dtype import is_scalar, is_complex, is_vector, is_matrix, is_dtype
+from .base.dtype import to_numpy_dtype, from_numpy_dtype, to_vector
+
+from .base.context import get_context, queue_wait_idle
+from .base.context import get_context_handle
+from .base.context import make_context, select_queue_families
+from .base.context import is_context_initialized
+
+from .base.buffer import asbuffer
+from .base.buffer import Buffer
+from .base.buffer import asrfftbuffer
+from .base.buffer import RFFTBuffer
+
+from .base.image import image_format
+from .base.image import image_type
+from .base.image import image_view_type
+from .base.image import Image
+from .base.image import Image1D
+from .base.image import Image2D
+from .base.image import Image2DArray
+from .base.image import Image3D
+from .base.image import Sampler
+from .base.image import Filter
+from .base.image import AddressMode
+from .base.image import BorderColor
+
+from .base.compute_plan import ComputePlan
+
+from .base.descriptor_set import DescriptorSet
+
+from .base.command_list import CommandList
+
+from .execution_pipeline.buffer_builder import BufferUsage, BufferedStructEntry, BufferBuilder
+
+from .execution_pipeline.command_graph import CommandGraph, BufferBindInfo, ImageBindInfo
+from .execution_pipeline.command_graph import global_graph, set_global_graph, default_graph
+
+from .shader_generation.signature import ShaderArgumentType
+from .shader_generation.signature import ShaderArgument
+from .shader_generation.signature import ShaderSignature
+
+from .shader_generation.shader_object import ShaderObject
+from .shader_generation.shader_object import ExectionBounds
+from .shader_generation.shader_object import LaunchParametersHolder
+
+from .shader_generation.mapping_shader import map, map_registers, MappingFunction
+
+from .shader_generation.reduction_operations import ReductionOperation, SubgroupAdd, SubgroupMul, SubgroupMin
+from .shader_generation.reduction_operations import SubgroupMax, SubgroupAnd, SubgroupOr, SubgroupXor
+
+from .shader_generation.reduction_stage import make_reduction_stage, ReductionParams
+
+from .shader_generation.reduction_object import ReductionObject
+
+from .shader_generation.decorators import shader, reduce, map_reduce
+
+import vkdispatch.vkfft as vkfft
+import vkdispatch.fft as fft
+
+import vkdispatch.fft as fft
+
+__version__ = "0.0.30"
