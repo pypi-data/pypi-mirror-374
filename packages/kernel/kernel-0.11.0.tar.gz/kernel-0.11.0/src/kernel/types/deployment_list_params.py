84afa114f0de94dd9a5c265a61d3f97bbf7ff238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:08 +0000

    Add package kernel/0.11.0/kernel-0.11.0.tar.gz

---
+++ packages/kernel/kernel-0.11.0.tar.gz/kernel-0.11.0/src/kernel/types/deployment_list_params.py
@@ -0,0 +1,12 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import TypedDict
+
+__all__ = ["DeploymentListParams"]
+
+
+class DeploymentListParams(TypedDict, total=False):
+    app_name: str
+    """Filter results by application name."""
---
+++ packages/kernel/kernel-0.11.0.tar.gz/kernel-0.11.0/src/kernel/types/deployment_list_response.py
@@ -0,0 +1,38 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Dict, List, Optional
+from datetime import datetime
+from typing_extensions import Literal, TypeAlias
+
+from .._models import BaseModel
+
+__all__ = ["DeploymentListResponse", "DeploymentListResponseItem"]
+
+
+class DeploymentListResponseItem(BaseModel):
+    id: str
+    """Unique identifier for the deployment"""
+
+    created_at: datetime
+    """Timestamp when the deployment was created"""
+
+    region: Literal["aws.us-east-1a"]
+    """Deployment region code"""
+
+    status: Literal["queued", "in_progress", "running", "failed", "stopped"]
+    """Current status of the deployment"""
+
+    entrypoint_rel_path: Optional[str] = None
+    """Relative path to the application entrypoint"""
+
+    env_vars: Optional[Dict[str, str]] = None
+    """Environment variables configured for this deployment"""
+
+    status_reason: Optional[str] = None
+    """Status reason"""
+
+    updated_at: Optional[datetime] = None
+    """Timestamp when the deployment was last updated"""
+
+
+DeploymentListResponse: TypeAlias = List[DeploymentListResponseItem]
