56775e3e0bca593bd2dd7b8398817dd0271d7377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:36 +0000

    Add package smithery/0.1.14/smithery-0.1.14-py3-none-any.whl

---
+++ packages/smithery/smithery-0.1.14-py3-none-any.whl/smithery/__init__.py
@@ -0,0 +1,18 @@
+"""
+Smithery Python SDK
+==================
+
+SDK for using Smithery in Python.
+"""
+
+from .server.fastmcp_patch import (
+    from_fastmcp,
+)
+from .utils.url import create_smithery_url
+
+__version__ = "0.1.4"
+
+__all__ = [
+    "create_smithery_url",
+    "from_fastmcp",
+]
