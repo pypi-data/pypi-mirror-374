060027d32d2322023e71c0ce4ce885d1b83e938f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:06:10 +0000

    Add package music-voicegen/0.1.0/music_voicegen-0.1.0-py3-none-any.whl

---
+++ packages/music-voicegen/music_voicegen-0.1.0-py3-none-any.whl/music_voicegen/__init__.py
@@ -0,0 +1,5 @@
+# The root of the entire project
+from music_voicegen.music_voicegen import MusicVoiceGen
+
+# This is the version number of your code. This will change each new release.
+__version__ = "0.1.0"
