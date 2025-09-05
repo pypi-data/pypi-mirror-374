3990b02a36c1be306afbc5ab70c216e583363cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:03 +0000

    Add package cfgtools/0.0.8/cfgtools-0.0.8.tar.gz

---
+++ packages/cfgtools/cfgtools-0.0.8.tar.gz/cfgtools-0.0.8/README.md
@@ -0,0 +1,90 @@
+# cfgtools
+Provides tools for managing config files.
+
+## Installation
+```sh
+$ pip install cfgtools
+```
+
+## Requirements
+```txt
+pyyaml
+lazyr
+Faker
+htmlmaster
+toml
+```
+
+## Usage
+### Save to a config file
+
+```py
+>>> import cfgtools as cfg
+>>> f = cfg.config({"foo": "bar", "this": ["is", "an", "example"]})
+>>> f.save("test.cfg", "yaml") # or: f.to_yaml("test.cfg")
+```
+If not specifeid, the format of the file will be automatically detected according to the file suffix. Valid formats include `ini`, `json`, `yaml`, `pickle`, `toml`, etc. For example:
+```py
+>>> f.save("test.yaml") # a yaml file is created
+>>> f.save("test.pkl") # a pickle file is created
+>>> f.save("unspecified.cfg") # by default a json file is created
+```
+
+### Read from a config file
+```py
+>>> cfg.read("test.cfg")
+cfgtools.config({'foo': 'bar', 'this': ['is', 'an', 'example']})
+```
+The encoding and format of the file will be automatically detected if not specified.
+
+### Modify configs
+```py
+>>> f["foo"] = None
+>>> f["that"] = {"is": ["also", "an", "example"]}
+>>> f
+cfgtools.config({
+    'foo': None, 'this': ['is', 'an', 'example'],
+    'that': {'is': ['also', 'an', 'example']},
+})
+```
+If user wants to check the changed items, run:
+```py
+>>> f.view_change()
+```
+
+## See Also
+### Github repository
+* https://github.com/Chitaoji/cfgtools/
+
+### PyPI project
+* https://pypi.org/project/cfgtools/
+
+## License
+This project falls under the BSD 3-Clause License.
+
+## History
+### v0.0.8
+* Add a simple cli command `cfg [OPTIONS] FILENAME`.
+* Rename `ConfigIOWrapper.safematch()` to `ConfigIOWrapper.adapt()`.
+
+### v0.0.7
+* New method for `BasicWrapper`: `*.asstr()`, `*.asint()`, `*.asfloat()`, `*.asbool()`, `*.asnone()`.
+
+### v0.0.6
+* Beautified the output of `ConfigIOWrapper.view_change()` through `_repr_mimebundle_()`.
+
+### v0.0.5
+* Added support for .toml files.
+* New method for `ConfigIOWrapper`: `*.view_change()`, `*.asdict()`, `*.aslist()`.
+
+### v0.0.4
+* Fixed a bug in path resolution.
+
+### v0.0.3
+* Added reliance on `htmlmaster`.
+
+### v0.0.2
+* New method `ConfigIOWrapper.safematch()`.
+
+### v0.0.1
+* Initial release.
---
+++ packages/cfgtools/cfgtools-0.0.8.tar.gz/cfgtools-0.0.8/src/cfgtools/reader.py
@@ -0,0 +1,342 @@
+"""
+Contains methods for reading config files: read_yaml(), read_pickle(), etc.
+
+NOTE: this module is private. All functions and objects are available in the main
+`cfgtools` namespace - use that instead.
+
+"""
+
+import json
+import pickle
+from configparser import (
+    ConfigParser,
+    DuplicateSectionError,
+    MissingSectionHeaderError,
+    ParsingError,
+)
+from pathlib import Path
+from typing import TYPE_CHECKING, Callable
+
+import toml
+import yaml
+from yaml import MarkedYAMLError
+from yaml.reader import ReaderError
+
+from .iowrapper import FORMAT_MAPPING, ConfigIOWrapper
+from .saver import FileFormatError
+
+if TYPE_CHECKING:
+    from ._typing import ConfigFileFormat, UnwrappedDataObj
+
+__all__ = [
+    "detect_encoding",
+    "read_yaml",
+    "read_pickle",
+    "read_json",
+    "read_ini",
+    "read_toml",
+    "read_text",
+    "read_bytes",
+]
+
+
+def detect_encoding(path: str | Path) -> str:
+    """
+    Detect the encoding of a file.
+
+    Parameters
+    ----------
+    path : str | Path
+        File path.
+
+    Returns
+    -------
+    str
+        The name of the encoding used to encode the file.
+
+    """
+    with open(path, "rb") as f:
+        test_line = f.readline()
+    return json.detect_encoding(test_line)
+
+
+def read_yaml(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read a yaml file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the yaml file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    with open(path, "r", encoding=encoding) as f:
+        cfg = yaml.safe_load(f)
+    return ConfigIOWrapper(cfg, "yaml", path=path, encoding=encoding)
+
+
+def read_pickle(path: str | Path, /) -> ConfigIOWrapper:
+    """
+    Read a pickle file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the pickle file.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    with open(path, "rb") as f:
+        cfg = pickle.load(f)
+    return ConfigIOWrapper(cfg, "pickle", path=path)
+
+
+def read_json(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read a json file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the json file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    with open(path, "r", encoding=encoding) as f:
+        cfg = json.load(f)
+    return ConfigIOWrapper(cfg, "json", path=path, encoding=encoding)
+
+
+def read_ini(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read an ini file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the ini file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    parser = ConfigParser()
+    parser.read(path, encoding=encoding)
+    obj = {
+        s: {o: _obj_restore(parser.get(s, o)) for o in parser.options(s)}
+        for s in parser.sections()
+    }
+    if len(obj) == 1 and "null" in obj:
+        obj = obj["null"]
+        if len(obj) == 1 and "null" in obj:
+            obj = obj["null"]
+    return ConfigIOWrapper(obj, "ini", path=path, encoding=encoding)
+
+
+def read_toml(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read a toml file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the toml file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    with open(path, "r", encoding=encoding) as f:
+        obj = toml.load(f)
+    if len(obj) == 1 and "null" in obj:
+        obj = obj["null"]
+    return ConfigIOWrapper(obj, "toml", path=path, encoding=encoding)
+
+
+def read_text(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read plain text from a text file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the text file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    cfg = Path(path).read_text(encoding=encoding)
+    return ConfigIOWrapper(cfg, "text", path=path, encoding=encoding)
+
+
+def read_bytes(path: str | Path, /, encoding: str | None = None) -> ConfigIOWrapper:
+    """
+    Read bytes from a bytes file.
+
+    Parameters
+    ----------
+    path : str | Path
+        Path of the bytes file.
+    encoding : str | None, optional
+        The name of the encoding used to decode or encode the file,
+        by default None.
+
+    Returns
+    --------
+    ConfigIOWrapper
+        A wrapper for reading and writing config files.
+
+    """
+    encoding = detect_encoding(path) if encoding is None else encoding
+    cfg = Path(path).read_bytes().decode(encoding)
+    return ConfigIOWrapper(cfg, "bytes", path=path, encoding=encoding)
+
+
+def _obj_restore(string: str) -> "UnwrappedDataObj":
+    try:
+        return json.loads(string)
+    except json.JSONDecodeError:
+        return string
+    except UnicodeDecodeError:
+        return string
+
+
+class ConfigReader:
+    """Config reader."""
+
+    reader_mapping: dict[str, Callable[..., ConfigIOWrapper]] = {
+        "pickle": read_pickle,
+        "ini": read_ini,
+        "json": read_json,
+        "yaml": read_yaml,
+        "toml": read_toml,
+        "text": read_text,
+        "bytes": read_bytes,
+    }
+
+    @classmethod
+    def read(
+        cls,
+        path: str | Path,
+        fileformat: "ConfigFileFormat | None" = None,
+        /,
+        encoding: str | None = None,
+    ) -> ConfigIOWrapper:
+        """Read from the config file."""
+        if fileformat is None:
+            return cls.autoread(path, encoding=encoding)
+        encoding = detect_encoding(path) if encoding is None else encoding
+        if fileformat not in FORMAT_MAPPING:
+            raise FileFormatError(f"unsupported config file format: {fileformat!r}")
+        return cls.reader_mapping[FORMAT_MAPPING[fileformat]](path, encoding=encoding)
+
+    @classmethod
+    def autoread(
+        cls, path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper:
+        """Read from the config file, automatically detecting the fileformat."""
+        encoding = detect_encoding(path) if encoding is None else encoding
+        try_methods: tuple[Callable[..., ConfigIOWrapper | None]] = (
+            cls.__try_pickle,
+            cls.__try_ini,
+            cls.__try_json,
+            cls.__try_yaml,
+            cls.__try_toml,
+            cls.__try_text,
+        )
+        for m in try_methods:
+            if (wrapper := m(path, encoding=encoding)) is not None:
+                return wrapper
+        raise FileFormatError(f"failed to read the config file: '{path}'")
+
+    @staticmethod
+    def __try_pickle(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        _ = encoding
+        try:
+            return read_pickle(path)
+        except pickle.UnpicklingError:
+            return None
+
+    @staticmethod
+    def __try_ini(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        try:
+            return read_ini(path, encoding=encoding)
+        except (MissingSectionHeaderError, ParsingError, DuplicateSectionError):
+            return None
+
+    @staticmethod
+    def __try_json(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        try:
+            return read_json(path, encoding=encoding)
+        except json.JSONDecodeError:
+            return None
+
+    @staticmethod
+    def __try_yaml(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        try:
+            return read_yaml(path, encoding=encoding)
+        except (ReaderError, MarkedYAMLError):
+            return None
+
+    @staticmethod
+    def __try_toml(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        try:
+            return read_toml(path, encoding=encoding)
+        except (ReaderError, MarkedYAMLError):
+            return None
+
+    @staticmethod
+    def __try_text(
+        path: str | Path, /, encoding: str | None = None
+    ) -> ConfigIOWrapper | None:
+        return read_text(path, encoding=encoding)
