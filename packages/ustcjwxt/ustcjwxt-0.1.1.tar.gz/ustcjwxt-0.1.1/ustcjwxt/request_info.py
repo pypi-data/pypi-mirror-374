862de484ffea95ca39d2c872aa0280930fe21821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 18:13:14 +0000

    Add package ustcjwxt/0.1.1/ustcjwxt-0.1.1.tar.gz

---
+++ packages/ustcjwxt/ustcjwxt-0.1.1.tar.gz/ustcjwxt-0.1.1/ustcjwxt/request_info.py
@@ -0,0 +1,23 @@
+base_cookie = {
+    'user_locale': 'zh',
+    'fine_remember_login': '-1',
+    'SVRNAME': 'teacher1',
+}
+
+header_urlencoded = {
+    'origin': 'https://jw.ustc.edu.cn',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent': 'Mozilla/5.0 (Linux NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.2024.114514 Safari/537.36'
+}
+
+header_json = {
+    'origin': 'https://jw.ustc.edu.cn',
+    'content-type': 'application/json; charset=UTF-8',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent': 'Mozilla/5.0 (Linux NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.2024.114514 Safari/537.36'
+}
+
+header_uaonly = {
+    'user-agent': 'Mozilla/5.0 (Linux NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.2024.114514 Safari/537.36'
+}
