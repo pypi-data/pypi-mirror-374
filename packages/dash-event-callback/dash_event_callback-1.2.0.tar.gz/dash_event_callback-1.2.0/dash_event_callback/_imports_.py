eefe4a5e4465988a462e5f61af7023e64a3320e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:04:38 +0000

    Add package dash-event-callback/1.2.0/dash_event_callback-1.2.0.tar.gz

---
+++ packages/dash-event-callback/dash_event_callback-1.2.0.tar.gz/dash_event_callback-1.2.0/dash_event_callback/_imports_.py
@@ -0,0 +1,9 @@
+from .SSE import SSE
+from ._event_callback import event_callback, stream_props, setup_event_callback
+
+__all__ = [
+    "SSE",
+    "event_callback",
+    "stream_props",
+    "setup_event_callback",
+]
