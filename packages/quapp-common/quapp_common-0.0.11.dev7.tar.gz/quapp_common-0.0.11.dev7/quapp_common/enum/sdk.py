3701c5338ac41687cbb9505677dda4a514b0c340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:05:29 +0000

    Add package quapp-common/0.0.11.dev7/quapp_common-0.0.11.dev7.tar.gz

---
+++ packages/quapp-common/quapp_common-0.0.11.dev7.tar.gz/quapp_common-0.0.11.dev7/quapp_common/enum/sdk.py
@@ -0,0 +1,23 @@
+"""
+    QApp Platform Project sdk.py Copyright © CITYNOW Co. Ltd. All rights reserved.
+"""
+
+from ..enum.base_enum import BaseEnum
+
+
+class Sdk(BaseEnum):
+    QISKIT = 'qiskit'
+    BRAKET = 'braket'
+    TYTAN = 'tytan'
+    D_WAVE_OCEAN = "d-wave ocean"
+    CUDA_QUANTUM = "cuda quantum"
+    PENNYLANE = "pennylane"
+    PYQUIL = "pyquil"
+
+    @staticmethod
+    def resolve(sdk: str):
+        for element in Sdk:
+            if element.value.__eq__(sdk):
+                return element
+
+        raise Exception("Sdk type is not supported!")
