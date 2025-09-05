0b46fe06f3c3eace5f7744e9934717c25829398b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:21 +0000

    Add package filoma/1.4.0/filoma-1.4.0.tar.gz

---
+++ packages/filoma/filoma-1.4.0.tar.gz/filoma-1.4.0/tests/images/test_image_profiler.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from filoma.images.image_profiler import ImageProfiler
+
+
+def test_probe_basic_stats():
+    arr = np.array([[1, 2, 3], [4, 5, 6]])
+    profiler = ImageProfiler()
+    report = profiler.probe(arr)
+    assert report["shape"] == (2, 3)
+    assert report["dtype"] == "int64"
+    assert report["min"] == 1
+    assert report["max"] == 6
+    assert report["mean"] == 3.5
+    assert report["nans"] == 0
+    assert report["infs"] == 0
+    assert report["unique"] == 6
+
+
+def test_probe_with_nans_and_infs():
+    arr = np.array([[np.nan, np.inf, 1], [2, 2, 3]])
+    profiler = ImageProfiler()
+    report = profiler.probe(arr)
+    assert report["nans"] == 1
+    assert report["infs"] == 1
+    assert report["unique"] >= 3
+
+
+def test_probe_empty_array():
+    arr = np.array([])
+    profiler = ImageProfiler()
+    report = profiler.probe(arr)
+    assert report["shape"] == (0,)
+    assert report["unique"] == 0
