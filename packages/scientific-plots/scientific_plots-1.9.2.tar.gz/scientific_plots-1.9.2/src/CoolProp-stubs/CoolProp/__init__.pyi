046bbfc762c0a82fd369d6cbe7905e5894d1c6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:05:24 +0000

    Add package scientific-plots/1.9.2/scientific_plots-1.9.2.tar.gz

---
+++ packages/scientific-plots/scientific_plots-1.9.2.tar.gz/scientific_plots-1.9.2/src/CoolProp-stubs/CoolProp/__init__.pyi
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Stub-File for CoolProp.
+"""
+
+def PropsSI(
+    out: str,
+    in1: str, val1: str | None = None,
+    in2: str | None = None, val2: str | None = None,
+    fluid: str | None = None) -> float: ...
