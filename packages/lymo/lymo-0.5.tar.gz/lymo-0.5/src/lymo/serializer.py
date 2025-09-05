75c9fd6a6f4b1ac4a19ff5d20469cf2d8e5a7e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:06:21 +0000

    Add package lymo/0.5/lymo-0.5.tar.gz

---
+++ packages/lymo/lymo-0.5.tar.gz/lymo-0.5/src/lymo/serializer.py
@@ -0,0 +1,28 @@
+import json
+from datetime import datetime, date
+from decimal import Decimal
+import uuid
+
+
+def default_serializer(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    if isinstance(obj, uuid.UUID):
+        return str(obj)
+    if isinstance(obj, Decimal):
+        return float(obj)
+    return str(obj)
+
+
+def dumps(data, *, indent=None, sort_keys=False):
+    return json.dumps(
+        data,
+        default=default_serializer,
+        indent=indent,
+        sort_keys=sort_keys,
+        ensure_ascii=False,
+    )
+
+
+def loads(s):
+    return json.loads(s)
