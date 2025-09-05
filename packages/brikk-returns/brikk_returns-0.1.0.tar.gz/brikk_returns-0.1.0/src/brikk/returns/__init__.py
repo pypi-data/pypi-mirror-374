e2f539895f169f1933af3b3e5a7048668fc94ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:06:15 +0000

    Add package brikk-returns/0.1.0/brikk_returns-0.1.0.tar.gz

---
+++ packages/brikk-returns/brikk_returns-0.1.0.tar.gz/brikk_returns-0.1.0/src/brikk/returns/__init__.py
@@ -0,0 +1,55 @@
+from brikk.returns._errors import (
+    OptionError,
+    OptionExpectError,
+    OptionUnwrapError,
+    ResultError,
+    ResultExpectError,
+    ResultUnwrapError,
+    ReturnsError,
+)
+from brikk.returns._option import (
+    Nothing,
+    Option,
+    Some,
+    is_none,
+    is_some,
+)
+from brikk.returns._result import (
+    Error,
+    Ok,
+    Result,
+    is_err,
+    is_ok,
+)
+from brikk.returns._utils import (
+    as_optional,
+    as_result,
+    optional_of,
+    result_of,
+    safe,
+)
+
+__all__ = [
+    "Error",
+    "Nothing",
+    "Ok",
+    "Option",
+    "OptionError",
+    "OptionExpectError",
+    "OptionUnwrapError",
+    "Result",
+    "ResultError",
+    "ResultExpectError",
+    "ResultUnwrapError",
+    "ReturnsError",
+    "Some",
+    "as_optional",
+    "as_result",
+    "is_err",
+    "is_none",
+    "is_ok",
+    "is_some",
+    "optional_of",
+    "result_of",
+    "safe",
+]
