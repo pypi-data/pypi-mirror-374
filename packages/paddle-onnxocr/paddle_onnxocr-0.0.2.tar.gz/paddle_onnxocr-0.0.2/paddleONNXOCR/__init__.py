5d0cfb146c2e9d788363a1cdd75ddb5f74a8fd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:05:22 +0000

    Add package paddle-onnxocr/0.0.2/paddle_onnxocr-0.0.2.tar.gz

---
+++ packages/paddle-onnxocr/paddle_onnxocr-0.0.2.tar.gz/paddle_onnxocr-0.0.2/paddleONNXOCR/__init__.py
@@ -0,0 +1,22 @@
+from paddleONNXOCR.predict.predict_system import PredictSystem
+from paddleONNXOCR.models_enum import *
+from paddleONNXOCR.predict.predict_cls import TextLineOrientationDetector
+from paddleONNXOCR.predict.predict_det import TextDetector
+from paddleONNXOCR.predict.predict_rec import OCRRecognizer
+from paddleONNXOCR.predict.predict_doc_cls import DocumentOrientationDetector
+from paddleONNXOCR.predict.predict_uvdoc import DocumentRectifier
+
+__author__ = 'wyy-holding'
+__version__ = '0.0.1'
+__all__ = [
+    "TextLineOrientationDetector",
+    "TextDetector",
+    "OCRRecognizer",
+    "DocumentOrientationDetector",
+    "DocumentRectifier",
+    "TextLineModels",
+    "DetModels",
+    "RecModels",
+    "TableModels",
+    "ImageModels"
+]
