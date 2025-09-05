7a41344c2d500ee790efcf0611690fb7d57010c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:06:36 +0000

    Add package codeuchain/1.0.0/codeuchain-1.0.0.tar.gz

---
+++ packages/codeuchain/codeuchain-1.0.0.tar.gz/codeuchain-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codeuchain",
+    version="1.0.0",
+    description="Agape-optimized Python implementation of CodeUChain",
+    author="CodeUChain Team",
+    packages=find_packages(),
+    install_requires=[],  # Pure Python - zero dependencies!
+)
