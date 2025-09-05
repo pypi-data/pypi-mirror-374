ebdc82e55a59638d1659c0d379f6765b75d29c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:20 +0000

    Add package pcp-serversdk-python/1.3.1/pcp_serversdk_python-1.3.1-py3-none-any.whl

---
+++ packages/pcp-serversdk-python/pcp_serversdk_python-1.3.1-py3-none-any.whl/pcp_serversdk_python/models/PaymentProduct840SpecificOutput.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from .Address import Address
+from .PaymentProduct840CustomerAccount import PaymentProduct840CustomerAccount
+
+
+@dataclass(kw_only=True)
+class PaymentProduct840SpecificOutput:
+    billingAddress: Optional[Address] = None
+    customerAccount: Optional[PaymentProduct840CustomerAccount] = None
+    shippingAddress: Optional[Address] = None
