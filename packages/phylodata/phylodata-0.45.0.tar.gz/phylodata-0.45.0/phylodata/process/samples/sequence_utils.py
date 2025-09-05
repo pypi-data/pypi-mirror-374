4b7a93fc8e0720e7dabdd630da2ddc14f6cfd177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:34 +0000

    Add package phylodata/0.45.0/phylodata-0.45.0.tar.gz

---
+++ packages/phylodata/phylodata-0.45.0.tar.gz/phylodata-0.45.0/phylodata/process/samples/sequence_utils.py
@@ -0,0 +1,70 @@
+def contains_sequence(sequence: str):
+    return any([ch for ch in sequence if ch.isalpha()])
+
+
+def contains_non_placeholder_characters(sequence: str):
+    return 0 < len(set(sequence) - PLACEHOLDER_CHARACTERS)
+
+
+def is_amino_acid_sequence(sequence: str):
+    characters = {c.lower() for c in sequence}
+    return characters.issubset(AA_CHARACTERS)
+
+
+def is_dna_sequence(sequence: str):
+    characters = {c.lower() for c in sequence}
+    return characters.issubset(DNA_CHARACTERS)
+
+
+def is_rna_sequence(sequence: str):
+    characters = {c.lower() for c in sequence}
+    return characters.issubset(RNA_CHARACTERS)
+
+
+PLACEHOLDER_CHARACTERS = {
+    "*",
+    "?",
+    "-",
+}
+DNA_CHARACTERS = {
+    "a",
+    "t",
+    "c",
+    "g",
+    "r",
+    "y",
+    "s",
+    "w",
+    "k",
+    "m",
+    "b",
+    "d",
+    "h",
+    "v",
+    "n",
+} | PLACEHOLDER_CHARACTERS
+RNA_CHARACTERS = (DNA_CHARACTERS - {"t"}) | {"u"}
+AA_CHARACTERS = {
+    "a",
+    "r",
+    "n",
+    "d",
+    "c",
+    "e",
+    "q",
+    "g",
+    "h",
+    "i",
+    "l",
+    "k",
+    "m",
+    "f",
+    "p",
+    "s",
+    "t",
+    "w",
+    "y",
+    "v",
+    "u",
+    "o",
+} | PLACEHOLDER_CHARACTERS
