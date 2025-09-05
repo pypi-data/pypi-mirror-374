1f274faf4c38bc5ac6521503e23979be2d05ecdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:41 +0000

    Add package wexample-helpers-yaml/0.0.67/wexample_helpers_yaml-0.0.67.tar.gz

---
+++ packages/wexample-helpers-yaml/wexample_helpers_yaml-0.0.67.tar.gz/wexample_helpers_yaml-0.0.67/src/wexample_helpers_yaml/helpers/yaml_helpers.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import yaml
+from wexample_helpers_yaml.const.types import YamlContent, YamlContentDict
+
+
+def yaml_read(file_path: str, default: YamlContent | None = None) -> YamlContent | None:
+    try:
+        with open(file_path) as f:
+            content = yaml.safe_load(f)
+
+            if isinstance(content, dict):
+                return content
+            else:
+                return default
+    except Exception:
+        return default
+
+
+def yaml_read_dict(
+    file_path: str, default: YamlContentDict | None = None
+) -> YamlContentDict:
+    content = yaml_read(file_path, default)
+
+    if not content:
+        return default or {}
+
+    assert isinstance(content, dict)
+
+    return content
+
+
+def yaml_write(file_path: str, content: YamlContent) -> None:
+    with open(file_path, "w") as f:
+        yaml.safe_dump(content, f)
