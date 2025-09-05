fe4b21311315a160d514ecb5d04d022d254f53e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:04:50 +0000

    Add package mujoco-usd-converter/0.1.0a2/mujoco_usd_converter-0.1.0a2-py3-none-any.whl

---
+++ packages/mujoco-usd-converter/mujoco_usd_converter-0.1.0a2-py3-none-any.whl/mujoco_usd_converter/__main__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+import sys
+
+from ._impl.cli import run
+
+sys.exit(run())
