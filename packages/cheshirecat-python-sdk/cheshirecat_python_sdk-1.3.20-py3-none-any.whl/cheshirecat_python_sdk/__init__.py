97f582619164088e1a29b4830c783810a01b8958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:11 +0000

    Add package cheshirecat-python-sdk/1.3.20/cheshirecat_python_sdk-1.3.20-py3-none-any.whl

---
+++ packages/cheshirecat-python-sdk/cheshirecat_python_sdk-1.3.20-py3-none-any.whl/cheshirecat_python_sdk/__init__.py
@@ -0,0 +1,8 @@
+from cheshirecat_python_sdk.client import CheshireCatClient
+from cheshirecat_python_sdk.clients import HttpClient, WSClient
+from cheshirecat_python_sdk.configuration import Configuration
+from cheshirecat_python_sdk.enums import Collection
+
+
+from cheshirecat_python_sdk.builders import *
+from cheshirecat_python_sdk.models.dtos import *
