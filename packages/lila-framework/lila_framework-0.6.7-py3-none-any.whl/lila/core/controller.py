4dcf47b91d42710f420cfa2c109086a660b4a871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:51 +0000

    Add package lila-framework/0.6.7/lila_framework-0.6.7-py3-none-any.whl

---
+++ packages/lila-framework/lila_framework-0.6.7-py3-none-any.whl/lila/core/controller.py
@@ -0,0 +1,26 @@
+from lila.core.request import Request
+from lila.core.responses import JSONResponse
+from pydantic import BaseModel, ValidationError
+
+
+class Controller:
+    async def parse_request(
+        self, request: Request, schema: BaseModel, response: JSONResponse = None
+    ):
+        try:
+            body = await request.json()
+            return schema(**body)
+        except ValidationError as e:
+            if response:
+                return response(
+                    status_code=400,
+                    content={"error": True, "details": e.errors()},
+                )
+            return False
+        except Exception as e:
+            if response:
+                return response(
+                    status_code=400,
+                    content={"error": True, "details": e.errors()},
+                )
+            return False
