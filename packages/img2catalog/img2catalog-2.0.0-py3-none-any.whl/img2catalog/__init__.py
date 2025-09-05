cdb9681b38b40a0c13842aa4d6a8df778099117e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:05:30 +0000

    Add package img2catalog/2.0.0/img2catalog-2.0.0-py3-none-any.whl

---
+++ packages/img2catalog/img2catalog-2.0.0-py3-none-any.whl/img2catalog/__init__.py
@@ -0,0 +1,4 @@
+from .__about__ import __version__
+from .logger import Logger
+
+log = Logger(__name__)
---
+++ packages/img2catalog/img2catalog-2.0.0-py3-none-any.whl/img2catalog/const.py
@@ -0,0 +1,20 @@
+"""This file contains constants used by img2catalog"""
+
+from pathlib import Path
+
+CONFIG_HOME_PATH = Path.home() / ".img2catalog" / "config.toml"
+
+XNAT_HOST_ENV = "XNAT_HOST"
+XNATPY_HOST_ENV = "XNATPY_HOST"
+XNAT_USER_ENV = "XNAT_USER"
+XNAT_PASS_ENV = "XNAT_PASS"
+
+FDP_SERVER_ENV = "IMG2CATALOG_FDP"
+FDP_USER_ENV = "IMG2CATALOG_FDP_USER"
+FDP_PASS_ENV = "IMG2CATALOG_FDP_PASS"
+
+SPARQL_ENV = "IMG2CATALOG_SPARQL_ENDPOINT"
+
+# Default setting
+REMOVE_OPTIN_KEYWORD = True
+INCLUDE_PRIVATE = False
