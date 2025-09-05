208a631c9fd9bdf11c05ebcbe93bd608930f2890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:05:51 +0000

    Add package gradio-dropdownplus/0.0.3/gradio_dropdownplus-0.0.3.tar.gz

---
+++ packages/gradio-dropdownplus/gradio_dropdownplus-0.0.3.tar.gz/gradio_dropdownplus-0.0.3/backend/gradio_dropdownplus/templates/component/index.js
@@ -0,0 +1,8 @@
+import { a as e, D as o, E as p, M as t, I as l } from "./Index-dsULWx_B.js";
+export {
+  e as BaseDropdown,
+  o as BaseDropdownOptions,
+  p as BaseExample,
+  t as BaseMultiselect,
+  l as default
+};
