e32cd4400cf98e40f2741dbe41321b19768366d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:05:43 +0000

    Add package ml-profiler/0.2.0/ml_profiler-0.2.0.tar.gz

---
+++ packages/ml-profiler/ml_profiler-0.2.0.tar.gz/ml_profiler-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="ml_profiler",
+    version="0.2.0",  # bump again
+    author="Divyanshu Chouhan",
+    description="Automated ML profiling and reporting tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # this is crucial
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "shap"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
