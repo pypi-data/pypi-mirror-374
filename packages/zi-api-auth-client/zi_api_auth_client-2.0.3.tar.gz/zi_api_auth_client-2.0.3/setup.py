4b971b16d72ef0860a30d3c6d60ac57eb15c7815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:05:23 +0000

    Add package zi-api-auth-client/2.0.3/zi_api_auth_client-2.0.3.tar.gz

---
+++ packages/zi-api-auth-client/zi_api_auth_client-2.0.3.tar.gz/zi_api_auth_client-2.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+# reading long description from file
+with open('DESCRIPTION.txt') as file:
+    long_description = file.read()
+
+# external packages requirements
+REQUIREMENTS = [
+    'PyJWT>=2.6.0,<3.0.0',
+    'requests>=2.32.4,<3.0.0',
+    'cryptography>=41.0.6'
+]
+
+CLASSIFIERS = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+# calling the setup function
+setuptools.setup(name='zi_api_auth_client',
+                 version='2.0.3',
+                 description='A library that supports username-password and PKI authentication methods for enterprise-api',
+                 long_description=long_description,
+                 url='https://github.com/Zoominfo/api-auth-python-client',
+                 author='Krishna Teja Dinavahi',
+                 author_email='krishnateja.dinavahi@zoominfo.com',
+                 license='MIT',
+                 classifiers=CLASSIFIERS,
+                 install_requires=REQUIREMENTS,
+                 keywords='ZoomInfo enterprise-api pki-auth',
+                 packages=setuptools.find_packages()
+                 )
