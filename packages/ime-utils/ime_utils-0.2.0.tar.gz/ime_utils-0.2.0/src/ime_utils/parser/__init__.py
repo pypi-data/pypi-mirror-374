e738c3af3acf8925b0189e0b9cc395a9f514660d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:09 +0000

    Add package ime-utils/0.2.0/ime_utils-0.2.0.tar.gz

---
+++ packages/ime-utils/ime_utils-0.2.0.tar.gz/ime_utils-0.2.0/src/ime_utils/parser/__init__.py
@@ -0,0 +1,13 @@
+from .sogou import SogouParser
+from .baidu import BaiduParser, BaiduMobileParser
+from .qq import QQParser, QQV1Parser
+from .huayu import HuayuParser
+
+__all__ = [
+    "SogouParser",
+    "BaiduParser",
+    "BaiduMobileParser",
+    "QQParser",
+    "QQV1Parser",
+    "HuayuParser",
+]
