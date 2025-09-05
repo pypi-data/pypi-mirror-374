81944b227f73013de4a7e823e5b53bf3e0b20063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 14:06:04 +0000

    Add package tensorlake/0.2.47/tensorlake-0.2.47.tar.gz

---
+++ packages/tensorlake/tensorlake-0.2.47.tar.gz/tensorlake-0.2.47/src/tensorlake/documentai/__init__.py
@@ -0,0 +1,69 @@
+"""
+TensorLake Document AI SDK
+"""
+
+from tensorlake.documentai.client import DocumentAI
+from tensorlake.documentai.models import (
+    Chunk,
+    ChunkingStrategy,
+    DatasetDataFilter,
+    EnrichmentOptions,
+    Figure,
+    MimeType,
+    ModelProvider,
+    OcrPipelineProvider,
+    Options,
+    Page,
+    PageClass,
+    PageClassConfig,
+    PageFragment,
+    PageFragmentType,
+    ParseResult,
+    ParseStatus,
+    ParsingOptions,
+    PartitionStrategy,
+    Region,
+    Signature,
+    StructuredData,
+    StructuredExtractionOptions,
+    Table,
+    TableCell,
+    TableOutputMode,
+    TableParsingFormat,
+    Text,
+)
+
+__all__ = [
+    "DocumentAI",
+    "Region",
+    # Enums
+    "ChunkingStrategy",
+    "MimeType",
+    "ModelProvider",
+    "OcrPipelineProvider",
+    "ParseStatus",
+    "PartitionStrategy",
+    "TableOutputMode",
+    "TableParsingFormat",
+    # Options
+    "EnrichmentOptions",
+    "Options",
+    "PageClassConfig",
+    "ParsingOptions",
+    "StructuredExtractionOptions",
+    # Results models
+    "PageClass",
+    "ParseResult",
+    "Chunk",
+    "Figure",
+    "Page",
+    "PageFragment",
+    "PageFragmentType",
+    "Signature",
+    "StructuredData",
+    "Table",
+    "TableCell",
+    "Text",
+    # Filters
+    "DatasetDataFilter",
+]
