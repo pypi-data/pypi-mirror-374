f80b69ac517c432191416237ec0e912272504df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:05:35 +0000

    Add package holmesgpt/0.14.0a0/holmesgpt-0.14.0a0.tar.gz

---
+++ packages/holmesgpt/holmesgpt-0.14.0a0.tar.gz/holmesgpt-0.14.0a0/holmes/clients/robusta_client.py
@@ -0,0 +1,42 @@
+import logging
+from typing import List, Optional
+import requests  # type: ignore
+from functools import cache
+from pydantic import BaseModel, ConfigDict
+from holmes.common.env_vars import ROBUSTA_API_ENDPOINT
+
+HOLMES_GET_INFO_URL = f"{ROBUSTA_API_ENDPOINT}/api/holmes/get_info"
+TIMEOUT = 0.5
+
+
+class HolmesInfo(BaseModel):
+    model_config = ConfigDict(extra="ignore")
+    latest_version: Optional[str] = None
+
+
+@cache
+def fetch_robusta_models(account_id, token) -> Optional[List[str]]:
+    try:
+        session_request = {"session_token": token, "account_id": account_id}
+        resp = requests.post(
+            f"{ROBUSTA_API_ENDPOINT}/api/llm/models",
+            json=session_request,
+            timeout=10,
+        )
+        resp.raise_for_status()
+        response_json = resp.json()
+        return response_json.get("models")
+    except Exception:
+        logging.exception("Failed to fetch robusta models")
+        return None
+
+
+@cache
+def fetch_holmes_info() -> Optional[HolmesInfo]:
+    try:
+        response = requests.get(HOLMES_GET_INFO_URL, timeout=TIMEOUT)
+        response.raise_for_status()
+        result = response.json()
+        return HolmesInfo(**result)
+    except Exception:
+        return None
