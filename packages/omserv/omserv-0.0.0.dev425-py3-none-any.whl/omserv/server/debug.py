e62344dad09954ac5faed457a043c7d688df94ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 15:06:08 +0000

    Add package omserv/0.0.0.dev425/omserv-0.0.0.dev425-py3-none-any.whl

---
+++ packages/omserv/omserv-0.0.0.dev425-py3-none-any.whl/omserv/server/debug.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+from omlish.diag import pydevd as pdu
+
+
+log = logging.getLogger(__name__)
+
+
+##
+
+
+def handle_error_debug(e: BaseException) -> None:
+    exc_info = sys.exc_info()
+    log.warning('Launching debugger')
+    pdu.debug_unhandled_exception(exc_info)
+    log.warning('Done debugging')
