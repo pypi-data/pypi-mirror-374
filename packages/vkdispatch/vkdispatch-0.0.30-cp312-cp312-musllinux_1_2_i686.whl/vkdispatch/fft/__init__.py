353360e0284a301aec7e876ab951970d46188bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 15:08:20 +0000

    Add package vkdispatch/0.0.30/vkdispatch-0.0.30-cp312-cp312-musllinux_1_2_i686.whl

---
+++ packages/vkdispatch/vkdispatch-0.0.30-cp312-cp312-musllinux_1_2_i686.whl/vkdispatch/fft/__init__.py
@@ -0,0 +1,13 @@
+from .config import FFTConfig, FFTParams
+
+from .resources import FFTResources, allocate_fft_resources
+
+from .shader import make_fft_shader, get_cache_info, cache_clear, print_cache_info
+from .shader import make_convolution_shader
+
+from .functions import fft, fft2, fft3, ifft, ifft2, ifft3
+from .functions import rfft, rfft2, rfft3, irfft, irfft2, irfft3
+
+from .functions import convolve, convolve2D, convolve2DR
+
+from .prime_utils import pad_dim
