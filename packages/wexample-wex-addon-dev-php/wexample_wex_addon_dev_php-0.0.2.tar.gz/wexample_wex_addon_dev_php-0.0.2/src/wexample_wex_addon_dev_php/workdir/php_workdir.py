c608f53ef5f9f7511b4431fbc05cf0edbcf9d13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:41 +0000

    Add package wexample-wex-addon-dev-php/0.0.2/wexample_wex_addon_dev_php-0.0.2.tar.gz

---
+++ packages/wexample-wex-addon-dev-php/wexample_wex_addon_dev_php-0.0.2.tar.gz/wexample_wex_addon_dev_php-0.0.2/src/wexample_wex_addon_dev_php/workdir/php_workdir.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from wexample_config.const.types import DictConfig
+from wexample_filestate.config_option.children_file_factory_config_option import (
+    ChildrenFileFactoryConfigOption,
+)
+from wexample_filestate.const.disk import DiskItemType
+from wexample_wex_core.workdir.code_base_workdir import CodeBaseWorkdir
+
+
+class PhpWorkdir(CodeBaseWorkdir):
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+        raw_value = super().prepare_value(raw_value=raw_value)
+        from wexample_helpers.helpers.array import array_dict_get_by
+
+        # Ensure a composer.json file exists for any PHP package project
+        children = raw_value["children"]
+
+        children.append(
+            {
+                "name": "composer.json",
+                "type": DiskItemType.FILE,
+                "should_exist": True,
+            }
+        )
+
+        # Add rules to .gitignore
+        array_dict_get_by("name", ".gitignore", children).setdefault(
+            "should_contain_lines", []
+        ).extend(
+            [
+                ".php-cs-fixer.cache",
+                ".scannerwork",
+                "/vendor",
+            ]
+        )
+
+        return raw_value
+
+    def get_dependencies(self) -> list[str]:
+        # TODO search in composer.json
+        return []
+
+    def _create_php_file_children_filter(self) -> ChildrenFileFactoryConfigOption:
+        from wexample_filestate.config_option.children_filter_config_option import (
+            ChildrenFilterConfigOption,
+        )
+        from wexample_filestate.const.disk import DiskItemType
+        from wexample_filestate_php.file.php_file import PhpFile
+
+        return ChildrenFilterConfigOption(
+            pattern={
+                "class": PhpFile,
+                "name_pattern": r"^.*\.php$",
+                "type": DiskItemType.FILE,
+                "python": [],
+            },
+            recursive=True,
+        )
