440eb003f52756ef93ecfcfe50e323144f40dafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:38 +0000

    Add package wexample-filestate-python/0.0.41/wexample_filestate_python-0.0.41.tar.gz

---
+++ packages/wexample-filestate-python/wexample_filestate_python-0.0.41.tar.gz/wexample_filestate_python-0.0.41/src/wexample_filestate_python/options_provider/python_options_provider.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from wexample_config.options_provider.abstract_options_provider import (
+    AbstractOptionsProvider,
+)
+from wexample_filestate_python.config_option.python_config_option import (
+    PythonConfigOption,
+)
+
+if TYPE_CHECKING:
+    from wexample_config.config_option.abstract_config_option import (
+        AbstractConfigOption,
+    )
+
+
+class PythonOptionsProvider(AbstractOptionsProvider):
+    @classmethod
+    def get_options(cls) -> list[type[AbstractConfigOption]]:
+        return [
+            PythonConfigOption,
+        ]
