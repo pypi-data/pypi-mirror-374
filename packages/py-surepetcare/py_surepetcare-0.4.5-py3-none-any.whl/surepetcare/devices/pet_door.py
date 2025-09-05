3d2daddc8bd58239c02d963033f5308c44aa538d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:43 +0000

    Add package py-surepetcare/0.4.5/py_surepetcare-0.4.5-py3-none-any.whl

---
+++ packages/py-surepetcare/py_surepetcare-0.4.5-py3-none-any.whl/surepetcare/devices/pet_door.py
@@ -0,0 +1,39 @@
+import logging
+
+from .device import BaseControl
+from .device import BaseStatus
+from .device import SurepyDevice
+from surepetcare.command import Command
+from surepetcare.const import API_ENDPOINT_PRODUCTION
+from surepetcare.enums import ProductId
+
+logger = logging.getLogger(__name__)
+
+
+class PetDoor(SurepyDevice):
+    def __init__(self, data: dict) -> None:
+        try:
+            super().__init__(data)
+            self.status: BaseStatus = BaseStatus(**data)
+            self.control: BaseControl = BaseControl(**data)
+        except Exception as e:
+            logger.warning("Error while storing data %s", data)
+            raise e
+
+    @property
+    def product(self) -> ProductId:
+        return ProductId.PET_DOOR
+
+    def refresh(self):
+        def parse(response):
+            if not response:
+                return self
+            self.status = BaseStatus(**{**self.status.model_dump(), **response["data"]})
+            self.control = BaseControl(**{**self.control.model_dump(), **response["data"]})
+            return self
+
+        return Command(
+            method="GET",
+            endpoint=f"{API_ENDPOINT_PRODUCTION}/device/{self.id}",
+            callback=parse,
+        )
