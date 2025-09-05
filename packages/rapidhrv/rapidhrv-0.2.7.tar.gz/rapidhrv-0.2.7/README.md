64e6856f36abffcadef2f8edce0cfeefbe8918a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:20 +0000

    Add package rapidhrv/0.2.7/rapidhrv-0.2.7.tar.gz

---
+++ packages/rapidhrv/rapidhrv-0.2.7.tar.gz/rapidhrv-0.2.7/README.md
@@ -0,0 +1,49 @@
+# RapidHRV
+
+RapidHRV is a data processing pipeline for the analysis and visualization of cardiac data.
+
+Please provide credit where appropriate:
+
+Kirk, P. A., Bryan, A. D., Garfinkel, S. N., & Robinson, O. J. (2022). RapidHRV: An open-source toolbox for extracting heart rate and heart rate variability. PeerJ, 10, e13147. https://doi.org/10.7717/peerj.13147
+
+This library is distributed under an 
+[MIT License](https://raw.githubusercontent.com/peterakirk/RapidHRV/main/LICENSE)
+
+## Installation
+
+```shell
+pip install rapidhrv
+```
+
+## Usage
+
+Given a numpy array, or something convertable to it (such as a list),
+`rapidhrv.preprocess` can generate input suitable for analysis with
+`rapidhrv.analyze`, which will return a pandas dataframe containing HRV data.
+
+```python
+import numpy as np
+import rapidhrv as rhv
+
+my_data = np.load("my_data.npy")  # Load data
+data = rhv.Signal(my_data, sample_rate=50)  # Convert to rhv Signal class
+preprocessed = rhv.preprocess(data)  # Preprocess: may interpolate data, check the docstring on `rapidhrv.preprocess`
+result = rhv.analyze(preprocessed)  # Analyze signal
+```
+
+## Documentation
+
+Please see the included [tutorial notebook](https://github.com/peterakirk/RapidHRV/blob/main/resources/tutorial.ipynb).
+
+## Development
+
+In order to get a working development environment,
+please install [Poetry](https://python-poetry.org/) for your platform,
+and run `poetry install` to generate a virtual environment.
+
+If you plan on making any changes to the included notebooks,
+please run `nbstripout --install` from within the poetry venv before committing any changes.
+
+To run said notebooks from the environment provided by poetry,
+install the required dependencies with `poetry install --extras notebooks`.
+
---
+++ packages/rapidhrv/rapidhrv-0.2.7.tar.gz/rapidhrv-0.2.7/rapidhrv/visualization.py
@@ -0,0 +1,106 @@
+from typing import Dict, List
+
+import dash
+import pandas as pd
+import plotly.graph_objects as go
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+import rapidhrv as rhv
+
+# Define the type for an option
+OptionType = Dict[str, str]
+
+
+def results_graph(non_outliers, outliers, selected_column):
+    fig = go.Figure(
+        [
+            go.Scatter(
+                x=non_outliers["Time"],
+                y=non_outliers[selected_column],
+                name=selected_column,
+                mode="lines+markers",
+            ),
+            go.Scatter(
+                x=outliers["Time"],
+                y=outliers[selected_column],
+                name="Outliers",
+                mode="markers",
+            ),
+        ]
+    )
+
+    fig.update_layout(template="plotly_white", clickmode="event+select")
+    fig.update_traces(marker_size=10)
+
+    return fig
+
+
+def window_graph(window_data):
+    signal, peaks, properties = window_data
+    fig = go.Figure(
+        [
+            go.Scatter(y=signal),
+            go.Scatter(
+                x=peaks,
+                y=properties["peak_heights"],
+                mode="markers",
+                marker=dict(line_color="darkorange", symbol="x-thin", size=24, line_width=1.5),
+            ),
+        ],
+    )
+    fig.update_layout(showlegend=False, template="plotly_white")
+
+    return fig
+
+
+def visualize(analyzed: pd.DataFrame, debug=False):
+    app = dash.Dash()
+
+    options: List[OptionType] = [{"label": col, "value": col} for col in rhv.analysis.DATA_COLUMNS]
+
+    non_outlier_data = analyzed.loc[~analyzed["Outlier"]]
+    outlier_data = analyzed.loc[analyzed["Outlier"]]
+
+    selected_column = "BPM"
+    results = results_graph(non_outlier_data, outlier_data, selected_column)
+
+    app.layout = html.Div(
+        [
+            dcc.Dropdown(
+                id="column-dropdown",
+                options=options,  # type: ignore
+                value=selected_column,
+                clearable=False,
+            ),
+            dcc.Graph(id="results-graph", figure=results),
+            html.Div(id="window-container"),
+        ]
+    )
+
+    @app.callback(Output("results-graph", "figure"), Input("column-dropdown", "value"))
+    def update_results_graph(column):
+        return results_graph(non_outlier_data, outlier_data, column)
+
+    @app.callback(Output("window-container", "children"), Input("results-graph", "clickData"))
+    def update_window_graph(click_data):
+        if click_data is None:
+            return []
+
+        selected_point = click_data["points"][0]
+
+        if selected_point["curveNumber"] == 0:
+            window_data = non_outlier_data.iloc[selected_point["pointNumber"]]["Window"]
+        elif selected_point["curveNumber"] == 1:
+            window_data = outlier_data.iloc[selected_point["pointNumber"]]["Window"]
+
+        return [dcc.Graph(figure=window_graph(window_data))]
+
+    app.run(debug=debug, dev_tools_silence_routes_logging=True)
+
+
+if __name__ == "__main__":
+    signal = rhv.data.get_example_data()
+    preprocessed = rhv.preprocess(signal)
+    analyzed = rhv.analyze(preprocessed)
+    visualize(analyzed, debug=True)
