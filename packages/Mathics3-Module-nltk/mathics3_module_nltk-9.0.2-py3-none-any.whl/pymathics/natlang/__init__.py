61892b23600a4caa03b64a4e64d48b4db50ef130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:40 +0000

    Add package Mathics3-Module-nltk/9.0.2/mathics3_module_nltk-9.0.2-py3-none-any.whl

---
+++ packages/Mathics3-Module-nltk/mathics3_module_nltk-9.0.2-py3-none-any.whl/pymathics/natlang/__init__.py
@@ -0,0 +1,104 @@
+"""
+Natural Language Processing
+
+Mathics3 Module provides functions and variables to work with \
+expressions in natural language, using the Python libraries:
+
+<ul>
+  <li><url>:spacy:
+https://spacy.io/</url> for parsing natural languages</url>
+  <li><url>
+:nltk:
+https://www.nltk.org/</url> for functions using WordNet-related builtins
+  <li><url>
+:pyenchant:
+https://pyenchant.github.io/pyenchant/</url> and <url>
+:pycountry:
+https://pypi.org/project/pycountry/</url> for language identification
+</ul>
+
+Examples:
+
+   >> LoadModule["pymathics.natlang"]
+    = pymathics.natlang
+
+   >> Pluralize["try"]
+    = tries
+
+   >> LanguageIdentify["eins zwei drei"]
+    = German
+
+   >> WordFrequency["Apple Tree and apple", "apple", IgnoreCase -> True]
+    = 0.5
+
+   >> TextCases["I was in London last year.", "Pronoun"]
+    = {I}
+
+   >> DeleteStopwords["There was an Old Man of Apulia, whose conduct was very peculiar"]
+     = Old Man Apulia, conduct peculiar
+"""
+
+from pymathics.natlang.linguistic_data import (
+    Antonyms,
+    DictionaryLookup,
+    DictionaryWordQ,
+    RandomWord,
+    Synonyms,
+    WordData,
+    WordDefinition,
+    WordList,
+)
+from pymathics.natlang.manipulate import Pluralize
+from pymathics.natlang.normalization import (
+    DeleteStopwords,
+    TextCases,
+    TextPosition,
+    TextSentences,
+    TextStructure,
+    TextWords,
+)
+from pymathics.natlang.textual_analysis import (
+    Containing,
+    SpellingCorrectionList,
+    WordCount,
+    WordFrequency,
+    WordSimilarity,
+    WordStem,
+)
+
+from pymathics.natlang.linguistic_data.translation import LanguageIdentify
+from pymathics.natlang.version import __version__
+
+pymathics_version_data = {
+    "author": "The Mathics3 Team",
+    "version": __version__,
+    "name": "Natlang",
+    "requires": ["langid", "pyenchant", "nltk", "spacy"],
+}
+
+__all__ = [
+    "Antonyms",
+    "Containing",
+    "DeleteStopwords",
+    "DictionaryLookup",
+    "DictionaryWordQ",
+    "LanguageIdentify",
+    "Pluralize",
+    "RandomWord",
+    "SpellingCorrectionList",
+    "Synonyms",
+    "TextCases",
+    "TextPosition",
+    "TextSentences",
+    "TextStructure",
+    "TextWords",
+    "WordCount",
+    "WordData",
+    "WordDefinition",
+    "WordFrequency",
+    "WordList",
+    "WordSimilarity",
+    "WordStem",
+    "__version__",
+    "pymathics_version_data",
+]
