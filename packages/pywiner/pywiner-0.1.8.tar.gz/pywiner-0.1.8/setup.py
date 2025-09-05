fb3c812e69614550f661091ff6b6a1d6b8c1d20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 14:05:42 +0000

    Add package pywiner/0.1.8/pywiner-0.1.8.tar.gz

---
+++ packages/pywiner/pywiner-0.1.8.tar.gz/pywiner-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pywiner",
+    version="0.1.8",
+    author="GuestRoblox Studios",
+    author_email="maria.gomes23.1949@gmail.com",
+    description="A Python library for Windows automation and a native window framework.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/RoVerify/pywiner",
+    packages=['pywiner', 'pywiner.gui', 'pywiner.system'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'PyQt6'
+    ],
+)
