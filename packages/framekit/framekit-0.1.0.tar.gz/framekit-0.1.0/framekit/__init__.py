6dae29f95682a67dadb604914f8963264f6f3184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:06:25 +0000

    Add package framekit/0.1.0/framekit-0.1.0.tar.gz

---
+++ packages/framekit/framekit-0.1.0.tar.gz/framekit-0.1.0/framekit/__init__.py
@@ -0,0 +1,7 @@
+from .master_scene import *
+from .scene import *
+from .text_element import *
+from .image_element import *
+from .video_element import *
+from .audio_element import *
+from .animation import *
