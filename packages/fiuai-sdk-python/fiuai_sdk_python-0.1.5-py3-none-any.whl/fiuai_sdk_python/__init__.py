80a0489eb4b16360a1a4afa96fac9b90db721f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:51 +0000

    Add package fiuai-sdk-python/0.1.5/fiuai_sdk_python-0.1.5-py3-none-any.whl

---
+++ packages/fiuai-sdk-python/fiuai_sdk_python-0.1.5-py3-none-any.whl/fiuai_sdk_python/__init__.py
@@ -0,0 +1,10 @@
+from .client import FiuaiSDK, get_client
+from .token import TokensConfig
+from .util import init_fiuai
+
+__all__ = [
+    'FiuaiSDK',
+    'TokensConfig',
+    'init_fiuai',
+    'get_client'
+    ]
