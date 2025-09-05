aec9557fab7a0f9fb240f98b1442390f5d3cfcd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:19 +0000

    Add package RoadRunnerEDA/4.0.3/roadrunnereda-4.0.3-py3-none-any.whl

---
+++ packages/RoadRunnerEDA/roadrunnereda-4.0.3-py3-none-any.whl/roadrunner/runner/local.py
@@ -0,0 +1,42 @@
+####    ############
+####    ############
+####
+####
+############    ####
+############    ####
+####    ####    ####
+####    ####    ####
+############
+############
+
+from roadrunner.config import ConfigContext
+import subprocess
+import logging
+import sys
+
+from roadrunner.rr import Pipeline, workdir_name, resultBase
+from roadrunner.tasks import taskPool
+
+def run(cfg:ConfigContext) -> int:
+
+    wd = workdir_name(cfg)
+    rd = resultBase(cfg)
+    ard = rd.absolute()
+
+    args = [sys.executable, Pipeline.SCRIPTNAME]
+    args += ["--setup", f"RoadExec:resultDir={ard}"]
+    args += cfg.get(":_run.runArgs", isType=list, default=[])
+
+    isRoot = taskPool().getCurrTask().isRoot()
+
+    log = logging.getLogger("local")
+    log.debug(f"running:{args} at:{wd}")
+    stdio = None if isRoot else subprocess.DEVNULL
+    cp = subprocess.run(
+        cwd=wd,
+        args=args,
+        stdout=stdio,
+        stderr=stdio
+    )
+
+    return cp.returncode
