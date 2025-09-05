77b19c4ee0d1b715930358ea35c0d5f4d07f6bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 15:08:18 +0000

    Add package vkdispatch/0.0.30/vkdispatch-0.0.30-cp311-cp311-musllinux_1_2_i686.whl

---
+++ packages/vkdispatch/vkdispatch-0.0.30-cp311-cp311-musllinux_1_2_i686.whl/vkdispatch/codegen/__init__.py
@@ -0,0 +1,50 @@
+
+from .arguments import Constant, Variable, ConstantArray, VariableArray
+from .arguments import Buffer, Image1D, Image2D, Image3D
+
+from .arguments import _ArgType
+from .struct_builder import StructBuilder, StructElement
+#from .variables import ShaderVariable # BaseVariable, ShaderVariable
+#from .variables import BoundVariable, BufferVariable, ImageVariable
+
+from .builder import ShaderBinding
+from .builder import ShaderDescription
+from .builder import ShaderBuilder
+from .builder import ShaderVariable, BufferVariable, ImageVariable
+
+from .global_builder import inf_f32, ninf_f32, set_global_builder, comment
+from .global_builder import global_invocation, local_invocation, workgroup
+from .global_builder import workgroup_size, num_workgroups, num_subgroups
+from .global_builder import subgroup_id, subgroup_size, subgroup_invocation, shared_buffer
+
+from .global_builder import abs, acos, acosh, asin, asinh
+from .global_builder import atan, atan2, atanh, atomic_add, barrier
+from .global_builder import ceil, clamp, cos, cosh, cross
+from .global_builder import degrees, determinant, distance, dot
+from .global_builder import exp, exp2, float_bits_to_int, float_bits_to_uint
+from .global_builder import floor, fma, int_bits_to_float
+from .global_builder import inverse, inverse_sqrt, isinf, isnan
+from .global_builder import length, log, log2, max, memory_barrier
+from .global_builder import memory_barrier_shared, min, mix, mod
+from .global_builder import normalize, pow, radians, round, round_even
+from .global_builder import sign, sin, sinh, smoothstep, sqrt, step
+from .global_builder import tan, tanh, transpose, trunc, uint_bits_to_float
+from .global_builder import mult_c64, mult_conj_c64, complex_from_euler_angle, mult_c64_by_const
+
+from .global_builder import if_statement, if_any, if_all, else_statement
+from .global_builder import else_if_statement, else_if_any, else_if_all
+from .global_builder import return_statement, while_statement, end
+from .global_builder import logical_and, logical_or
+from .global_builder import subgroup_add, subgroup_mul
+from .global_builder import subgroup_min, subgroup_max, subgroup_and
+from .global_builder import subgroup_or, subgroup_xor, subgroup_elect
+from .global_builder import subgroup_barrier, mapping_index, kernel_index, mapping_registers
+from .global_builder import set_kernel_index, set_mapping_index, set_mapping_registers
+from .global_builder import printf, unravel_index
+from .global_builder import print_vars as print, builder_context
+from .global_builder import new, new_float, new_int, new_uint
+from .global_builder import new_vec2, new_ivec2, new_uvec2
+from .global_builder import new_vec3, new_ivec3, new_uvec3
+from .global_builder import new_vec4, new_ivec4, new_uvec4
+
+from .abreviations import *
