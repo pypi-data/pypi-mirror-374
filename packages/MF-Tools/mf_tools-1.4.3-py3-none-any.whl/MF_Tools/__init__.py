ef06f8151e8ac4fca56e79451167d821b9f91eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:05:19 +0000

    Add package MF-Tools/1.4.3/mf_tools-1.4.3-py3-none-any.whl

---
+++ packages/MF-Tools/mf_tools-1.4.3-py3-none-any.whl/MF_Tools/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.4.3'
+__author__ = 'John Connell - The Mathematic Fanatic'
+__description__ = 'Collection of helpful utilities for Manim'
+
+from .dual_compatibility import *
+from .transforms import *
+from .updaters import *
+from .geometry import *
+from .misc import *
+from .animations import *
+from .VT_sliders import *
+from .rescaling import *
+
