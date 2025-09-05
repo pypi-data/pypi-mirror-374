82b3c7c7d5ce5ab15a0ae0ce483ae7e692ca087d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:30 +0000

    Add package ai-memory-sdk/0.1.7/ai_memory_sdk-0.1.7-py3-none-any.whl

---
+++ packages/ai-memory-sdk/ai_memory_sdk-0.1.7-py3-none-any.whl/tests/test_messages.py
@@ -0,0 +1,45 @@
+import pytest
+from ai_memory_sdk import Memory 
+import os
+import time
+
+def test_conversational_memory():
+    """ Test the conversational memory client """
+    client = Memory(api_key=os.getenv("LETTA_API_KEY"))
+    test_user_id = "test_user_id_123"
+
+    # initialize the userA
+    client.initialize_user_memory(test_user_id, reset=True)
+
+    # add messages 
+    run =  client.add_messages(test_user_id, messages=[
+        {
+            "role": "user",
+            "content": "Hi my name is Bob"
+        }, 
+        {
+            "role": "assistant",
+            "content": "Hi Bob, how can I help you today?"
+        },
+        {
+            "role": "user",
+            "content": "I love cats"
+        }
+    ], skip_vector_storage=False)
+
+    # wait for the run to complete
+    client.wait_for_run(run)
+
+    # get the memory 
+    memory = client.get_user_memory(test_user_id)
+    assert "Bob" in memory, f"Expected 'Bob' in memory value, got {memory}"
+    print(memory)
+
+    # search 
+    search_results = client.search(test_user_id, "animals")
+    assert len(search_results) > 0, f"Expected search results, got {search_results}"
+    print("SEARCH RESULTS", search_results)
+    assert "cats" in search_results[0], f"Expected 'cats' in search results, got {search_results[0]}"
+    print(search_results)
+
+
