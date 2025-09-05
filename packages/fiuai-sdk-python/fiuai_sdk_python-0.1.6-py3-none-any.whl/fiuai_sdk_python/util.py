109458ac55d3b356ac22a6728293a4351e217422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:50 +0000

    Add package fiuai-sdk-python/0.1.6/fiuai_sdk_python-0.1.6-py3-none-any.whl

---
+++ packages/fiuai-sdk-python/fiuai_sdk_python-0.1.6-py3-none-any.whl/fiuai_sdk_python/util.py
@@ -0,0 +1,55 @@
+# -- coding: utf-8 --
+# Project: frappeclient
+# Created Date: 2025 05 Sa
+# Author: liming
+# Email: lmlala@aliyun.com
+# Copyright (c) 2025 FiuAI
+
+
+
+import logging
+from typing import List
+from .token import TokenConfig, Tokens
+from .type import ClientConfig
+
+logger = logging.getLogger(__name__)
+
+TOKENS = None
+CLIENTCONFIG = None
+
+def create_headers(key, secret, user, tenant="", company=""):
+
+    headers = {
+        "Fiuai-Internal-Auth": "true",
+        "Fiuai-Internal-Key": key,
+        "Fiuai-Internal-Secret": secret,
+        "Fiuai-Internal-User": user,
+        "Fiuai-Internal-Tenant": tenant,
+        "Fiuai-Internal-Company": company,
+        "Accept": "application/json",
+    }
+
+    return headers
+
+
+
+
+def init_fiuai(
+    url: str,
+    tokens: List[TokenConfig],
+    max_api_retry: int=3,
+    timeout: int=5,
+    verify: bool=False
+):
+    
+    global TOKENS, CLIENTCONFIG
+    CLIENTCONFIG = ClientConfig(
+        url=url, 
+        max_api_retry=max_api_retry, 
+        timeout=timeout,
+        verify=verify)
+
+    TOKENS = Tokens(tokens)
+    TOKENS.validate_tokens()
+    TOKENS.load_tokens(tokens)
+
