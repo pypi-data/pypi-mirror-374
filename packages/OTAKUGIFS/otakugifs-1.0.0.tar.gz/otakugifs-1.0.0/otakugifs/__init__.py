1b355d422eddf32414f4a3ff9580f8b0132ddfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 18:13:14 +0000

    Add package OTAKUGIFS/1.0.0/otakugifs-1.0.0.tar.gz

---
+++ packages/OTAKUGIFS/otakugifs-1.0.0.tar.gz/otakugifs-1.0.0/otakugifs/__init__.py
@@ -0,0 +1,5 @@
+from .client import OtakuGIFS
+
+__version__ = "0.1.0"
+__author__ = "infinite"
+__all__ = ["OtakuGIFS"]
---
+++ packages/OTAKUGIFS/otakugifs-1.0.0.tar.gz/otakugifs-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="OTAKUGIFS",
+    version="1.0.0",
+    author="INFINITE_.",
+    author_email="work4infinite@gmail.com",
+    description="A Python wrapper for the OtakuGIFS API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/infinite31/OTAKUGIFS",
+    packages=find_packages(),
+    install_requires=[
+        'httpx'
+    ],
+    classifiers=classifiers,
+    python_requires='>=3.6',
+    keywords=['python', 'gif-generator', 'anime', 'api', 'wrapper'],
+)
