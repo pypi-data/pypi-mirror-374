14adc8e8aaff57c37e8d155d3116e9f958241f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:06:28 +0000

    Add package complete-numbers/0.0.4/complete_numbers-0.0.4-py3-none-any.whl

---
+++ packages/complete-numbers/complete_numbers-0.0.4-py3-none-any.whl/complete_numbers/ctn.py
@@ -0,0 +1,17 @@
+class Ctn:
+    '''The basic numerical object we work with'''
+
+    def __init__(self, realpart, imagpart = 0, intangpart = 0, immatpart = 0):
+        self.a = realpart
+        self.b = imagpart
+        self.c = intangpart
+        self.d = immatpart
+
+    def __str__(self):
+        selfstring = str(self.a)
+        if self.b > 0: selfstring += ' + ' + self.b +'i'
+        if self.c > 0: selfstring += ' + ' + self.c + 'phi'
+        if self.d > 0: selfstring += ' + ' + self.d + 'psi'
+        return selfstring
+
+    
