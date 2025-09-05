8b3c8f892c73f8100cab4c62d099e94c9075f0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 18:13:01 +0000

    Add package apimatic-aviation-stack-sdk/1.0.0/apimatic_aviation_stack_sdk-1.0.0-py3-none-any.whl

---
+++ packages/apimatic-aviation-stack-sdk/apimatic_aviation_stack_sdk-1.0.0-py3-none-any.whl/aviationstack/controllers/__init__.py
@@ -0,0 +1,14 @@
+__all__ = [
+    'base_controller',
+    'flights_controller',
+    'routes_controller',
+    'airports_controller',
+    'airlines_controller',
+    'airplanes_controller',
+    'aircraft_types_controller',
+    'taxes_controller',
+    'cities_controller',
+    'countries_controller',
+    'flight_schedules_controller',
+    'flight_future_schedules_controller',
+]
