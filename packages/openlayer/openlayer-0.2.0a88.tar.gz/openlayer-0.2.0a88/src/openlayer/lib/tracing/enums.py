aa91e478066924e59be17463f94ae6af6c40c000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:21 +0000

    Add package openlayer/0.2.0a88/openlayer-0.2.0a88.tar.gz

---
+++ packages/openlayer/openlayer-0.2.0a88.tar.gz/openlayer-0.2.0a88/src/openlayer/lib/tracing/enums.py
@@ -0,0 +1,12 @@
+"""Module with the enums used in the tracing module."""
+
+import enum
+
+
+class StepType(enum.Enum):
+    USER_CALL = "user_call"
+    CHAT_COMPLETION = "chat_completion"
+    AGENT = "agent"
+    RETRIEVER = "retriever"
+    TOOL = "tool"
+    HANDOFF = "handoff"
