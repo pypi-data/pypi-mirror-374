3031ceecde8b8d4306a73ca505dd16c33fd2a927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 03:15:30 +0000

    Add package dstack/0.19.27/dstack-0.19.27.tar.gz

---
+++ packages/dstack/dstack-0.19.27.tar.gz/dstack-0.19.27/src/dstack/_internal/server/schemas/gateways.py
@@ -0,0 +1,39 @@
+from typing import Annotated, Any, Dict, List, Optional
+
+from pydantic import Field
+
+from dstack._internal.core.models.backends.base import BackendType
+from dstack._internal.core.models.common import CoreModel
+from dstack._internal.core.models.gateways import GatewayConfiguration
+
+
+class CreateGatewayRequest(CoreModel):
+    configuration: GatewayConfiguration
+    # Deprecated and unused. Left for compatibility with 0.18 clients.
+    name: Annotated[Optional[str], Field(exclude=True)] = None
+    backend_type: Annotated[Optional[BackendType], Field(exclude=True)] = None
+    region: Annotated[Optional[str], Field(exclude=True)] = None
+
+    class Config(CoreModel.Config):
+        @staticmethod
+        def schema_extra(schema: Dict[str, Any]) -> None:
+            del schema["properties"]["name"]
+            del schema["properties"]["backend_type"]
+            del schema["properties"]["region"]
+
+
+class GetGatewayRequest(CoreModel):
+    name: str
+
+
+class DeleteGatewaysRequest(CoreModel):
+    names: List[str]
+
+
+class SetDefaultGatewayRequest(CoreModel):
+    name: str
+
+
+class SetWildcardDomainRequest(CoreModel):
+    name: str
+    wildcard_domain: str
