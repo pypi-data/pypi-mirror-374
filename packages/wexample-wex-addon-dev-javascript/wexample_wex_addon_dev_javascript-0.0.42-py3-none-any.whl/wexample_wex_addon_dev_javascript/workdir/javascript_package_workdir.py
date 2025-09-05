36d90eba590bbd5507d03f0f26e3a19b10a865fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:42 +0000

    Add package wexample-wex-addon-dev-javascript/0.0.42/wexample_wex_addon_dev_javascript-0.0.42-py3-none-any.whl

---
+++ packages/wexample-wex-addon-dev-javascript/wexample_wex_addon_dev_javascript-0.0.42-py3-none-any.whl/wexample_wex_addon_dev_javascript/workdir/javascript_package_workdir.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from wexample_config.const.types import DictConfig
+from wexample_wex_addon_dev_javascript.workdir.javascript_workdir import (
+    JavascriptWorkdir,
+)
+
+
+class JavascriptPackageWorkdir(JavascriptWorkdir):
+    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
+
+        raw_value = super().prepare_value(raw_value=raw_value)
+
+        return raw_value
