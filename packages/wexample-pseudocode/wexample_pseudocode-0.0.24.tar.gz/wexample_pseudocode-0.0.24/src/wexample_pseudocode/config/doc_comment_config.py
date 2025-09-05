c3198dc6ef81b57660e092bac3bf78b25f8a3c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:04:38 +0000

    Add package wexample-pseudocode/0.0.24/wexample_pseudocode-0.0.24.tar.gz

---
+++ packages/wexample-pseudocode/wexample_pseudocode-0.0.24.tar.gz/wexample_pseudocode-0.0.24/src/wexample_pseudocode/config/doc_comment_config.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+from wexample_pseudocode.config.doc_comment_parameter_config import (
+    DocCommentParameterConfig,
+)
+from wexample_pseudocode.config.doc_comment_return_config import DocCommentReturnConfig
+
+
+@dataclass
+class DocCommentConfig:
+    description: str | None = None
+    parameters: list[DocCommentParameterConfig] = field(default_factory=list)
+    returns: DocCommentReturnConfig | None = None
