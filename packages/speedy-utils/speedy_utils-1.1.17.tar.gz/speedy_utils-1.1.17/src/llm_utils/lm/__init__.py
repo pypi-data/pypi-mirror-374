627bfe623a3b3419dac427b2771a80abaff4e2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 17:06:07 +0000

    Add package speedy-utils/1.1.17/speedy_utils-1.1.17.tar.gz

---
+++ packages/speedy-utils/speedy_utils-1.1.17.tar.gz/speedy_utils-1.1.17/src/llm_utils/lm/__init__.py
@@ -0,0 +1,12 @@
+# from .async_lm import AsyncLLMTask, AsyncLM
+# from .lm import LM, LLMTask
+
+# OAI_LM = LM
+
+# __all__ = [
+#     "LM",
+#     "OAI_LM",
+#     "AsyncLM",
+#     "LLMTask",
+#     "AsyncLLMTask",
+# ]
