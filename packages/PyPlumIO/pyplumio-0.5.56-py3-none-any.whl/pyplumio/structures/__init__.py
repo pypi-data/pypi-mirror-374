8ac4e2698f9a41316c3d1ce30e74755772ba9d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:19 +0000

    Add package PyPlumIO/0.5.56/pyplumio-0.5.56-py3-none-any.whl

---
+++ packages/PyPlumIO/pyplumio-0.5.56-py3-none-any.whl/pyplumio/structures/__init__.py
@@ -0,0 +1,56 @@
+"""Contains structure decoders."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+
+from pyplumio.frames import Frame
+
+
+@dataclass
+class StructureDataClass:
+    """Represents a structure dataclass mixin."""
+
+    __slots__ = ("frame",)
+
+    frame: Frame
+
+
+class Structure(ABC, StructureDataClass):
+    """Represents a data structure."""
+
+    __slots__ = ()
+
+    @abstractmethod
+    def encode(self, data: dict[str, Any]) -> bytearray:
+        """Encode data to the bytearray message."""
+
+    @abstractmethod
+    def decode(
+        self,
+        message: bytearray,
+        offset: int = 0,
+        data: dict[str, Any] | None = None,
+    ) -> tuple[dict[str, Any], int]:
+        """Decode bytes and return message data and offset."""
+
+
+class StructureDecoder(Structure, ABC):
+    """Represents a structure that only handles decoding."""
+
+    __slots__ = ()
+
+    def encode(self, data: dict[str, Any]) -> bytearray:
+        """Encode data to the bytearray message."""
+        return bytearray()
+
+    @abstractmethod
+    def decode(
+        self, message: bytearray, offset: int = 0, data: dict[str, Any] | None = None
+    ) -> tuple[dict[str, Any], int]:
+        """Decode bytes and return message data and offset."""
+
+
+__all__ = ["Structure", "StructureDecoder"]
