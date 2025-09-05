76bc5e035c5cd47652aee12d3935b86367fccbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 04:05:59 +0000

    Add package asyauth-bAD/0.0.29/asyauth_bad-0.0.29-py3-none-any.whl

---
+++ packages/asyauth-bAD/asyauth_bad-0.0.29-py3-none-any.whl/asyauth/_version.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.0.29"
+__banner__ = \
+"""
+# asyauth-bAD %s 
+# Author: Tamas Jos @skelsec (info@skelsecprojects.com)
+# Current Maintainer: Baptiste Crépin (baptiste@cravaterouge.com)
+""" % __version__
