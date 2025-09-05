1167a052b0fd02c31904dc7595e3071f227ae52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 07:05:43 +0000

    Add package mc-netease-sdk-nyrev/1.0/mc_netease_sdk_nyrev-1.0.tar.gz

---
+++ packages/mc-netease-sdk-nyrev/mc_netease_sdk_nyrev-1.0.tar.gz/mc_netease_sdk_nyrev-1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mc-netease-sdk-nyrev",
+    version="1.0.0",
+    description="Netease ModSDK completion library revised version by Nuoyan",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Nuoyan",
+    author_email="1279735247@qq.com",
+    url="https://github.com/charminglee/mc-netease-sdk-nyrev",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=2.7, <4",
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
