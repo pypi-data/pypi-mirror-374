62482da072a3d83c911f3d0911ec985c6939f8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 05:05:09 +0000

    Add package ct-cloud-storage-mocker/0.1.5/ct_cloud_storage_mocker-0.1.5.tar.gz

---
+++ packages/ct-cloud-storage-mocker/ct_cloud_storage_mocker-0.1.5.tar.gz/ct_cloud_storage_mocker-0.1.5/src/ct_cloud_storage_mocker/_example_test.py
@@ -0,0 +1,46 @@
+"""Test for README example."""
+
+import pathlib
+
+import google.cloud.storage  # type: ignore[import-untyped]
+
+from ct_cloud_storage_mocker import BlobMetadata, Mount
+from ct_cloud_storage_mocker import patch as gcs_patch
+
+
+def test_something(tmp_path: pathlib.Path) -> None:
+    # Creates two temporary directories for readable/writable buckets.
+    src_dir = tmp_path / "src"
+    dest_dir = tmp_path / "dest"
+    src_dir.mkdir()
+    dest_dir.mkdir()
+
+    # A sample file on the readable bucket.
+    (src_dir / "hello.txt").write_text("Hello.")
+    # Optionally, object metadata can also be specified by the file beside the
+    # content, suffixed by ".__metadata__".
+    (src_dir / "hello.txt.__metadata__").write_text(
+        BlobMetadata(content_type="text/plain").dump_json()
+    )
+
+    # Mounts directories. Empty list is allowed if no actual access is required.
+    with gcs_patch(
+        [
+            Mount("readable", src_dir, readable=True),
+            Mount("writable", dest_dir, writable=True),
+        ],
+    ):
+        client = google.cloud.storage.Client()
+
+        # Reads a blob.
+        blob = client.bucket("readable").blob("hello.txt")
+        assert blob.download_as_text() == "Hello."
+        # Metadata is available after downloading the content.
+        assert blob.content_type == "text/plain"
+
+        # Writes a blob.
+        blob = client.bucket("writable").blob("world.txt")
+        blob.upload_from_string("World.")
+
+    # Checks if the file is written appropriately.
+    assert (dest_dir / "world.txt").read_text() == "World."
