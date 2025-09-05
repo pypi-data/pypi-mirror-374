b0d792163276062e19f95085873cad300f8fe358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:04:30 +0000

    Add package millheater/0.13.1/millheater-0.13.1.tar.gz

---
+++ packages/millheater/millheater-0.13.1.tar.gz/millheater-0.13.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="millheater",
+    packages=["mill"],
+    install_requires=["aiohttp>=3.7.4,<4", "async_timeout>=3.0.0"],
+    version="0.13.1",
+    description="A python3 library to communicate with Mill",
+    long_description="A python3 library to communicate with Mill",
+    python_requires=">=3.10",
+    author="Daniel Hjelseth Hoyer",
+    author_email="mail@dahoiv.net",
+    url="https://github.com/Danielhiversen/pymill",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
