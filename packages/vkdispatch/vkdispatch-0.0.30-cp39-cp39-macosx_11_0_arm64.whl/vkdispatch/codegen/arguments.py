4a6928433c203825196ff153f925f064daa50071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 15:08:26 +0000

    Add package vkdispatch/0.0.30/vkdispatch-0.0.30-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/vkdispatch/vkdispatch-0.0.30-cp39-cp39-macosx_11_0_arm64.whl/vkdispatch/codegen/arguments.py
@@ -0,0 +1,43 @@
+import typing
+
+from .builder import ShaderVariable, BufferVariable, ImageVariable
+from vkdispatch.base.dtype import dtype
+
+_ArgType = typing.TypeVar('_ArgType', bound=dtype)
+_ArgCount = typing.TypeVar('_ArgCount', bound=int)
+
+class Constant(ShaderVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class Variable(ShaderVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class ConstantArray(ShaderVariable, typing.Generic[_ArgType, _ArgCount]):
+    def __init__(self) -> None:
+        pass
+
+class VariableArray(ShaderVariable, typing.Generic[_ArgType, _ArgCount]):
+    def __init__(self) -> None:
+        pass
+
+class Buffer(BufferVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class Image1D(ImageVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class Image2D(ImageVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class Image2DArray(ImageVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
+
+class Image3D(ImageVariable, typing.Generic[_ArgType]):
+    def __init__(self) -> None:
+        pass
