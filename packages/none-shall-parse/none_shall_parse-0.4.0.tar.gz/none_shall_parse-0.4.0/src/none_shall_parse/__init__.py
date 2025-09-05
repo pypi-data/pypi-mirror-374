66fb8231d3256111b81233c8c0ce13947e474378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 06:04:54 +0000

    Add package none-shall-parse/0.4.0/none_shall_parse-0.4.0.tar.gz

---
+++ packages/none-shall-parse/none_shall_parse-0.4.0.tar.gz/none_shall_parse-0.4.0/src/none_shall_parse/__init__.py
@@ -0,0 +1,15 @@
+"""Trinity Shared Python utilities.
+
+A collection of shared utilities for Trinity projects.
+
+Originally intended to be parsing utilities, this grew to include
+other useful functions.
+
+Named for its author Andries Niemandt - whose surname loosely
+translates to "none". Combined this with our parsing intentions
+to create a name which nods to the Black Knight in Monty Python's Holy Grail.
+https://www.youtube.com/watch?v=zKhEw7nD9C4
+"""
+
+__author__ = "Andries Niemandt, Jan Badenhorst"
+__email__ = "andries.niemandt@trintel.co.za, jan@trintel.co.za"
