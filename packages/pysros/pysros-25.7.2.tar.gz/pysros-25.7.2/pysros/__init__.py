b512d2bce0d852e1a7d35338afdf927fc1ac211f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-05 16:05:32 +0000

    Add package pysros/25.7.2/pysros-25.7.2.tar.gz

---
+++ packages/pysros/pysros-25.7.2.tar.gz/pysros-25.7.2/pysros/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2021-2024 Nokia
+
+__all__ = ("management", "exceptions", "wrappers", "pprint", )
+
+__doc__ = """Library for management of model-driven nodes."""
+
+__version__ = "25.7.2"
