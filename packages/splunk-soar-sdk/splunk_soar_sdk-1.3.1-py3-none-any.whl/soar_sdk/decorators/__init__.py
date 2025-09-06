2af3d24ef5bf18d8ce10c89f7969af3ccb44a4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:05:36 +0000

    Add package splunk-soar-sdk/1.3.1/splunk_soar_sdk-1.3.1-py3-none-any.whl

---
+++ packages/splunk-soar-sdk/splunk_soar_sdk-1.3.1-py3-none-any.whl/soar_sdk/decorators/__init__.py
@@ -0,0 +1,21 @@
+"""
+SOAR SDK Decorators
+
+This module provides class-based decorators for SOAR app development.
+"""
+
+from .action import ActionDecorator
+from .test_connectivity import ConnectivityTestDecorator
+from .view_handler import ViewHandlerDecorator
+from .on_poll import OnPollDecorator
+from .webhook import WebhookDecorator
+from .generic_action import GenericActionDecorator
+
+__all__ = [
+    "ActionDecorator",
+    "ConnectivityTestDecorator",
+    "GenericActionDecorator",
+    "OnPollDecorator",
+    "ViewHandlerDecorator",
+    "WebhookDecorator",
+]
