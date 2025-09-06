28c717c5793b255064b5f5cc2e6c598226db9edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:24 +0000

    Add package pyThermoDB/1.11.23/pythermodb-1.11.23.tar.gz

---
+++ packages/pyThermoDB/pythermodb-1.11.23.tar.gz/pythermodb-1.11.23/pyThermoDB/models/references.py
@@ -0,0 +1,29 @@
+# import libs
+from typing import (
+    Any,
+    List,
+    Literal,
+    Dict,
+    Optional
+)
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    Field,
+    field_validator,
+    model_validator
+)
+
+
+class Component(BaseModel):
+    """Component model for input validation"""
+    name: str = Field(..., description="Name of the component")
+    formula: str = Field(..., description="Chemical formula of the component")
+    state: Literal['g', 'l', 's', 'aq'] = Field(
+        ...,
+        description="State of the component: 'g' for gas, 'l' for liquid, 's' for solid, 'aq' for aqueous"
+    )
+    mole_fraction: float = Field(
+        default=1.0,
+        description="Mole fraction of the component in a mixture, if applicable"
+    )
