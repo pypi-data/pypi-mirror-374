401d33cd46ee0d737369648465fe827c96154d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:36 +0000

    Add package ecodev-front/0.0.96/ecodev_front-0.0.96-py3-none-any.whl

---
+++ packages/ecodev-front/ecodev_front-0.0.96-py3-none-any.whl/ecodev_front/constants.py
@@ -0,0 +1,84 @@
+"""
+Callbacks constants
+"""
+CHILDREN = 'children'
+DATA = 'data'
+PATHNAME = 'pathname'
+N_CLICKS = 'n_clicks'
+VALUE = 'value'
+DISABLED = 'disabled'
+STYLE = 'style'
+ACTIVE = 'active'
+LOCAL = 'local'
+OPENED = 'opened'
+CONTENTS = 'contents'
+FILENAME = 'filename'
+TYPE = 'type'
+INDEX = 'index'
+ID = 'id'
+VISIBLE = 'visible'
+CHECKED = 'checked'
+HIDDEN = 'hidden'
+HREF = 'href'
+FIGURE = 'figure'
+
+CLICK_DATA = 'clickData'
+ERROR = 'error'
+LOADING = 'loading'
+ALLOW_STEP_CLICK = 'allowStepClick'
+STEPS = 'steps'
+LABEL = 'label'
+OPTIONS = 'options'
+REQUIRED = 'required'
+DISPLAY = 'display'
+SEND_NOTIFICATIONS = 'sendNotifications'
+TITLE = 'title'
+ACTION = 'action'
+MESSAGE = 'message'
+ICON = 'icon'
+COLOR = 'color'
+CREATED_AT = 'created_at'
+WITH_CLOSE_BUTTON = 'withCloseButton'
+
+"""
+Table constants
+"""
+CELL_RENDERER_DATA = 'cellRendererData'
+ROW_DATA = 'rowData'
+ROW_INDEX = 'rowIndex'
+CELL_CLICKED = 'cellClicked'
+CELL_VALUE_CHANGED = 'cellValueChanged'
+ROW_ID = 'rowId'
+COL_ID = 'colId'
+
+"""
+Stores
+"""
+SESSION_STORE = 'session'
+LOCAL_STORE = 'local'
+
+NOTIFICATION_ID = 'notification-id'
+MAIN_PAGE_URL = '/'
+LOGIN_PAGE_URL = '/login'
+
+"""
+Unit Converters
+"""
+THOUSAND = 1000
+MILLION = 1_000_000
+BILLION = 1_000_000_000
+TRILLION = 1_000_000_000_000
+
+"""
+DATA VERSIONING
+"""
+UPDATE_TYPE = 'update_type'
+INSERTED = 'inserted'
+UPDATED = 'updated'
+DELETED = 'deleted'
+UNCHANGED = 'unchanged'
+OLD_VALUE = 'old-value'
+NEW_VALUE = 'new-value'
+MAIN_ID = 'main-id'
+STAGING_ID = 'staging-id'
+UPDATED_AT = 'updated_at'
---
+++ packages/ecodev-front/ecodev_front-0.0.96-py3-none-any.whl/ecodev_front/ids.py
@@ -0,0 +1,79 @@
+"""
+File containing component IDs, to use in callbacks
+"""
+
+URL = 'url'
+TOKEN = 'token'
+HEADER_ID = 'navbar-id'
+LOGIN = 'login'
+USERNAME = 'username'
+PASSWORD = 'password'
+LOGIN_USERNAME_INPUT_ID = 'login-username-id'
+LOGIN_PASSWORD_INPUT_ID = 'login-password-id'
+LOGIN_BTN_ID = 'login-button-id'
+LOGOUT_BTN_ID = 'logout-button-id'
+FOOTER_ID = 'footer-id'
+PROJECT_HEADER_ID = 'page-header-project-placeholder-id'
+MAIN_CONTENT_ID = 'main-content-id'
+PAGE = 'page'
+APPSHELL = 'appshell'
+ASIDE = 'aside'
+NAVBAR = 'navbar'
+HOME_BUTTON = 'home-button-id'
+MODULE_BUTTON = 'module-button'
+DOCUMENTATION = 'documentation'
+NOTIFICATION = 'notification'
+
+
+"""
+Button Actions
+"""
+USE = 'use'
+CONTINUE = 'continue'
+SAVE = 'save'
+COLUMN = 'column'
+ACCEPT = 'accept'
+REJECT = 'reject'
+
+SUBMIT_BTN = 'submit-btn'
+CLOSE_BTN = 'close-btn'
+SAVE_BTN = 'save-btn'
+CONTINUE_BTN = 'continue-btn'
+ACCEPT_BTN = 'accept-btn'
+CONFIRM_BTN = 'confirm-btn'
+CANCEL_BTN = 'cancel-btn'
+DELETE_BTN = 'delete-btn'
+UPDATE_BTN = 'update-btn'
+ADD_BTN = 'add-btn'
+"""
+Component types
+"""
+TABLE = 'table'
+LOADING_OVERLAY = 'loading-overlay'
+MODAL = 'modal'
+BUTTON = 'button'
+OUTPUT = 'output'
+ACCORDION_ITEM = 'accordion-item'
+SELECT = 'select'
+NULLIFY = 'nullify'
+SWITCH = 'switch'
+TEXT_INPUT = 'text-input'
+DRAWER = 'drawer'
+DIV = 'div'
+SEGMENTED_CONTROL = 'segmented-control'
+CHECKBOX = 'checkbox'
+ACTION_ITEM = 'action-item'
+TAGS_INPUT = 'tags-input'
+INTERVAL = 'interval'
+ACCORDION = 'accordion'
+STACK = 'stack'
+MULTI_SELECT = 'multi-select'
+
+__all__ = ['URL', 'TOKEN', 'HEADER_ID', 'LOGIN_USERNAME_INPUT_ID', 'LOGIN_PASSWORD_INPUT_ID',
+           'LOGIN_BTN_ID', 'LOGOUT_BTN_ID', 'FOOTER_ID', 'MAIN_CONTENT_ID', 'NAVBAR',
+           'ASIDE', 'PAGE', 'APPSHELL', 'HOME_BUTTON', 'MODULE_BUTTON', 'LOGIN', 'USERNAME',
+           'PASSWORD', 'BUTTON', 'TABLE', 'LOADING_OVERLAY', 'MODAL', 'OUTPUT', 'USE', 'CONTINUE',
+           'ACCORDION_ITEM', 'NULLIFY', 'SWITCH', 'TEXT_INPUT', 'DRAWER', 'DIV',
+           'SEGMENTED_CONTROL', 'CHECKBOX', 'DOCUMENTATION', 'ACTION_ITEM', 'TAGS_INPUT',
+           'INTERVAL', 'ACCORDION', 'STACK', 'SELECT', 'MULTI_SELECT', 'NOTIFICATION', 'SAVE_BTN',
+           'SUBMIT_BTN', 'CLOSE_BTN']
