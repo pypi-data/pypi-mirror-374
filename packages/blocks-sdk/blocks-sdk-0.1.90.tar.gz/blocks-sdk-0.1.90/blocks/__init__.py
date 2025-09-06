82104c3d651a1e520c349467c2aa3a89c9c844cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:04 +0000

    Add package blocks-sdk/0.1.90/blocks-sdk-0.1.90.tar.gz

---
+++ packages/blocks-sdk/blocks-sdk-0.1.90.tar.gz/blocks-sdk-0.1.90/blocks/__init__.py
@@ -0,0 +1,14 @@
+from .repo import Repo
+from .on import OnClass
+from .task import TaskClass
+from .state import BlocksState
+from .utils import bash, experimental_bash
+from .git import Git
+from .config import Config
+
+config = Config()
+git = Git(config)
+repo = Repo(config)
+state = BlocksState()
+task = TaskClass.get_decorator(state)
+on = OnClass.get_decorator(state)
