989fb5413d0a6f6325717667c27af92145ec11cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:53 +0000

    Add package kubeSmartService/0.1.0/kubesmartservice-0.1.0.tar.gz

---
+++ packages/kubeSmartService/kubesmartservice-0.1.0.tar.gz/kubesmartservice-0.1.0/src/kubesmartservice/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+"""kubeSmartService public API."""
+
+from .core import kube_service, SmartService, init_resolver, KubeSmartServiceError
+
+__all__ = ["kube_service", "SmartService", "init_resolver", "KubeSmartServiceError"]
+__version__ = "0.1.0"
+
+
