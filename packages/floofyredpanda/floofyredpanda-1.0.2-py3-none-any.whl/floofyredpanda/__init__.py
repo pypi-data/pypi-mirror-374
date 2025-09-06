e314600455212bfb9b472bc831b5fd3d2df3175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:03 +0000

    Add package floofyredpanda/1.0.2/floofyredpanda-1.0.2-py3-none-any.whl

---
+++ packages/floofyredpanda/floofyredpanda-1.0.2-py3-none-any.whl/floofyredpanda/__init__.py
@@ -0,0 +1,11 @@
+from .client import tell_the_server, secretly_tell_the_server, RedPandaClient
+from .server import start_floofy_server, ParanoidPanda
+__version__ = "1.0.2"
+__all__ = [
+    "tell_the_server",
+    "secretly_tell_the_server",
+    "RedPandaClient",
+    "start_floofy_server",
+    "ParanoidPanda",
+    "__version__",
+]
