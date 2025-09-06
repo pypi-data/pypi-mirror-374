928ffb55504ec32caa8a23cf28cac06c5147b83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:12 +0000

    Add package cdk-cross-account-route53/1.0.3/cdk_cross_account_route53-1.0.3.tar.gz

---
+++ packages/cdk-cross-account-route53/cdk_cross_account_route53-1.0.3.tar.gz/cdk_cross_account_route53-1.0.3/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cross-account-route53",
+    "version": "1.0.3",
+    "description": "CDK Construct to allow creation of Route 53 records in a different account",
+    "license": "Apache-2.0",
+    "url": "https://github.com/johnf/cdk-cross-account-route53.git",
+    "long_description_content_type": "text/markdown",
+    "author": "John Ferlito<johnf@inodes.org>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/johnf/cdk-cross-account-route53.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cross_account_route53",
+        "cdk_cross_account_route53._jsii"
+    ],
+    "package_data": {
+        "cdk_cross_account_route53._jsii": [
+            "cdk-cross-account-route53@1.0.3.jsii.tgz"
+        ],
+        "cdk_cross_account_route53": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.9",
+    "install_requires": [
+        "aws-cdk-lib>=2.146.0, <3.0.0",
+        "constructs>=10.0.5, <11.0.0",
+        "jsii>=1.114.1, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
