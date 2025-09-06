d526ba978cf1611694184db95b579ced5ebfcb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 07:05:33 +0000

    Add package aiida-pythonjob/0.3.5/aiida_pythonjob-0.3.5.tar.gz

---
+++ packages/aiida-pythonjob/aiida_pythonjob-0.3.5.tar.gz/aiida_pythonjob-0.3.5/src/aiida_pythonjob/__init__.py
@@ -0,0 +1,21 @@
+"""AiiDA plugin that run Python function on remote computers."""
+
+__version__ = "0.3.5"
+
+from node_graph import socket_spec as spec
+
+from .calculations import PyFunction, PythonJob
+from .decorator import pyfunction
+from .launch import prepare_pyfunction_inputs, prepare_pythonjob_inputs
+from .parsers import PythonJobParser
+
+__all__ = (
+    "PickledData",
+    "PyFunction",
+    "PythonJob",
+    "PythonJobParser",
+    "prepare_pyfunction_inputs",
+    "prepare_pythonjob_inputs",
+    "pyfunction",
+    "spec",
+)
