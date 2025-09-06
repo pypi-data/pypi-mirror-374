ca136ee25cba876fe02dc5223800ff09bd7e1717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:32 +0000

    Add package lightning-parser-overlays/0.3.51/lightning_parser_overlays-0.3.51.tar.gz

---
+++ packages/lightning-parser-overlays/lightning_parser_overlays-0.3.51.tar.gz/lightning_parser_overlays-0.3.51/lightning_parser_overlays/goes_overlays.py
@@ -0,0 +1,8 @@
+
+from core import Overlay, FigureDetails, ColorbarDetails, FigBaseDimensions, RangeParams
+
+def receive_overlay(figbasedimensions: FigBaseDimensions, rangeparams: RangeParams) -> Overlay:
+  """
+  Receives an overlay for the lightning parser lib
+  """
+  
