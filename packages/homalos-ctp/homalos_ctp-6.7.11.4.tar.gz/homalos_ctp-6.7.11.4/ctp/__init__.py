688334aa840db30e26431ed32c4cf3c11ccaa40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:11 +0000

    Add package homalos-ctp/6.7.11.4/homalos_ctp-6.7.11.4.tar.gz

---
+++ packages/homalos-ctp/homalos_ctp-6.7.11.4.tar.gz/homalos_ctp-6.7.11.4/ctp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@ProjectName: homalos-ctp
+@FileName   : __init__.py.py
+@Date       : 2025/8/26 17:34
+@Author     : Donny
+@Email      : donnymoving@gmail.com
+@Software   : PyCharm
+@Description: 要编译的CTP版本号配置
+"""
+ctp_version = "6.7.11"
