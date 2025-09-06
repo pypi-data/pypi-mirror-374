003f9605622d6c69bd9a61c1166794c0ba33c359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 03:13:27 +0000

    Add package bermuda/0.1.6/bermuda-0.1.6-cp314-cp314-win_arm64.whl

---
+++ packages/bermuda/bermuda-0.1.6-cp314-cp314-win_arm64.whl/bermuda/__init__.py
@@ -0,0 +1,15 @@
+from bermuda._bermuda import (
+    split_polygons_on_repeated_edges,
+    triangulate_path_edge,
+    triangulate_polygons_face,
+    triangulate_polygons_face_3d,
+    triangulate_polygons_with_edge,
+)
+
+__all__ = (
+    'split_polygons_on_repeated_edges',
+    'triangulate_path_edge',
+    'triangulate_polygons_face',
+    'triangulate_polygons_face_3d',
+    'triangulate_polygons_with_edge',
+)
