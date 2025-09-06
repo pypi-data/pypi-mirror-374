60f30537f82bfdb21ec2b7dfd4952b4d525266e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:17 +0000

    Add package pyThermoDB/1.11.21/pythermodb-1.11.21.tar.gz

---
+++ packages/pyThermoDB/pythermodb-1.11.21.tar.gz/pythermodb-1.11.21/pyThermoDB/config/setting.py
@@ -0,0 +1,11 @@
+# import packages/modules
+# internal
+
+# NOTE: api url
+API_URL = "https://script.google.com/macros/s/"
+
+# SECTION: reference config keys
+REFERENCE_CONFIG_KEYS = [
+    'ALL',
+    'DEFAULT'
+]
