98f1ec2b7674bcd049635a0097208cad9fc2997a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:05:56 +0000

    Add package logie/0.1.6/logie-0.1.6-py3-none-any.whl

---
+++ packages/logie/logie-0.1.6-py3-none-any.whl/test/test.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append('/home/kimyh/library/logie')
+import logie as lo
+
+def main():
+    log = lo.get_logger(
+        console_display=True
+    )
+    log.info('아메리카노 마시고 싶다.')
+    log.error('이건 에러 문구가 나올지도 모른다.')
+    pass
+
+if __name__ == '__main__':
+    main()
+    
