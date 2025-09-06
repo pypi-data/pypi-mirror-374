502d71b97596931f7dfbb21b5ad9b4593edf030d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:07:20 +0000

    Add package python-autofixture/1.1.2/python-autofixture-1.1.2.tar.gz

---
+++ packages/python-autofixture/python-autofixture-1.1.2.tar.gz/python-autofixture-1.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+DESCRIPTION = 'A Version of C#s AutoFixture but for python'
+LONG_DESCRIPTION = 'Allows creating of fake test data that autogenerates both primitive and deep object graph types'
+
+# Setting up
+setup(
+    name="python-autofixture",
+    version="1.1.2",
+    author="GanTheMan",
+    author_email="aidanwilliamgannon@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'autofixture', 'python autofixture', 'python testing', 'python autofixture c#'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix"
+    ]
+)
