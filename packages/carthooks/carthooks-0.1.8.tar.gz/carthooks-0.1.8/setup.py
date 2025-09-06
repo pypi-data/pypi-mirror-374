8879f648b3b9bd1a5127bf1321fec92d810add4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 16:06:01 +0000

    Add package carthooks/0.1.8/carthooks-0.1.8.tar.gz

---
+++ packages/carthooks/carthooks-0.1.8.tar.gz/carthooks-0.1.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carthooks',
+    version='0.1.8',
+    packages=find_packages(),
+    description='Carthooks Python SDK',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Carthooks',
+    author_email='developer@carthooks.com',
+    license='MIT',
+    install_requires=[
+        'httpx[http2]>=0.24.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
