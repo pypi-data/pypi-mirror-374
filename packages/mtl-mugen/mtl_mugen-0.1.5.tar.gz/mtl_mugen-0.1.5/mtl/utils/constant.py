b515e05bc9a8b3eaf8cd65fa14a11f0d8dd98a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:33 +0000

    Add package mtl-mugen/0.1.5/mtl_mugen-0.1.5.tar.gz

---
+++ packages/mtl-mugen/mtl_mugen-0.1.5.tar.gz/mtl_mugen-0.1.5/mtl/utils/constant.py
@@ -0,0 +1,7 @@
+MTL_VERSION = "20250729.01"
+DEBUGGER_VERSION = 1
+
+LEGAL_COMPILER_FLAGS = [
+    "no_implicit_conversion", "no_numeric", "no_implicit_bool", "no_implicit_enum",
+    "no_changestate_expression", "no_compiler_internal", "allow_changestate_target"
+]
