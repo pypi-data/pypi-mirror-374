c5982eb7e1d18d4731ce3438db26143fd98398ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:06:53 +0000

    Add package i2net/2.6.1/i2net-2.6.1.tar.gz

---
+++ packages/i2net/i2net-2.6.1.tar.gz/i2net-2.6.1/src/i2net/__init__.py
@@ -0,0 +1,13 @@
+from .i2net import decode, decode_single, get_decoder, load_data_with_ipv4_ipv6_support, I2NeT_Decoder
+
+__version__ = "2.6.1"
+__author__ = "Omesh Fernando"
+
+
+__all__ = [
+    'decode',
+    'decode_single',
+    'get_decoder',
+    'load_data_with_ipv4_ipv6_support',
+    'I2NeT_Decoder'
+]
