e4073371f4af84eb5d21ff75e5a463313077cf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:06:18 +0000

    Add package pyhausbus/1.0.14/pyhausbus-1.0.14.tar.gz

---
+++ packages/pyhausbus/pyhausbus-1.0.14.tar.gz/pyhausbus-1.0.14/pyhausbus/GenericBusData.py
@@ -0,0 +1,13 @@
+
+class GenericBusData:
+
+  def __init__(self):
+
+  @staticmethod
+  def _fromBytes(dataIn:bytearray, offset):
+    return GenericBusData()
+
+  def __str__(self):
+    return f"GenericBusData()"
+
+
