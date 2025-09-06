c2c232c27251240895d0393775dd2335ebfd34a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 05:06:42 +0000

    Add package pl-horizontal/0.1.4/pl_horizontal-0.1.4.tar.gz

---
+++ packages/pl-horizontal/pl_horizontal-0.1.4.tar.gz/pl_horizontal-0.1.4/src/lib.rs
@@ -0,0 +1,16 @@
+mod collapse;
+mod arg_true;
+mod multi_index;
+mod arg_minmax;
+mod is_minmax;
+use pyo3::prelude::*;
+use pyo3_polars::PolarsAllocator;
+
+#[pymodule]
+fn _internal(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
+
+#[global_allocator]
+static ALLOC: PolarsAllocator = PolarsAllocator::new();
