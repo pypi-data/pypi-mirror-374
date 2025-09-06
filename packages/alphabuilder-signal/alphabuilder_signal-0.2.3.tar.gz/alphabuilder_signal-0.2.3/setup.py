7fc8484fd42abd3651a5205719999e6392a752ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 16:06:21 +0000

    Add package alphabuilder-signal/0.2.3/alphabuilder_signal-0.2.3.tar.gz

---
+++ packages/alphabuilder-signal/alphabuilder_signal-0.2.3.tar.gz/alphabuilder_signal-0.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.MD", "r") as f:
+    description = f.read()
+
+setup(
+    name="alphabuilder_signal",
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires = [
+        # dependencies
+    ],
+    entry_points={
+        "console_scripts": [
+            "alphabuilder_signal = indicators:TechnicalIndicators"
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
