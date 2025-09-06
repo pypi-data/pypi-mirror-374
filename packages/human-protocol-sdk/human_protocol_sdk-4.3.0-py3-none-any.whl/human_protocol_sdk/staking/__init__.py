0d315fa35fb60f602535ffcfc95f68dd0777b6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:53 +0000

    Add package human-protocol-sdk/4.3.0/human_protocol_sdk-4.3.0-py3-none-any.whl

---
+++ packages/human-protocol-sdk/human_protocol_sdk-4.3.0-py3-none-any.whl/human_protocol_sdk/staking/__init__.py
@@ -0,0 +1,6 @@
+"""
+This module enables to perform actions on staking contracts and
+obtain staking information from both the contracts and subgraph.
+"""
+
+from .staking_client import StakingClient, StakingClientError
