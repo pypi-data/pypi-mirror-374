b1c53556743a48ad32b6771d7e7d1ef3bd369cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 05:07:48 +0000

    Add package griffe-typingdoc/0.2.9/griffe_typingdoc-0.2.9.tar.gz

---
+++ packages/griffe-typingdoc/griffe_typingdoc-0.2.9.tar.gz/griffe_typingdoc-0.2.9/src/griffe_typingdoc/__init__.py
@@ -0,0 +1,10 @@
+"""Griffe TypingDoc package.
+
+Griffe extension for PEP 727 - Documentation Metadata in Typing.
+"""
+
+from __future__ import annotations
+
+from griffe_typingdoc._internal.extension import TypingDocExtension
+
+__all__: list[str] = ["TypingDocExtension"]
