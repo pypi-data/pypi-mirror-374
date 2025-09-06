28075cf15432032c0a09782189022674974c75bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:05 +0000

    Add package neon-skill-audio-recording/0.0.2a4/neon_skill_audio_recording-0.0.2a4-py3-none-any.whl

---
+++ packages/neon-skill-audio-recording/neon_skill_audio_recording-0.0.2a4-py3-none-any.whl/neon_skill_audio_recording/__init__.py
@@ -0,0 +1,98 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2025 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from datetime import timedelta
+
+from ovos_bus_client.message import Message
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_utils.time import now_local
+from ovos_workshop.decorators import intent_handler
+from ovos_workshop.skills import OVOSSkill
+
+
+class AudioRecordingSkill(OVOSSkill):
+    def __init__(self, **kwargs):
+        OVOSSkill.__init__(self, **kwargs)
+        self.recording = False
+        self.add_event("recognizer_loop:record_stop", self.handle_recording_stop)
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(
+            internet_before_load=False,
+            network_before_load=False,
+            gui_before_load=False,
+            requires_internet=False,
+            requires_network=False,
+            requires_gui=False,
+            no_internet_fallback=True,
+            no_network_fallback=True,
+            no_gui_fallback=True,
+        )
+
+    @property
+    def max_recording_time(self):
+        return self.settings.get("max_recording_seconds", 240)
+
+    @intent_handler("start_recording.intent")
+    def handle_start_recording(self, message):
+        recording_name = message.data.get("name", str(now_local()))
+        self.recording = True
+        self.bus.emit(message.forward("recognizer_loop:state.set",
+                                      {"state": "recording",
+                                       "recording_name": recording_name}))
+
+        def maybe_stop(message):
+            if self.recording:
+                self.bus.emit(message.forward("recognizer_loop:record_stop"))
+                self.recording = False
+
+        # force a way out of recording mode after timeout
+        self.schedule_event(maybe_stop, now_local() + timedelta(seconds=self.max_recording_time))
+
+    @intent_handler("start_recording.intent")
+    def handle_captains_log(self, message):
+        message.data["name"] = message.data.get("name", "captains_log_" + str(now_local()))
+        self.handle_start_recording(message)
+
+    def handle_recording_stop(self, message):
+        self.recording = False
+
+    def stop(self):
+        """
+        Optional action to take when "stop" is requested by the user.
+        This method should return True if it stopped something or
+        False (or None) otherwise.
+        If not relevant to your skill, feel free to remove.
+        """
+        if self.recording:
+            self.recording = False
+            self.bus.emit(Message("recognizer_loop:record_stop"))
+            return True
+        return False
