af4dfa9184084a3ed5a3e3a657969b7b5924177b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:05:43 +0000

    Add package data-compute/0.0.1/data_compute-0.0.1.tar.gz

---
+++ packages/data-compute/data_compute-0.0.1.tar.gz/data_compute-0.0.1/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import find_packages, setup
+setup(
+    name='data-compute',
+    packages=find_packages(include=['data-compute']),
+    version='0.0.1',
+    description='x',
+    author=
+    'x',
+)
