452b963f3fc9751ff388ba276f33558cb1581a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:49 +0000

    Add package tkinter-videoplayer/1.0.0/tkinter_videoplayer-1.0.0-py3-none-any.whl

---
+++ packages/tkinter-videoplayer/tkinter_videoplayer-1.0.0-py3-none-any.whl/tkinter_videoplayer/theme.py
@@ -0,0 +1,5 @@
+COLOR_CONTROLS_BG = "#171a1f"  # Main background
+COLOR_SLIDER_BG = "#fff"  # Main foreground/text
+FONT = ("Segoe UI", 13, "bold")
+COLOR_PRIMARY = "#3498db"  # Primary accent (blue)
+COLOR_TERTIARY = "#888"  # Tertiary accent (gray)
