c955bd41cc611d1107863e249612310b0e5f358c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 03:13:35 +0000

    Add package somepytools/1.4.0/somepytools-1.4.0-py3-none-any.whl

---
+++ packages/somepytools/somepytools-1.4.0-py3-none-any.whl/somepytools/general.py
@@ -0,0 +1,187 @@
+import shutil
+from datetime import date, datetime, timedelta
+from functools import wraps
+from inspect import getfullargspec
+from pathlib import Path
+from typing import Any, Iterator, Union, get_args
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+from .constants import SIZE_CONSTANTS
+from .typing import Directory, File
+
+
+def str2pathlib(func):
+    """Decorator to convert string inputs to Path when they are annotated as Path
+
+    Allows to write decorated function as if you always have Path object as input
+        but use this function with string input variables (see Example section).
+
+    Under the hood it converts all input variables annotated as Path
+        and having type str to Path objects.
+
+    Example:
+        from pathlib import Path
+
+        @str2pathlib
+        def my_func(source: Path):
+            dest = source / "my_dir"
+            dest.mkdir()
+            return dest
+
+        my_func("~/home/user")
+
+        >>> PosixPath('~/home/user/my_dir')
+    """
+
+    def str_to_path(item: Any) -> Any:
+        if isinstance(item, str):
+            return Path(item)
+        return item
+
+    full_arg_spec = getfullargspec(func)
+    all_defaults = full_arg_spec.defaults or ()
+
+    path_args = []
+    for arg_name, arg_type in full_arg_spec.annotations.items():
+        if (arg_type != Path and Path not in get_args(arg_type)) or arg_name == "return":
+            continue
+
+        if arg_name in full_arg_spec.kwonlyargs:
+            index = float("inf")
+            needs_default = True
+            default = full_arg_spec.kwonlydefaults[arg_name]
+        else:
+            index = full_arg_spec.args.index(arg_name)
+            def_index = index - len(full_arg_spec.args) + len(all_defaults)
+            needs_default = def_index >= 0
+            default = full_arg_spec.defaults[def_index] if needs_default else None
+
+        path_args.append((index, arg_name, needs_default, str_to_path(default)))
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        args = list(args)
+
+        for index, arg_name, needs_default, default in path_args:
+            if arg_name in kwargs:
+                kwargs[arg_name] = str_to_path(kwargs[arg_name])
+            elif index < len(args):  # this was passed as arg
+                args[index] = str_to_path(args[index])
+            elif needs_default:
+                kwargs[arg_name] = default
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@str2pathlib
+def download_url(url: str, save_path: Union[File, Directory, None] = None) -> File:
+    """Downloads and saves data from url
+
+    Args:
+        url: address of file to download
+        save_path: file or directory path to save to.
+            If filename is not provided, it is taken from url.
+            If directory is not provided, file saved to current directory.
+
+    Returns:
+        Actual path to downloaded file
+    """
+    if save_path is None:
+        save_path = Path(urlparse(url).path.split("/")[-1])
+    elif save_path.is_dir():
+        save_path = save_path / urlparse(url).path.split("/")[-1]
+
+    with urlopen(url) as response:
+        with open(save_path, "wb") as file:
+            file.write(response.read())
+
+    return save_path
+
+
+@str2pathlib
+def extract_zip(zip_path: File, save_dir: Directory):
+    """Unzips archive
+
+    Args:
+        zip_file_path: path to *.zip file to unzip
+        save_dir:  path for files and folders to save
+    """
+    with ZipFile(zip_path) as zip_file:
+        zip_file.extractall(save_dir)
+
+
+@str2pathlib
+def cp(source: Path, dest: Path, parents: bool = True) -> Path:
+    """Copies file or folder to destination for both strings and pathlib objects
+
+    Unfortunately pathlib doesn't have a native copying function =(((
+    """
+    if source.is_dir():
+        copied = shutil.copytree(source, dest, dirs_exist_ok=True)
+    else:
+        if parents:
+            if dest.is_dir():
+                dest.mkdir(exist_ok=True, parents=True)
+            else:
+                dest.parent.mkdir(exist_ok=True, parents=True)
+        copied = shutil.copy2(source, dest)
+    return Path(copied)
+
+
+@str2pathlib
+def rm_r(folder: Directory):
+    """Emulates `rm -r <folder>` command
+
+    Ignores not existing directory
+    """
+    if not folder.exists():
+        return
+    shutil.rmtree(folder)
+
+
+@str2pathlib
+def dir_size(
+    directory: Directory, units: str = "Bytes", check_softlinks: bool = True
+) -> float:
+    """Calculates the total size of files within the directory.
+
+    Args:
+        root: target directory
+        units: size units (from .constants.SIZE_CONSTANTS.keys())
+        check_softlinks: flag indicating whether to count files by links or not
+    """
+    total_size = 0
+    for f in directory.glob("**/*"):
+        if f.is_file():
+            total_size += f.stat().st_size / SIZE_CONSTANTS[units]
+        if check_softlinks and f.is_symlink():
+            src = f.resolve()
+            total_size += dir_size(src, units)
+    return total_size
+
+
+def daterange(
+    start_date: Union[str, datetime],
+    end_date: Union[str, datetime],
+    *,
+    include_last: bool = False,
+) -> Iterator[date]:
+    """Generates dates in requested range
+
+    Note:
+        Date format is ISO: 'yyyy-mm-dd'
+    """
+    if isinstance(start_date, str):
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    if isinstance(end_date, str):
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+    days = (end_date - start_date).days
+    if include_last:
+        days += 1
+
+    for i in range(days):
+        yield start_date + timedelta(i)
---
+++ packages/somepytools/somepytools-1.4.0-py3-none-any.whl/somepytools/video.py
@@ -0,0 +1,128 @@
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Iterable, Sequence, Union
+
+from .general import str2pathlib
+from .typing import Array, File
+
+
+try:
+    import cv2
+except ModuleNotFoundError:
+    pass
+
+
+@contextmanager
+@str2pathlib
+def open_video(video_path: File, mode: str = "r", *args):
+    """Context manager to work with cv2 videos
+
+    Mimics python's standard `open` function
+
+    Args:
+        video_path: path to video to open
+        mode: either 'r' for read or 'w' write
+        args: additional arguments passed to Capture or Writer
+            according to OpenCV documentation
+
+    Returns:
+        cv2.VideoCapture or cv2.VideoWriter depending on mode
+
+    Example of writing:
+        open_video(
+            out_path,
+            'w',
+            cv2.VideoWriter_fourcc(*'XVID'), # fourcc
+            15, # fps
+            (width, height), # frame size
+        )
+    """
+    if mode == "r":
+        video = cv2.VideoCapture(video_path.as_posix(), *args)
+    elif mode == "w":
+        video = cv2.VideoWriter(video_path.as_posix(), *args)
+    else:
+        raise ValueError(f'Incorrect open mode {mode!r}; "r" or "w" expected!')
+
+    if not video.isOpened():
+        raise ValueError(f"Video {video_path} is not opened!")
+
+    try:
+        yield video
+    finally:
+        video.release()
+
+
+@str2pathlib
+def write_video(
+    images: Sequence[Array],
+    video_path: File,
+    codec_code: str = "XVID",
+    fps: int = 2,
+    is_color=True,
+):
+    """Writes images to video file by given path
+
+    Args:
+        images: List of RGB or binary images.
+        video_path: The name of the file to save the video to.
+        codec_code: FourCC - a 4-byte code used to specify the video codec.
+        fps: Framerate of the created video stream.
+        is_color: RGB images or not.
+
+    """
+    fourcc = cv2.VideoWriter_fourcc(*codec_code)
+    height, width, channels = images[0].shape
+    with open_video(video_path, "w", fourcc, fps, (width, height), is_color) as capture:
+        for frame in images:
+            if is_color:
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            capture.write(frame)
+
+
+@str2pathlib
+def frames(video: Union[File, "cv2.VideoCapture"], rgb: bool = True) -> Iterable[Array]:
+    """Generator of frames from the video provided
+
+    Args:
+        video: either Path or Video capture to read frames from
+            in former case file will be opened with :py:funct:`.open_video`
+        rgb: if True returns RGB image, else BGR - native to opencv format
+
+    Yields:
+        Frames of video in (H, W, C) format
+    """
+    if isinstance(video, Path) or isinstance(video, str):
+        with open_video(video) as capture:
+            yield from frames(capture, rgb)
+    else:
+        while True:
+            retval, frame = video.read()
+            if not retval:
+                break
+            if rgb:
+                frame = frame[:, :, ::-1]
+            yield frame
+
+
+@str2pathlib
+def get_meta(video_path: File, count_frames: bool = True):
+    """Extracts main video meta data as dict
+
+    Eliminates a need in ugly OpenCV constants
+
+    Args:
+        video_path: video to get info from
+        count_frames: if True - reads frames from video and count them
+            Warning: can be long to execute on big files
+    """
+    with open_video(video_path) as video:
+        real_frame_count = sum(1 for _ in frames(video)) if count_frames else None
+        return {
+            "width": video.get(cv2.CAP_PROP_FRAME_WIDTH),
+            "height": video.get(cv2.CAP_PROP_FRAME_HEIGHT),
+            "fps": video.get(cv2.CAP_PROP_FPS),
+            "fourcc": video.get(cv2.CAP_PROP_FOURCC),
+            "frame_count_meta": video.get(cv2.CAP_PROP_FRAME_COUNT),
+            "frame_count": real_frame_count,
+        }
