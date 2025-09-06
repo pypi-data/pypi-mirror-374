f548b83a132b5833d6d55e73e8eb8996ec0a3f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 16:05:49 +0000

    Add package warpcoder/0.4.4/warpcoder-0.4.4.tar.gz

---
+++ packages/warpcoder/warpcoder-0.4.4.tar.gz/warpcoder-0.4.4/src/warpcoder/__init__.py
@@ -0,0 +1,11 @@
+"""
+WarpCoder - Universal BDD Project Generator for Claude Code
+"""
+
+__version__ = "0.4.4"
+__author__ = "Warpcoders LLC"
+__email__ = "starshipagentic@gmail.com"
+
+from .main import main
+
+__all__ = ["main"]
