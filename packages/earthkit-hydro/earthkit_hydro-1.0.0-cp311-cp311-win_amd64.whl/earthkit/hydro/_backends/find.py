ad4d3206ba863bcd4ffa38c3d705f88c3f32fb60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:28 +0000

    Add package earthkit-hydro/1.0.0/earthkit_hydro-1.0.0-cp311-cp311-win_amd64.whl

---
+++ packages/earthkit-hydro/earthkit_hydro-1.0.0-cp311-cp311-win_amd64.whl/earthkit/hydro/_backends/find.py
@@ -0,0 +1,29 @@
+def get_array_backend(x):
+
+    if type(x) is str:
+        mod = x
+    else:
+        mod = type(x).__module__
+
+    if "torch" in mod:
+        from .torch_backend import TorchBackend
+
+        return TorchBackend()
+    elif "tensorflow" in mod:
+        from .tensorflow_backend import TFBackend
+
+        return TFBackend()
+    elif "jax" in mod:
+        from .jax_backend import JAXBackend
+
+        return JAXBackend()
+    elif "cupy" in mod:
+        from .cupy_backend import CuPyBackend
+
+        return CuPyBackend()
+    elif "numpy" in mod:
+        from .numpy_backend import NumPyBackend
+
+        return NumPyBackend()
+    else:
+        raise TypeError(f"Unsupported array type: {type(x)}")
