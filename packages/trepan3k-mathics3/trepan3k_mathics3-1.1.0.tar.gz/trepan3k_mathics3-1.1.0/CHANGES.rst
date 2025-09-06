26ad78c077dae2f5865466c7ac2a22de92e8dea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 05:06:53 +0000

    Add package trepan3k-mathics3/1.1.0/trepan3k_mathics3-1.1.0.tar.gz

---
+++ packages/trepan3k-mathics3/trepan3k_mathics3-1.1.0.tar.gz/trepan3k_mathics3-1.1.0/CHANGES.rst
@@ -0,0 +1,25 @@
+CHANGES
+=======
+
+1.1.0
+-----
+
+Revise for Mathics 9.0.0 release
+
+* Add ``mframe``: is like ``frame`` but for Mathics3-related frame motion
+* Add ``mprogram``: is like ``program`` but for Mathics3-related frame
+* Add ``abort``: Aborts current Mathics3 command
+
+
+1.0.0
+-----
+
+01/20/2025
+
+First public release. Add trepan3k debugger command functions
+
+* ``mathics3``: Go into a Mathic3 shell
+* ``mbacktrace``: backtrace showing interesting Mathics3 frame interpretation
+* ``mup``: frame up allowing for jumping to interesting Mathics3 frame
+* ``mdown``, frame down allowing for jumping to interesting Mathics3 frame
+* ``printelement``: print customized for Mathics3 objects
---
+++ packages/trepan3k-mathics3/trepan3k_mathics3-1.1.0.tar.gz/trepan3k_mathics3-1.1.0/trepan3k_mathics3/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2024-2025 Rocky Bernstein <rocky@gnu.org>
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+trepan3k plugin which adds command `mathics3`.
+This adds Mathics3 debugging support from inside trepan3k.
+
+Authors: Rocky Bernstein rb@dustyfeet.com
+
+License: GPL3
+
+"""
+__docformat__ = "restructuredtext"
+from trepan3k_mathics3 import mathics3, mabort, mbacktrace, mdown, mframe, mprogram, mup, printelement
+from trepan3k_mathics3.version import __version__
+
+mdown.DownCommand.__doc__ = mdown.DownCommand.__doc__.replace("down", "mdown")
+mframe.FrameCommand.__doc__ = mframe.FrameCommand.__doc__.replace("frame", "mframe")
+mup.UpCommand.__doc__ = mup.UpCommand.__doc__.replace("up", "mup")
+
+__all__ = ["__version__", "mabort", "mathics3", "mbacktrace", "mdown", "mframe", "mprogram", "mup", "printelement"]
