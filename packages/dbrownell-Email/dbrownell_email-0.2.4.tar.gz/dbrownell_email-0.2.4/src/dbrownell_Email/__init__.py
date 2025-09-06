910ed5aa2551224b10ecfe8482953a80d6e8378a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:07:32 +0000

    Add package dbrownell-Email/0.2.4/dbrownell_email-0.2.4.tar.gz

---
+++ packages/dbrownell-Email/dbrownell_email-0.2.4.tar.gz/dbrownell_email-0.2.4/src/dbrownell_Email/__init__.py
@@ -0,0 +1,14 @@
+# noqa: D104
+
+# Wheel names will be generated according to this value. Do not manually modify this value; instead
+# update it according to committed changes by running this command from the root of the repository:
+#
+#   uv run python -m AutoGitSemVer.scripts.UpdatePythonVersion ./src/dbrownell_Email/__init__.py ./src
+#
+__version__ = "0.2.4"
+
+from .SmtpMailer import SmtpMailer
+
+__all__ = [
+    "SmtpMailer",
+]
