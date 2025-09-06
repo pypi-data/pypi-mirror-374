81a3a9d53d3cebe22e72c979a25323b89d80a58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:25 +0000

    Add package deltacat/2.0.0.post2/deltacat-2.0.0.post2-py3-none-any.whl

---
+++ packages/deltacat/deltacat-2.0.0.post2-py3-none-any.whl/deltacat/tests/conftest.py
@@ -0,0 +1,25 @@
+import tempfile
+
+import pytest
+from deltacat.catalog import CatalogProperties
+from deltacat.tests.test_utils.filesystem import temp_dir_autocleanup
+
+
+@pytest.fixture
+def temp_dir():
+    """
+    Temp dir which is removed after usage
+    note that each method which is injected with temp_dir will get a separate new tmp directory
+    """
+    with temp_dir_autocleanup() as tmp_dir:
+        yield tmp_dir
+
+
+@pytest.fixture
+def keep_temp_dir():
+    return tempfile.mkdtemp()
+
+
+@pytest.fixture
+def temp_catalog_properties(temp_dir):
+    return CatalogProperties(root=temp_dir)
