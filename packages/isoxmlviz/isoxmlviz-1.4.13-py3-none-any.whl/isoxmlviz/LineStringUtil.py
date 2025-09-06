c6fe72f6353abf3e7bd0a128a932a76b095b2f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 03:13:11 +0000

    Add package isoxmlviz/1.4.13/isoxmlviz-1.4.13-py3-none-any.whl

---
+++ packages/isoxmlviz/isoxmlviz-1.4.13-py3-none-any.whl/isoxmlviz/LineStringUtil.py
@@ -0,0 +1,148 @@
+from itertools import tee
+
+# import shapely.geometry as SHP
+from shapely.geometry import Point, Polygon, MultiPoint, LineString
+from shapely.ops import linemerge
+'''utility function to iterate a list in pairs'''
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+'''
+https://stackoverflow.com/questions/11907947/how-to-check-if-a-point-lies-on-a-line-between-2-other-points
+'''
+
+
+def is_point_on_line(a, b, point):
+    dxl = b[0] - a[0]
+    dyl = b[1] - a[1]
+    dxp = point[0] - a[0]
+    dyp = point[1] - a[1]
+    c = ((dxp * dxl + dyp * dyl) / (dxl * dxl + dyl * dyl))
+    return 0 < c < 1
+
+
+'''
+extract line segments from a line and a list of polygon restricting the line to within the polygon. May result in many line segments
+'''
+
+def get_coordinates(lines: list[LineString]):
+    return [l.coords for l in lines]
+
+def extract_lines_within(base_line: LineString, polygons: list[Polygon]):
+    lines = [base_line]
+    if len(polygons) == 0:
+        return  lines
+
+    for p in polygons:
+        lines = [extract_line_within(line, p) for line in lines]
+        lines = [item for sublist in lines for item in sublist]
+    return lines
+
+
+'''
+extract line segments from a line and a polygon restricting the line to within the polygon. May result in many line segments
+'''
+
+
+def extract_line_within(base_line: LineString, polygon: Polygon):
+    inter2 = base_line.intersection(polygon.boundary)
+    lines = []
+
+    # from matplotlib import pyplot as plt
+    # plt.scatter([p.coords.xy[0] for p in inter2], [p.coords.xy[1] for p in inter2], color="red")
+
+    expanded_points = []
+
+    interpolated_points = []
+
+    if type(inter2) is Point:
+        interpolated_points.append(inter2)
+    elif type(inter2) is MultiPoint:
+        for p in inter2.geoms:
+            interpolated_points.append(p)
+
+    for a, b in pairwise(base_line.coords):
+        expanded_points.append(a)
+
+        # check for points between and add them with key as distance from a
+        points_between = [(Point(a).distance(Point(p)), p) for p in interpolated_points if
+                          is_point_on_line(a, b, p.coords[0]) and LineString((a, b)).distance(Point(p)) < 0.01]
+
+        # sort the points between with distance from a
+        points_between.sort(key=lambda x: x[0])
+
+        for p in points_between:
+            # add point between in the order of distance between
+            expanded_points.append(p[1])
+            interpolated_points = [g for g in interpolated_points if g != p[1]]
+
+        expanded_points.append(b)
+
+    for a, b in pairwise(expanded_points):
+        line = LineString([a, b])
+
+        mid_point = line.interpolate(0.5, normalized=True)
+        if mid_point.within(polygon):
+            lines.append(line)
+    return lines
+
+
+def insert_intersection_in_polygon(polygon: Polygon, line: LineString):
+    # Get the intersection points
+    intersection = polygon.intersection(line)
+
+    # Collect all intersection points
+    if intersection.is_empty:
+        print("No intersection")
+        new_polygon = polygon
+    else:
+        intersection_points = []
+        intersection_points.append(Point(intersection.coords[-1]))
+        # if intersection.geom_type == "Point":
+        #     intersection_points = [intersection]
+        # elif intersection.geom_type == "MultiPoint":
+        #     intersection_points = list(intersection.geoms)
+        # elif intersection.geom_type in ["LineString", "MultiLineString"]:
+        #     # Convert LineString(s) to Points
+        #     merged = linemerge(intersection)
+        #     if merged.geom_type == "LineString":
+        #         intersection_points = [Point(merged.coords[0]), Point(merged.coords[-1])]
+        #     elif merged.geom_type == "MultiLineString":
+        #         intersection_points = []
+        #         for ls in merged.geoms:
+        #             intersection_points.append(Point(ls.coords[0]))
+        #             intersection_points.append(Point(ls.coords[-1]))
+        #     else:
+        #         raise ValueError("Unexpected merged type:", merged.geom_type)
+        # else:
+        #     raise ValueError("Unexpected intersection type:", intersection.geom_type)
+
+        # Original polygon coordinates (excluding the closing point)
+        coords = list(polygon.exterior.coords[:-1])
+
+        # Insert intersection points into the right place
+        new_coords = []
+        for i in range(len(coords)):
+            a = Point(coords[i])
+            b = Point(coords[(i + 1) % len(coords)])
+            segment = LineString([a, b])
+            new_coords.append((a.x, a.y))
+
+            for ip in intersection_points:
+                if segment.distance(ip) < 1e-8 and segment.project(ip) > 0 and segment.project(ip) < segment.length:
+                    # Only insert if truly on the segment and not a vertex
+                    if (ip.x, ip.y) not in new_coords:
+                        new_coords.append((ip.x, ip.y))
+
+        # Re-close the polygon
+        new_coords.append(new_coords[0])
+        new_polygon = Polygon(new_coords)
+    return new_polygon
+
+
---
+++ packages/isoxmlviz/isoxmlviz-1.4.13-py3-none-any.whl/isoxmlviz/webmap.py
@@ -0,0 +1,221 @@
+import folium
+import pymap3d
+
+import logging
+
+logger = logging.getLogger("isoxmlviz")
+logger.setLevel(logging.DEBUG)
+
+ell_wgs84 = pymap3d.Ellipsoid.from_name('wgs84')
+
+
+def flatten(matrix):
+    return [item for row in matrix for item in row]
+
+
+def transform(shape, func):
+    ''' Apply a function to every coordinate in a geometry.
+    '''
+    from shapely.geometry import Point, LineString, Polygon, GeometryCollection
+    construct = shape.__class__
+
+    if shape.geom_type.startswith('Multi'):
+        parts = [transform(geom, func) for geom in shape.geoms]
+        return construct(parts)
+
+    if shape.geom_type in ('LineString'):
+        return construct(map(func, shape.coords))
+    if shape.geom_type in ('Point'):
+        return construct(flatten(map(func, shape.coords)))
+
+    if shape.geom_type == 'Polygon':
+        exterior = map(func, shape.exterior.coords)
+        rings = [list(map(func, ring.coords)) for ring in shape.interiors]
+        return construct(exterior, rings)
+
+    if shape.geom_type == 'GeometryCollection':
+        return construct()
+
+    raise ValueError('Unknown geometry type, "%s"' % shape.geom_type)
+
+
+class WebMap:
+    def __init__(self, ref_lat, ref_lng) -> None:
+        super().__init__()
+        self.ref_lat = ref_lat
+        self.ref_lng = ref_lng
+        self.m = folium.Map(location=[40.730610, -73.935242],
+                            zoom_start=12, control_scale=True, prefer_canvas=True, max_zoom=22)
+
+        folium.TileLayer('openstreetmap').add_to(self.m)
+        tile = folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Satellite',
+            overlay=False,
+            control=True,
+            max_zoom=22
+        ).add_to(self.m)
+
+        # Add custom basemaps to folium
+        basemaps = {
+            'Google Maps': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Maps',
+                overlay=False,
+                control=True,
+                show=False
+            ),
+            'Google Satellite': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=True,
+                control=True,
+                show= False
+            ),
+            'Google Terrain': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Terrain',
+                overlay=True,
+                control=True
+            ),
+            'Google Satellite Hybrid': folium.TileLayer(
+                tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+                attr='Google',
+                name='Google Satellite',
+                overlay=False,
+                control=True,
+                show=False
+            ),
+            'Esri Satellite': folium.TileLayer(
+                tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+                attr='Esri',
+                name='Esri Satellite',
+                overlay=True,
+                control=True
+            )
+        }
+
+        def add_ee_layer(self, ee_object, vis_params, name):
+
+            try:
+                # display ee.Image()
+                if isinstance(ee_object, ee.image.Image):
+                    map_id_dict = ee.Image(ee_object).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.ImageCollection()
+                elif isinstance(ee_object, ee.imagecollection.ImageCollection):
+                    ee_object_new = ee_object.mosaic()
+                    map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.Geometry()
+                elif isinstance(ee_object, ee.geometry.Geometry):
+                    folium.GeoJson(
+                        data=ee_object.getInfo(),
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+                # display ee.FeatureCollection()
+                elif isinstance(ee_object, ee.featurecollection.FeatureCollection):
+                    ee_object_new = ee.Image().paint(ee_object, 0, 2)
+                    map_id_dict = ee.Image(ee_object_new).getMapId(vis_params)
+                    folium.raster_layers.TileLayer(
+                        tiles=map_id_dict['tile_fetcher'].url_format,
+                        attr='Google Earth Engine',
+                        name=name,
+                        overlay=True,
+                        control=True
+                    ).add_to(self)
+
+            except:
+                logger.error("Could not display {}".format(name))
+
+        # Add EE drawing method to folium.
+        # folium.Map.add_ee_layer = add_ee_layer
+
+        vis_params = {
+            'min': 0,
+            'max': 4000,
+            'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+
+        # Create a folium map object.
+        my_map = folium.Map(location=[20, 0], zoom_start=3, height=500)
+
+        # Add custom basemaps
+        basemaps['Google Maps'].add_to(self.m)
+        basemaps['Google Satellite Hybrid'].add_to(self.m)
+
+        # Add the elevation model to the map object.
+        # self.m.add_ee_layer(dem.updateMask(dem.gt(0)), vis_params, 'DEM')
+
+
+
+    def set_refernce(self, ref_lat, ref_lng):
+        self.ref_lat = ref_lat
+        self.ref_lng = ref_lng
+
+    def add(self, geom, tooltip=None, style={'fillColor': 'red', 'lineColor': '#228B22', 'opacity': '1'}, group=None):
+        mkt = lambda x: folium.features.Popup(tooltip) if tooltip else None
+
+        ff = lambda c: [c[1], c[0], c[2]]
+        folium.GeoJson(transform(geom, lambda p: ff(
+            pymap3d.enu2geodetic(p[0], p[1], 0, self.ref_lat, self.ref_lng, 0, ell=ell_wgs84, deg=True))),
+                       style_function=lambda x: style, tooltip=mkt(tooltip), popup=mkt(tooltip)).add_to(
+            self.m if not group else group)
+
+    def addPoly(self, geom, tooltip=None, style={'color': 'red', 'lineColor': '#228B22', 'opacity': '1'}, group=None):
+        mkt = lambda x: folium.features.Popup(tooltip) if tooltip else None
+        ff = lambda c: [c[0], c[1]]
+        g = transform(geom, lambda p: ff(
+            pymap3d.enu2geodetic(p[0], p[1], 0, self.ref_lat, self.ref_lng, 0, ell=ell_wgs84, deg=True)))
+
+        ff = lambda c: [c[1], c[0], c[2]]
+        # folium.GeoJson(transform(geom, lambda p: ff(
+        #     pymap3d.enu2geodetic(p[0], p[1], 0, self.ref_lat, self.ref_lng, 0, ell=ell_wgs84, deg=True))),
+        #                style_function=lambda x: style,tooltip=mkt(tooltip),popup=mkt(tooltip)).add_to(self.m)
+        folium.PolyLine(locations=g.coords, tooltip=mkt(tooltip), color=style['color'],
+                        opacity=style['opacity'] if 'opacity' in style else 1).add_to(self.m if not group else group)
+
+    def add_marker(self, name, geom, group=None):
+        ff = lambda c: [c[0], c[1], c[2]]
+        g = transform(geom, lambda p: ff(
+            pymap3d.enu2geodetic(p[0], p[1], 0, self.ref_lat, self.ref_lng, 0, ell=ell_wgs84, deg=True)))
+
+        folium.Marker(location=[c[:-1] for c in g.coords][0], popup=folium.features.Popup(name)).add_to(
+            self.m if not group else group)
+
+    def add_point(self, name, geom, group=None, style={'color': 'red', 'lineColor': '#228B22', 'opacity': '1'}):
+        ff = lambda c: [c[0], c[1], c[2]]
+        g = transform(geom, lambda p: ff(
+            pymap3d.enu2geodetic(p[0], p[1], 0, self.ref_lat, self.ref_lng, 0, ell=ell_wgs84, deg=True)))
+
+        folium.CircleMarker(location=[c[:-1] for c in g.coords][0],
+                            radius=2,
+                            weight=5, color=style['color'],
+                            opacity=style['opacity'] if 'opacity' in style else 1,
+                            popup=folium.features.Popup(name)).add_to(self.m if not group else group)
+
+    def create_group(self, name):
+        return folium.FeatureGroup(name=name).add_to(self.m)
+
+    def save(self, path):
+        folium.LayerControl().add_to(self.m)
+        self.m.fit_bounds(self.m.get_bounds(), padding=(30, 30))
+        self.m._repr_html_()
+        self.m.save(path)
