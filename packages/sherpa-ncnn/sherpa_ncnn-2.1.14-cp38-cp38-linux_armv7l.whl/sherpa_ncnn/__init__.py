1e0f44e884e7d985d0ee2b6d5061780b8574fe05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 05:07:00 +0000

    Add package sherpa-ncnn/2.1.14/sherpa_ncnn-2.1.14-cp38-cp38-linux_armv7l.whl

---
+++ packages/sherpa-ncnn/sherpa_ncnn-2.1.14-cp38-cp38-linux_armv7l.whl/sherpa_ncnn/__init__.py
@@ -0,0 +1,12 @@
+from sherpa_ncnn.lib._sherpa_ncnn import (
+    Alsa,
+    Display,
+    OfflineTtsModelConfig,
+    OfflineTtsVitsModelConfig,
+    OfflineTtsConfig,
+    OfflineTts,
+    TtsArgs,
+)
+
+from .recognizer import Recognizer
+__version__ = '2.1.14'
