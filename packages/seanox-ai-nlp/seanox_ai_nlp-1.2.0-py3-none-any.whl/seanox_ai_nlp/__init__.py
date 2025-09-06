56421d8d02f81cc00f5575efa213ab2cecb4f24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:06 +0000

    Add package seanox-ai-nlp/1.2.0/seanox_ai_nlp-1.2.0-py3-none-any.whl

---
+++ packages/seanox-ai-nlp/seanox_ai_nlp-1.2.0-py3-none-any.whl/seanox_ai_nlp/__init__.py
@@ -0,0 +1,47 @@
+# seanox_ai_nlp/__init__.py
+
+from seanox_ai_nlp.units import (
+    UNIT_PATTERN,
+    UNIT_CLASSIFICATION_PATTERN,
+    UNIT_OPERATORS_PATTERN,
+    UNIT_VALIDATION_PATTERN,
+    UNIT_EXPRESSION_VALIDATION_PATTERN,
+    NUMERIC_PATTERN,
+    NUMERIC_DIMENSIONAL_SEPARATORS_PATTERN,
+    NUMERIC_VALIDATION_PATTERN,
+    NUMERIC_EXPRESSION_VALIDATION_PATTERN,
+    units,
+    SpacingMode,
+    spacing
+)
+
+from .synthetics import (
+    synthetics,
+    Synthetic,
+    TemplateException,
+    TemplateConditionException,
+    TemplateSyntaxException
+)
+
+__all__ = [
+    # units
+    "UNIT_PATTERN",
+    "UNIT_CLASSIFICATION_PATTERN",
+    "UNIT_OPERATORS_PATTERN",
+    "UNIT_VALIDATION_PATTERN",
+    "UNIT_EXPRESSION_VALIDATION_PATTERN",
+    "NUMERIC_PATTERN",
+    "NUMERIC_DIMENSIONAL_SEPARATORS_PATTERN",
+    "NUMERIC_VALIDATION_PATTERN",
+    "NUMERIC_EXPRESSION_VALIDATION_PATTERN",
+    "units",
+    "SpacingMode",
+    "spacing",
+
+    # synthetics
+    "synthetics",
+    "Synthetic",
+    "TemplateException",
+    "TemplateConditionException",
+    "TemplateSyntaxException"
+]
---
+++ packages/seanox-ai-nlp/seanox_ai_nlp-1.2.0-py3-none-any.whl/seanox_ai_nlp/synthetics/__init__.py
@@ -0,0 +1,17 @@
+# seanox_ai_nlp/synthetics/__init__.py
+
+from .synthetics import (
+    synthetics,
+    Synthetic,
+    TemplateException,
+    TemplateConditionException,
+    TemplateSyntaxException
+)
+
+__all__ = [
+    "synthetics",
+    "Synthetic",
+    "TemplateException",
+    "TemplateConditionException",
+    "TemplateSyntaxException"
+]
