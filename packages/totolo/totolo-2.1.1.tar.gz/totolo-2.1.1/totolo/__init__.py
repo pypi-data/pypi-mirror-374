18c9a202e171fc5fb240797a6a0dbdd518476e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 03:13:47 +0000

    Add package totolo/2.1.1/totolo-2.1.1.tar.gz

---
+++ packages/totolo/totolo-2.1.1.tar.gz/totolo-2.1.1/totolo/__init__.py
@@ -0,0 +1,13 @@
+"""
+The Python interface to themeontology.org.
+"""
+from totolo.impl.api import TORemote, empty, files
+
+
+remote = TORemote()
+__version__ = "2.1.1"
+__ALL__ = [
+    empty,
+    files,
+    remote,
+]
