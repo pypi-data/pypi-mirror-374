d6f64a15a63657f7be8cb8e52c55000c2b83b281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 14:06:15 +0000

    Add package reflex-azure-auth/0.1.1/reflex_azure_auth-0.1.1-py3-none-any.whl

---
+++ packages/reflex-azure-auth/reflex_azure_auth-0.1.1-py3-none-any.whl/reflex_azure_auth/__init__.py
@@ -0,0 +1,25 @@
+"""Public package exports for reflex_azure_auth."""
+
+from .config import azure_issuer_uri, azure_valid_tenant_ids, client_id, client_secret
+from .endpoints import register_auth_endpoints
+from .message_listener import WindowMessage, message_listener
+from .oidc import azure_issuer_endpoint, verify_jwt
+from .state import AzureAuthState
+from .types import AzureUserInfo, user_info_from_dict
+from .ui import azure_login_button
+
+__all__ = [
+    "AzureAuthState",
+    "AzureUserInfo",
+    "WindowMessage",
+    "azure_issuer_endpoint",
+    "azure_issuer_uri",
+    "azure_login_button",
+    "azure_valid_tenant_ids",
+    "client_id",
+    "client_secret",
+    "message_listener",
+    "register_auth_endpoints",
+    "user_info_from_dict",
+    "verify_jwt",
+]
