e37712f3090da30f653d26c19fb2d1c1e771272d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 07:06:40 +0000

    Add package wnnet/2.6.1/wnnet-2.6.1-py3-none-any.whl

---
+++ packages/wnnet/wnnet-2.6.1-py3-none-any.whl/wnnet/__init__.py
@@ -0,0 +1,11 @@
+"""
+A package of python routines to handle webnucleo networks.
+"""
+
+import wnnet.consts
+import wnnet.nuc
+import wnnet.reac
+import wnnet.net
+import wnnet.graph
+import wnnet.flows
+import wnnet.zones
