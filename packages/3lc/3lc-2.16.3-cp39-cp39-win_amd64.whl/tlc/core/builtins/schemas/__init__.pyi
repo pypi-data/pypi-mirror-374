47e8d33c7202e6fea02ee85f24411d6d04d979e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:26 +0000

    Add package 3lc/2.16.3/3lc-2.16.3-cp39-cp39-win_amd64.whl

---
+++ packages/3lc/3lc-2.16.3-cp39-cp39-win_amd64.whl/tlc/core/builtins/schemas/__init__.pyi
@@ -0,0 +1,6 @@
+from .bounding_box_list_schema import BoundingBoxListSchema as BoundingBoxListSchema
+from .rgb_image_schema import rgb_image_schema as rgb_image_schema
+from .schemas import CIFAR10LabelSchema as CIFAR10LabelSchema, COCOLabelSchema as COCOLabelSchema, CategoricalLabelSchema as CategoricalLabelSchema, EpochSchema as EpochSchema, ExampleIdSchema as ExampleIdSchema, FloatVector2Schema as FloatVector2Schema, FloatVector3Schema as FloatVector3Schema, ForeignTableIdSchema as ForeignTableIdSchema, IterationSchema as IterationSchema, SampleWeightSchema as SampleWeightSchema, XyzSchema as XyzSchema
+from .segmentation_schema import SegmentationSchema as SegmentationSchema
+from .top_n_prediction_schema import top_n_prediction_schema as top_n_prediction_schema
+from .voxelset_schema import voxelset_schema as voxelset_schema
---
+++ packages/3lc/3lc-2.16.3-cp39-cp39-win_amd64.whl/tlcsaas/pyarmor_runtime_005329/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 9.1.8 (pro), 005329, 2025-09-05T12:06:52.731153
+from .pyarmor_runtime import __pyarmor__
