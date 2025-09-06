d0ede30da3c6325d66ee1725e9ca75b2667522d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 06:06:51 +0000

    Add package adopt/0.5.0/adopt-0.5.0-py3-none-any.whl

---
+++ packages/adopt/adopt-0.5.0-py3-none-any.whl/adopt/cli/backlog/options.py
@@ -0,0 +1,12 @@
+# custom category type and option for backlog commands
+import click
+
+category_type = click.Choice(['Story', 'Feature', 'Epic'], case_sensitive=False)
+category_option = click.option(
+    '--category',
+    '-c',
+    help='Backlog Category',
+    default='Story',
+    type=category_type,
+    required=True,
+)
