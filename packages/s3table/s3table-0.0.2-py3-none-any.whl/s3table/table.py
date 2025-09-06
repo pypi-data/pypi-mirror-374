581f76f2ecbf83ee281973fb85d182219c70dbaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:05:58 +0000

    Add package s3table/0.0.2/s3table-0.0.2-py3-none-any.whl

---
+++ packages/s3table/s3table-0.0.2-py3-none-any.whl/s3table/table.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from pyiceberg.table import Table
+from pyiceberg.typedef import EMPTY_DICT, Properties
+from pyiceberg.io import load_file_io
+
+class S3Table(Table):
+    """Load a table directly from a metadata file in S3."""
+
+    def refresh(self) -> Table:
+        """Refresh the current table metadata."""
+        raise NotImplementedError("To be implemented")
+
+    @classmethod
+    def from_metadata(cls, metadata_location: str, properties: Properties = EMPTY_DICT) -> S3Table:
+        io = load_file_io(properties=properties, location=metadata_location)
+        file = io.new_input(metadata_location)
+
+        from pyiceberg.serializers import FromInputFile
+
+        metadata = FromInputFile.table_metadata(file)
+
+        from pyiceberg.catalog.noop import NoopCatalog
+
+        return cls(
+            identifier=("s3table", metadata_location),
+            metadata_location=metadata_location,
+            metadata=metadata,
+            io=load_file_io({**properties, **metadata.properties}),
+            catalog=NoopCatalog("s3table"),
+        )
