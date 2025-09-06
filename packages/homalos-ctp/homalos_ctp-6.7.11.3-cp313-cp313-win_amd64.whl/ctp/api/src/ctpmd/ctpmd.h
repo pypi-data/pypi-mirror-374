b7b80b4afe10c5a26f8947ce892023bdb17ab421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:05:58 +0000

    Add package homalos-ctp/6.7.11.3/homalos_ctp-6.7.11.3-cp313-cp313-win_amd64.whl

---
+++ packages/homalos-ctp/homalos_ctp-6.7.11.3-cp313-cp313-win_amd64.whl/ctp/api/src/ctpmd/ctpmd.h
@@ -0,0 +1,190 @@
+//系统
+#ifdef WIN32
+#include "stdafx.h"
+#endif
+
+#include "ctp.h"
+#include "pybind11/pybind11.h"
+#include "ctp/api/include/ThostFtdcMdApi.h"
+
+using namespace pybind11;
+
+//常量
+#define ONFRONTCONNECTED 0
+#define ONFRONTDISCONNECTED 1
+#define ONHEARTBEATWARNING 2
+#define ONRSPUSERLOGIN 3
+#define ONRSPUSERLOGOUT 4
+#define ONRSPQRYMULTICASTINSTRUMENT 5
+#define ONRSPERROR 6
+#define ONRSPSUBMARKETDATA 7
+#define ONRSPUNSUBMARKETDATA 8
+#define ONRSPSUBFORQUOTERSP 9
+#define ONRSPUNSUBFORQUOTERSP 10
+#define ONRTNDEPTHMARKETDATA 11
+#define ONRTNFORQUOTERSP 12
+
+
+///-------------------------------------------------------------------------------------
+///C++ SPI的回调函数的继承实现
+///-------------------------------------------------------------------------------------
+
+//API的继承实现
+class MdApi : public CThostFtdcMdSpi
+{
+private:
+	CThostFtdcMdApi* api;				//API对象
+	thread task_thread;					//工作线程指针（向python推送数据）
+	TaskQueue task_queue;			//任务队列
+	bool active = false;				//活动状态
+
+public:
+	MdApi()
+	{
+	};
+
+	virtual ~MdApi()
+	{
+		if (this->active)
+		{
+			this->exit();
+		}
+	};
+
+	//-------------------------------------------------------------------------------------
+	//从CThostFtdcMdSpi继承的C++回调函数
+	//-------------------------------------------------------------------------------------
+
+	virtual void OnFrontConnected();
+
+	virtual void OnFrontDisconnected(int nReason);
+
+	virtual void OnHeartBeatWarning(int nTimeLapse);
+
+	virtual void OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspUserLogout(CThostFtdcUserLogoutField *pUserLogout, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspQryMulticastInstrument(CThostFtdcMulticastInstrumentField *pMulticastInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspUnSubMarketData(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRspUnSubForQuoteRsp(CThostFtdcSpecificInstrumentField *pSpecificInstrument, CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast);
+
+	virtual void OnRtnDepthMarketData(CThostFtdcDepthMarketDataField *pDepthMarketData);
+
+	virtual void OnRtnForQuoteRsp(CThostFtdcForQuoteRspField *pForQuoteRsp);
+
+
+	//-------------------------------------------------------------------------------------
+	//工作线程处理函数
+	//-------------------------------------------------------------------------------------
+
+	void processTask();
+
+	void processFrontConnected(Task *task);
+
+	void processFrontDisconnected(Task *task);
+
+	void processHeartBeatWarning(Task *task);
+
+	void processRspUserLogin(Task *task);
+
+	void processRspUserLogout(Task *task);
+
+	void processRspQryMulticastInstrument(Task *task);
+
+	void processRspError(Task *task);
+
+	void processRspSubMarketData(Task *task);
+
+	void processRspUnSubMarketData(Task *task);
+
+	void processRspSubForQuoteRsp(Task *task);
+
+	void processRspUnSubForQuoteRsp(Task *task);
+
+	void processRtnDepthMarketData(Task *task);
+
+	void processRtnForQuoteRsp(Task *task);
+
+
+	//-------------------------------------------------------------------------------------
+	//Python回调函数
+	//data：回调函数的数据字典
+	//error：回调函数的错误字典
+	//id：请求id
+	//last：是否为最后返回
+	//i：整数
+	//-------------------------------------------------------------------------------------
+
+	virtual void onFrontConnected() {};
+
+	virtual void onFrontDisconnected(int reqid) {};
+
+	virtual void onHeartBeatWarning(int reqid) {};
+
+	virtual void onRspUserLogin(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspUserLogout(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspQryMulticastInstrument(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspError(const dict &error, int reqid, bool last) {};
+
+	virtual void onRspSubMarketData(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspUnSubMarketData(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspSubForQuoteRsp(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRspUnSubForQuoteRsp(const dict &data, const dict &error, int reqid, bool last) {};
+
+	virtual void onRtnDepthMarketData(const dict &data) {};
+
+	virtual void onRtnForQuoteRsp(const dict &data) {};
+
+	//-------------------------------------------------------------------------------------
+	//主动函数
+	//-------------------------------------------------------------------------------------
+
+	void createFtdcMdApi(string pszFlowPath="", bool bIsUsingUdp=false, bool bIsMulticast=false, bool bIsProductionMode=true);
+
+	string getApiVersion();
+
+	void release();
+
+	void init();
+
+	int join();
+
+	string getTradingDay();
+
+	void registerFront(string pszFrontAddress);
+
+	void registerNameServer(string pszNsAddress);
+
+	void registerFensUserInfo(const dict &req);
+
+	int exit();
+
+	int subscribeMarketData(string instrumentID);
+
+	int unSubscribeMarketData(string instrumentID);
+
+	int subscribeForQuoteRsp(string instrumentID);
+
+	int unSubscribeForQuoteRsp(string instrumentID);
+
+	int reqUserLogin(const dict &req, int reqid);
+
+	int reqUserLogout(const dict &req, int reqid);
+
+	int reqQryMulticastInstrument(const dict &req, int reqid);
+};
