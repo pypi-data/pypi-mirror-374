1d0ccd051200d28de415e214a0db28a884239d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 07:06:35 +0000

    Add package flooder/1.0rc5/flooder-1.0rc5.tar.gz

---
+++ packages/flooder/flooder-1.0rc5.tar.gz/flooder-1.0rc5/flooder/__init__.py
@@ -0,0 +1,18 @@
+from .io import save_to_disk
+from .core import flood_complex, generate_landmarks
+from .synthetic_data_generators import (
+    generate_swiss_cheese_points,
+    generate_annulus_points_2d,
+    generate_noisy_torus_points_3d,
+    generate_figure_eight_points_2d,
+)
+
+__all__ = [
+    "flood_complex",
+    "generate_landmarks",
+    "save_to_disk",
+    "generate_swiss_cheese_points",
+    "generate_annulus_points_2d",
+    "generate_noisy_torus_points_3d",
+    "generate_figure_eight_points_2d",
+]
---
+++ packages/flooder/flooder-1.0rc5.tar.gz/flooder-1.0rc5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flooder",
+    version="1.0rc5",
+    description="Flood complex PH",
+    author="Paolo Pellizzoni, Florian Graf, Martin Uray, Stefan Huber, Roland Kwitt",
+    author_email="roland.kwitt@gmail.com",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
