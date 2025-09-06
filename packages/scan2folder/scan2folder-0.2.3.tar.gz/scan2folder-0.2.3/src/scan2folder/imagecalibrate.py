e38b61f3e9a24d5ea51d0e7c0e532a485887c0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 07:05:27 +0000

    Add package scan2folder/0.2.3/scan2folder-0.2.3.tar.gz

---
+++ packages/scan2folder/scan2folder-0.2.3.tar.gz/scan2folder-0.2.3/src/scan2folder/imagecalibrate.py
@@ -0,0 +1,329 @@
+# This Python file uses the following encoding: utf-8
+import sys, io, os
+from PyQt6.QtWidgets import QWidget, QGraphicsScene, QGraphicsPixmapItem, QGraphicsView
+from PyQt6 import uic
+from PyQt6.QtGui import QPixmap, QImage
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot, QBuffer, Qt, QPoint, QRect, QPointF, QVariant, QMimeData, QSize, QIODevice
+from PIL import ImageEnhance, Image
+import numpy as np
+import cv2
+
+from scan2folder.rubberband import RubberBand
+
+#from ui_configform import Ui_Form
+class GraphicsScene(QGraphicsScene):
+    def __init__(self, parent=None):
+        super(GraphicsScene, self).__init__(parent)
+
+        self.parent = parent
+
+        self.origin = QPoint()
+        self.region = QRect()
+
+
+    def mousePressEvent(self, event):
+
+        if self.parent.rubberBand.isVisible():
+            self.parent.rubberBand.hide()
+
+    def mouseReleaseEvent(self, event):
+
+        if event.button() == Qt.LeftButton:
+           # self.rubberBand.hide()
+            self.region = QRect(self.origin, event.screenPos()).normalized()
+
+    def mouseMoveEvent(self, event):
+        #print('mouse move')
+        if not event.buttons() == Qt.LeftButton:
+            return
+
+
+    def dragEnterEvent(self,event):
+        #print(event.mimeData().text())
+        typ = event.mimeData().text().split('.').pop().lower()
+        if typ in self.parent.imageTypes:
+            event.acceptProposedAction()
+
+    ## needed to get drop events from outside!
+    def dragMoveEvent(self,event):
+        event.acceptProposedAction()
+
+    def dropEvent(self, event):
+        typ = event.mimeData().text().split('.').pop().lower()
+        if typ in self.parent.imageTypes:
+            event.accept()
+            imgpath = event.mimeData().text().lstrip("file:")
+            pix = QPixmap(imgpath)
+            #self.parent.pixmapItem.setPixmap(pix.scaled(int(pix.width()*0.5),int(pix.height()*0.5),Qt.KeepAspectRatio))
+            self.parent.pixmapItem.setPixmap(pix)
+            self.parent.ui.view.fitInView(self.parent.pixmapItem,Qt.KeepAspectRatio)
+            img = Image.open(imgpath)
+            #self.parent.pilBufferImg = img.resize((int(img.width*0.5),int(img.height*0.5)))
+            self.parent.pilBufferImg = img
+            print()
+            rect = self.parent.ui.view.mapFromScene(0.0,0.0, float(img.size[0]),float(img.size[1])).boundingRect()
+            self.parent.rubberBand.setMaximumSize(QSize(rect.width(),rect.height()))
+            self.parent.enhanceImage()
+
+
+
+class ConfigWindow(QWidget):
+    def __init__(self,parent=None):
+        super().__init__()
+        ui_file = os.path.join(os.path.dirname(__file__),"configform.ui")
+        self.ui = uic.loadUi(ui_file, self)
+        #self.ui = Ui_Form()
+        #self.ui.setupUi(self)
+
+        self.parent = parent
+
+        self.ui.brigthnesSlider.valueChanged.connect(self.brigthnesSlot)
+        self.ui.contrastSlider.valueChanged.connect(self.contrastSlot)
+
+        self.ui.brigthnessLcd.valueChanged.connect(self.setBrightSlider)
+        self.ui.contrastLcd.valueChanged.connect(self.setContastSlider)
+
+        self.ui.colorSlider.valueChanged.connect(self.colorSlot)
+        self.ui.sharpnessSlider.valueChanged.connect(self.sharpnessSlot)
+
+        self.ui.colorLcd.valueChanged.connect(self.setColorSlider)
+        self.ui.sharpnessLcd.valueChanged.connect(self.setSharpnessSlider)
+
+        self.ui.gammaLcd.valueChanged.connect(self.setGammaSlider)
+        self.ui.gammaSlider.valueChanged.connect(self.gammaSlot)
+
+        self.ui.checkCrop.stateChanged.connect(self.setCrop)
+
+
+        self.rubberBand = RubberBand(self.ui.view)
+        #self.rubberBand.setMaximumSize(QSize(397,552))
+        self.ui.view.setRubberBandSelectionMode(Qt.ItemSelectionMode.IntersectsItemBoundingRect)
+        self.ui.view.rubberBandChanged.connect(self.getCropRect)
+        self.rubberBand.cropSignal.connect(self.crop)
+        self.rubberBand.sizeSignal.connect(self.setSize)
+        self.rubberBand.hide()
+
+        self.scene = GraphicsScene(self)
+        self.pixmap = QPixmap()
+        self.pixmapItem = QGraphicsPixmapItem()
+        self.scene.addItem(self.pixmapItem)
+        self.ui.view.setScene(self.scene)
+        #self.ui.view.fitInView(self.pixmapItem,Qt.KeepAspectRatio)
+        #self.pixmap.load("Calibrate_Test_1.png")
+        self.qbuffer = QBuffer()
+
+        self.pilBufferPath = io.BytesIO()
+        self.pilBufferImg = Image.new('RGBA',(1,1))
+
+
+        self.bright = self.ui.brigthnesSlider.maximum()/2
+        self.contast = self.ui.contrastSlider.maximum()/2
+
+        self.imageTypes = ['png','jpg','tiff','tif']
+
+
+    @pyqtSlot(int)
+    def gammaSlot(self, value):
+        val =value/10
+        self.ui.gammaLcd.blockSignals(True)
+        self.ui.gammaLcd.setValue(val)
+        self.ui.gammaLcd.blockSignals(False)
+        self.enhanceImage()
+
+    @pyqtSlot(float)
+    def setGammaSlider(self,val):
+        value = val*10
+        self.ui.gammaSlider.blockSignals(True)
+        self.ui.gammaSlider.setValue(int(value))
+        self.ui.gammaSlider.blockSignals(False)
+        self.ui.gammaSlider.valueChanged.emit(int(value))
+
+    @pyqtSlot(int)
+    def colorSlot(self,value):
+        val =value/10
+        self.ui.colorLcd.blockSignals(True)
+        self.ui.colorLcd.setValue(val)
+        self.ui.colorLcd.blockSignals(False)
+        color = self.ui.colorSlider.value()/10
+        #self.enhanceImage(None, None,color,None)
+        self.enhanceImage()
+
+    @pyqtSlot(int)
+    def sharpnessSlot(self,value):
+        val =value/10
+        self.ui.sharpnessLcd.blockSignals(True)
+        self.ui.sharpnessLcd.setValue(val)
+        self.ui.sharpnessLcd.blockSignals(False)
+        sharp = self.ui.sharpnessSlider.value()/10
+        #self.enhanceImage(None, None,None,sharp)
+        self.enhanceImage()
+
+    @pyqtSlot(float)
+    def setSharpnessSlider(self,val):
+        value = val*10
+        self.ui.sharpnessSlider.blockSignals(True)
+        self.ui.sharpnessSlider.setValue(int(value))
+        self.ui.sharpnessSlider.blockSignals(False)
+        self.ui.sharpnessSlider.valueChanged.emit(int(value))
+
+    @pyqtSlot(float)
+    def setColorSlider(self,val):
+        value = val*10
+        self.ui.colorSlider.blockSignals(True)
+        self.ui.colorSlider.setValue(int(value))
+        self.ui.colorSlider.blockSignals(False)
+        self.ui.colorSlider.valueChanged.emit(int(value))
+
+    @pyqtSlot(int)
+    def brigthnesSlot(self,int):
+        
+        val =int/10
+        self.ui.brigthnessLcd.blockSignals(True)
+        self.ui.brigthnessLcd.setValue(val)
+        self.ui.brigthnessLcd.blockSignals(False)
+        contrast = self.ui.contrastSlider.value()/10
+        #self.enhanceImage(None,contrast,None,None)
+        self.enhanceImage()
+    
+    @pyqtSlot(int)
+    def contrastSlot(self,int):
+        val =int/10
+        self.ui.contrastLcd.blockSignals(True)
+        self.ui.contrastLcd.setValue(val)
+        self.ui.contrastLcd.blockSignals(False)
+        bright = self.ui.brigthnesSlider.value()/10
+        #self.enhanceImage(bright,None,None,None)
+        self.enhanceImage()
+
+    @pyqtSlot(float)
+    def setBrightSlider(self,val):
+        value = val*10
+        self.ui.brigthnesSlider.blockSignals(True)
+        self.ui.brigthnesSlider.setValue(int(value))
+        self.ui.brigthnesSlider.blockSignals(False)
+        self.ui.brigthnesSlider.valueChanged.emit(int(value))
+    
+    @pyqtSlot(float)
+    def setContastSlider(self,val):
+        value = val*10
+        self.ui.contrastSlider.blockSignals(True)
+        self.ui.contrastSlider.setValue(int(value))
+        self.ui.contrastSlider.blockSignals(False)
+        self.ui.contrastSlider.valueChanged.emit(int(value))
+
+    @pyqtSlot(QRect, QPointF, QPointF)
+    def getCropRect(self, rect, start, end):
+        pass
+        if self.ui.checkCrop.isChecked():
+            self.rubberBand.show()
+        srect = self.ui.view.mapToScene(rect).boundingRect().toRect()
+        #print(srect)
+        if rect.isEmpty():
+
+            self.rubberBand.show()
+        else:
+            self.rubberBand.setGeometry(rect)
+
+    @pyqtSlot()
+    def crop(self):
+        pass
+        self.parent.cropSize["left"]   = self.ui.cropX.value()
+        self.parent.cropSize["top"]    = self.ui.cropY.value()
+        self.parent.cropSize["width"]  = self.ui.cropW.value()
+        self.parent.cropSize["height"] = self.ui.cropH.value()
+        self.rubberBand.hide()
+
+
+    @pyqtSlot()
+    def setCrop(self):
+        pass
+        ## ensure that RubberBandDrag dragmode is set
+        ## if not rubberBand will not work correctly
+        self.ui.view.setDragMode( QGraphicsView.DragMode.RubberBandDrag)
+
+    @pyqtSlot(QRect)
+    def setSize(self,rect):
+        ## this the offset to subtract from srect
+        offset = 13
+        srect = self.ui.view.mapToScene(rect).boundingRect().toRect()
+        self.ui.cropX.setValue(srect.x()-offset)
+        self.ui.cropY.setValue(srect.y()-offset)
+        self.ui.cropH.setValue(srect.height()+-(offset*2))
+        self.ui.cropW.setValue(srect.width()-(offset*2))
+        pass
+
+    def gamma_table(self, bands, gamma_r, gamma_g, gamma_b, gamma_a, gain_r=1.0, gain_g=1.0, gain_b=1.0, gain_a=1.0):
+        r_tbl = [min(255, int((x / 255.) ** (1. / gamma_r) * gain_r * 255.)) for x in range(256)]
+        g_tbl = [min(255, int((x / 255.) ** (1. / gamma_g) * gain_g * 255.)) for x in range(256)]
+        b_tbl = [min(255, int((x / 255.) ** (1. / gamma_b) * gain_b * 255.)) for x in range(256)]
+        a_tbl = [min(255, int((x / 255.) ** (1. / gamma_a) * gain_a * 255.)) for x in range(256)]
+
+        if len(bands) == 3:
+            return r_tbl + g_tbl + b_tbl
+        else:
+            return r_tbl + g_tbl + b_tbl + a_tbl
+
+    def enhanceImage(self):
+
+        cont = self.ui.contrastSlider.value()/10
+        sharp = self.ui.sharpnessSlider.value()/10
+        bright = self.ui.brigthnesSlider.value()/10
+        color = self.ui.colorSlider.value()/10
+        gamma = self.ui.gammaSlider.value()/10
+
+        convertImg = io.BytesIO()
+        pilImg = self.pilBufferImg
+
+        brightness = ImageEnhance.Brightness(pilImg)
+        pilImg = brightness.enhance(bright)
+        contrast = ImageEnhance.Contrast(pilImg)
+        pilImg = contrast.enhance(cont)
+        colour = ImageEnhance.Color(pilImg)
+        pilImg = colour.enhance(color)
+        sharpness = ImageEnhance.Sharpness(pilImg)
+        pilImg = sharpness.enhance(sharp)
+
+       # print("Bands: ",pilImg.getbands())
+        bands = pilImg.getbands()
+        gammaImg = pilImg.point(self.gamma_table(bands, gamma, gamma, gamma,gamma))
+        pilImg = gammaImg.copy()
+
+
+        pilImg.save(convertImg,"BMP")
+        self.pixmap.loadFromData(convertImg.getvalue(), "BMP")
+        self.pixmapItem.setPixmap(self.pixmap)
+
+        
+    def setBufferImage(self):
+
+        self.qbuffer.open(QIODevice.ReadWrite)
+
+        img = self.pixmapItem.pixmap()
+        img.save(self.qbuffer,"PNG")
+
+        self.pilBufferPath.write(self.qbuffer.data())
+        self.qbuffer.close()
+        self.pilBufferPath.seek(0)
+        self.pilBufferImg = Image.open(self.pilBufferPath)
+
+    def wheelEvent(self, event):
+        point = event.position()
+        degrees = event.angleDelta() / 8
+
+        self.pixmapItemScale = self.pixmapItem.scale()
+
+        print(self.pixmapItemScale)
+
+        if self.pixmapItem.isUnderMouse():
+
+            if degrees.y()/15 > 0:
+                #self.ui.view.scale(1.25, 1.25)
+                self.pixmapItem.setScale(self.pixmapItemScale+0.01)
+            else:
+                #self.ui.view.scale(0.8, 0.8)
+                if self.pixmapItemScale > 0.15:
+                    self.pixmapItem.setScale(self.pixmapItemScale-0.01)
+
+    def closeEvent(self, event):
+        self.scene.clear()
+        event.accept()
