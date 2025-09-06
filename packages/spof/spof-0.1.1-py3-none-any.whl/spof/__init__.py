78d6ad0dcb7a8a29fe6bbfb94815f848efb19c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:02 +0000

    Add package spof/0.1.1/spof-0.1.1-py3-none-any.whl

---
+++ packages/spof/spof-0.1.1-py3-none-any.whl/spof/__init__.py
@@ -0,0 +1,11 @@
+from .spof import InstructionBlock, Text, Items, ModelBlock, wrap_model, RenderFormat
+
+
+__all__ = [
+    "InstructionBlock",
+    "Text",
+    "Items",
+    "ModelBlock",
+    "wrap_model",
+    "RenderFormat",
+]
