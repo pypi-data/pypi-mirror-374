ebbd416502aa128b09ce2b5c62a4755afe3c1701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:29 +0000

    Add package rocketlib/0.1.0/rocketlib-0.1.0.tar.gz

---
+++ packages/rocketlib/rocketlib-0.1.0.tar.gz/rocketlib-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rocketlib",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if needed
+    description="Republished version of rlsdk_python",
+    author="Ian Mattas",
+    author_email="ian@mattas.net",
+    url="https://pypi.org/project/rocketlib/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
