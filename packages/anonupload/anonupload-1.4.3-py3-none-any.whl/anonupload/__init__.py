05fa6ac82a58860e5496b514390ee230fa9ccaa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 15:06:22 +0000

    Add package anonupload/1.4.3/anonupload-1.4.3-py3-none-any.whl

---
+++ packages/anonupload/anonupload-1.4.3-py3-none-any.whl/anonupload/__init__.py
@@ -0,0 +1,5 @@
+__version__= "1.4.3"
+
+from .main import download, downloads, upload, changefile_and_upload, detect_filename, download_and_upload, multi_upload
+
+__all__ = ["download", "downloads", "upload", "changefile_and_upload", "detect_filename", "download_and_upload", "multi_upload"]
