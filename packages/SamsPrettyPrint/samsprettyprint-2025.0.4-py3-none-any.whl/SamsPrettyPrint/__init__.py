dccca9ba05fb78a9f7281f18c1160826e775a817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 04:06:54 +0000

    Add package SamsPrettyPrint/2025.0.4/samsprettyprint-2025.0.4-py3-none-any.whl

---
+++ packages/SamsPrettyPrint/samsprettyprint-2025.0.4-py3-none-any.whl/SamsPrettyPrint/__init__.py
@@ -0,0 +1,48 @@
+__name__ = "SamsPrettyPrint"
+__version__ = "2025.0.4"
+__author__ = "Samuel Ward"
+
+
+from .pretty_print import (
+    align,
+    cast_to_fixed_length_list,
+    disable_ascii_colour_codes,
+    enable_ascii_colour_codes,
+    isnumeric,
+    itemised_dictionary,
+    print_dict,
+    print_table,
+    print_title,
+    table,
+    title1,
+    title3,
+    title5,
+)
+from .pretty_examples import (
+    ex_array,
+    ex_df,
+    ex_dict,
+    ex_list,
+    ex_str,
+)
+
+__all__ = [
+    'align',
+    'cast_to_fixed_length_list',
+    'disable_ascii_colour_codes',
+    'enable_ascii_colour_codes',
+    'ex_array',
+    'ex_df',
+    'ex_dict',
+    'ex_list',
+    'ex_str',
+    'isnumeric',
+    'itemised_dictionary',
+    'print_dict',
+    'print_table',
+    'print_title',
+    'table',
+    'title1',
+    'title3',
+    'title5',
+]
