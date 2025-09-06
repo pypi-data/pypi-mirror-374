08b555985a65f3cdb85eac7b62f1ade20de8c2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-06 07:06:36 +0000

    Add package tm-profiler/0.0.1/tm_profiler-0.0.1-py3-none-any.whl

---
+++ packages/tm-profiler/tm_profiler-0.0.1-py3-none-any.whl/tm_profiler/__init__.py
@@ -0,0 +1,60 @@
+"""
+Time Profiler
+=============
+
+The tm_profiler module for time profiling of functions
+
++---------------+--------------------------------------------+
+| GitHub        | https://github.com/NorchaHack/tm-profiler  |
++---------------+--------------------------------------------+
+| Pypi          | https://pypi.org/project/tm-profiler       |
++---------------+--------------------------------------------+
+
+
+Installation
+============
+
+Releases of :py:mod:`tm_profiler` can be installed
+using pip
+
+.. code:: bash
+
+    pip install tm_profiler
+
+Time Profiler Basic Usage
+=========================
+
+............
+
+"""
+
+# __submodules__ = [
+#     'tm_profiler',
+#     'ipython_extension',
+# ]
+#
+# __autogen__ = """
+# mkinit ./tm_profiler/__init__.py --relative
+# mkinit ./tm_profiler/__init__.py --relative -w
+# """
+
+from .tm_profiler import (profile, print_stat, print_table, print_last,
+                          set_output_dec, set_output_name_format,
+                          FuncNameFormat, log_time_enabled)
+
+
+__all__ = [
+    "profile",
+    # "sum_by_pairs_to_file",
+    # "sum_file_total",
+    # "read_numbers_from_text_file",
+    # "set_parse_float",
+    # "set_max_number",
+    # "set_min_number",
+    # "reset_to_default",
+]
+
+__author__ = "Normunds Pureklis <norchahack@gmail.com>"
+__status__ = "development"
+__version__ = "0.0.1"
+__date__ = "05 Sep 2025"
