7944c94b58defbb7179a71a509487b82b5f40c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:25 +0000

    Add package yanga/2.10.0/yanga-2.10.0.tar.gz

---
+++ packages/yanga/yanga-2.10.0.tar.gz/yanga-2.10.0/src/yanga/steps/kconfig_gen.py
@@ -0,0 +1,63 @@
+from pathlib import Path
+from typing import Any, Optional
+
+from kspl.generate import HeaderWriter
+from kspl.kconfig import KConfig
+from py_app_dev.core.logging import logger
+from pypeline.domain.pipeline import PipelineStep
+
+from yanga.domain.execution_context import ExecutionContext, IncludeDirectoriesProvider
+
+
+class KConfigIncludeDirectoriesProvider(IncludeDirectoriesProvider):
+    def __init__(self, output_dir: Path) -> None:
+        self.output_dir = output_dir
+
+    def get_include_directories(self) -> list[Path]:
+        return [self.output_dir]
+
+
+class KConfigGen(PipelineStep[ExecutionContext]):
+    def __init__(self, execution_context: ExecutionContext, group_name: Optional[str] = None, config: Optional[dict[str, Any]] = None) -> None:
+        super().__init__(execution_context, group_name, config)
+        self.logger = logger.bind()
+        self.input_files: list[Path] = []
+
+    @property
+    def output_dir(self) -> Path:
+        return self.execution_context.create_artifacts_locator().variant_build_dir / "kconfig"
+
+    def get_name(self) -> str:
+        return self.__class__.__name__
+
+    @property
+    def header_file(self) -> Path:
+        return self.output_dir.joinpath("autoconf.h")
+
+    def run(self) -> int:
+        self.logger.debug(f"Run {self.get_name()} stage. Output dir: {self.output_dir}")
+        kconfig_model_file = self.project_root_dir.joinpath("KConfig")
+        if not kconfig_model_file.is_file():
+            self.logger.info("No KConfig file found. Skip this stage.")
+            return 0
+        kconfig = KConfig(
+            kconfig_model_file,
+            self.execution_context.features_selection_file,
+        )
+        self.input_files = kconfig.get_parsed_files()
+        config = kconfig.collect_config_data()
+        HeaderWriter(self.header_file).write(config)
+        return 0
+
+    def get_inputs(self) -> list[Path]:
+        # TODO: Use as input only the user config file where variant configuration is defined.
+        # Now all the user config files are used as inputs, which will trigger the generation
+        # if any of the file has changed.
+        return self.execution_context.user_config_files + self.input_files
+
+    def get_outputs(self) -> list[Path]:
+        return [self.header_file]
+
+    def update_execution_context(self) -> None:
+        # Update the include directories for the subsequent steps
+        self.execution_context.add_include_dirs_provider(KConfigIncludeDirectoriesProvider(self.output_dir))
