4e2d96c2d5b95bfe26436a65ac8900ba4cb4f069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:20 +0000

    Add package pyThermoDB/1.11.25/pythermodb-1.11.25-py3-none-any.whl

---
+++ packages/pyThermoDB/pythermodb-1.11.25-py3-none-any.whl/pyThermoDB/models/__init__.py
@@ -0,0 +1,23 @@
+from .ref import (
+    DataBookTableTypes,
+    PayLoadType,
+    DataResultType,
+    MatrixDataType,
+    DataResult,
+    EquationResult
+)
+
+from .references import Component, ComponentReferenceThermoDB
+from .property import PropertyMatch
+
+__all__ = [
+    'DataBookTableTypes',
+    'PayLoadType',
+    'DataResultType',
+    'MatrixDataType',
+    'DataResult',
+    'EquationResult',
+    'Component',
+    'PropertyMatch',
+    'ComponentReferenceThermoDB'
+]
