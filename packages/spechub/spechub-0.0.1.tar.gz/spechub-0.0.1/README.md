8b2c13dc4d9fca3867dfe874919aa7ad81b8d9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 05:06:21 +0000

    Add package spechub/0.0.1/spechub-0.0.1.tar.gz

---
+++ packages/spechub/spechub-0.0.1.tar.gz/spechub-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Spechub
+
+**🚧 Under Construction 🚧**
+
+*This is a placeholder package. The full framework will be available soon.*
---
+++ packages/spechub/spechub-0.0.1.tar.gz/spechub-0.0.1/publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Building and publishing with uv..."
+echo ""
+echo "To publish to PyPI:"
+echo "  uv publish"
+echo ""
+echo "You'll be prompted for:"
+echo "- Username: __token__"
+echo "- Password: your-pypi-token"
