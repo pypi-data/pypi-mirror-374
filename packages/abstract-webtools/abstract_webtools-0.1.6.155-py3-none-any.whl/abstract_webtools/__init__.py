49c78ce4efa004e27abba6ef4446080f93ea5466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 15:06:27 +0000

    Add package abstract-webtools/0.1.6.155/abstract_webtools-0.1.6.155-py3-none-any.whl

---
+++ packages/abstract-webtools/abstract_webtools-0.1.6.155-py3-none-any.whl/abstract_webtools/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_webtools import *
+from .url_grabber import url_grabber_component
+from .abstract_webtools import get_url_mgr
+from .managers import *
+from .abstract_usurpit import usurpManager,usurpit
