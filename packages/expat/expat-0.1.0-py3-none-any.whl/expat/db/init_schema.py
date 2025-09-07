d5b3c658d0329f7440d4e68c6518aef457a2946f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:06:45 +0000

    Add package expat/0.1.0/expat-0.1.0-py3-none-any.whl

---
+++ packages/expat/expat-0.1.0-py3-none-any.whl/expat/db/init_schema.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from _typeshed.dbapi import DBAPIConnection
+
+from pathlib import Path
+
+
+def init_schema(connection: DBAPIConnection) -> None:
+    schema_script = Path(__file__).parent.parent.parent.parent / "init_schema.sql"
+
+    with open(schema_script, "r") as f:
+        script = f.read()
+
+    cursor = connection.cursor()
+    cursor.execute(script)
+    cursor.close()
