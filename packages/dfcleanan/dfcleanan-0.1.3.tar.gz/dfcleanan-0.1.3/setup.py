3b32a3a1b77629a49fb13846da05d29137486b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:33 +0000

    Add package dfcleanan/0.1.3/dfcleanan-0.1.3.tar.gz

---
+++ packages/dfcleanan/dfcleanan-0.1.3.tar.gz/dfcleanan-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dfcleanan",  
+    version="0.1.3",
+    author="Ozodbek_Qurbonov",
+    description="DataFrame clean nan data",
+    packages=find_packages(),
+    install_requires=["pandas>=2.0.0"],
+    python_requires=">=3.13",
+    license="MIT",
+)
