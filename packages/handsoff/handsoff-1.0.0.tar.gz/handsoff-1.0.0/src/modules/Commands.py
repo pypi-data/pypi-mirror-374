032f9da39d7c1142a5bb3571e235812d1b2669a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:06:09 +0000

    Add package handsoff/1.0.0/handsoff-1.0.0.tar.gz

---
+++ packages/handsoff/handsoff-1.0.0.tar.gz/handsoff-1.0.0/src/modules/Commands.py
@@ -0,0 +1,59 @@
+import subprocess
+
+class Commands:
+    def __init__(self, settings: dict[str, str] | None = None) -> None:
+        self.valid_params = {"host", "port", "user", "from", "to", "pem"}
+
+        self.params: dict[str, str] = {}
+
+        if settings:
+            for key, value in settings.items():
+                if key in self.valid_params:
+                    self.params[key] = value
+
+        self.params.setdefault("port", "22")
+
+    def set_params(self, params: dict[str, str]) -> None:
+        for key, value in params.items():
+            if key not in self.valid_params:
+                raise ValueError(f"Not valid parameter: {key}")
+            self.params[key] = value
+    
+    def get_params(self) -> dict[str, str] :
+        return self.params
+
+    def command_not_valid(self) -> bool:
+        required = ["host", "user", "from", "to"]
+        return not all(self.params.get(k) for k in required)
+
+    def pull(self) -> None:
+        if self.command_not_valid():
+            raise ConnectionAbortedError(
+                "Not enough parameters! You need at least HOST, USER, from, to."
+            )
+
+        options = "-r -o StrictHostKeyChecking=no"
+        if pem := self.params.get("pem"):
+            options += f" -i {pem}"
+
+        subprocess.run(
+            f"scp {options} -P {self.params['port']} {self.params['user']}@{self.params['host']}:{self.params['from']} {self.params['to']}",
+            # stderr=subprocess.DEVNULL,
+            shell=True
+        )
+
+    def push(self) -> None:
+        if self.command_not_valid():
+            raise ConnectionAbortedError(
+                "Not enough parameters! You need at least HOST, USER, from, to."
+            )
+
+        options = "-r -o StrictHostKeyChecking=no"
+        if pem := self.params.get("pem"):
+            options += f" -i {pem}"
+
+        subprocess.run(
+            f"scp {options} -P {self.params['port']} {self.params['from']} {self.params['user']}@{self.params['host']}:{self.params['to']}",
+            # stderr=subprocess.DEVNULL,
+            shell=True
+        )
