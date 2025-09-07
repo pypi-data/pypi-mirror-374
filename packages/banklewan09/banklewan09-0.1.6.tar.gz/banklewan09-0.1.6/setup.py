148231cabe5f734167cdd8c71a216fe3a96c65a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:11 +0000

    Add package banklewan09/0.1.6/banklewan09-0.1.6.tar.gz

---
+++ packages/banklewan09/banklewan09-0.1.6.tar.gz/banklewan09-0.1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="banklewan09",               # Must be unique on PyPI!
+    version="0.1.6",                # Follow semantic versioning
+    description="This is my first package. This package are calculation volume ,lateral and surface in cylinder .",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Lewan",
+    author_email="natthapong.nn@mail.wu.ac.th",
+    url="https://github.com/yourusername/mypackage",  # optional
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[],
+)
