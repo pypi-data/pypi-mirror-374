a8790281ce6c57be44cc547cc2a9339d6ec49a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:31 +0000

    Add package cspipes/0.1.0/cspipes-0.1.0-py3-none-any.whl

---
+++ packages/cspipes/cspipes-0.1.0-py3-none-any.whl/cspipes/__init__.py
@@ -0,0 +1,3 @@
+"""Compass Pipes - Data pipeline orchestration tools for Compass Bot."""
+
+__version__ = "0.1.0"
---
+++ packages/cspipes/cspipes-0.1.0-py3-none-any.whl/cspipes/cli.py
@@ -0,0 +1,11 @@
+"""CLI entry point for cspipes."""
+
+
+def main():
+    """Main entry point for cspipes CLI."""
+    print("cspipes v0.1.0 - Compass Pipes tools")
+    print("Available commands will be added in future versions.")
+
+
+if __name__ == "__main__":
+    main()
