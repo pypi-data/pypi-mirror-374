b7feabbfb86079d477a451f74db27f4fbb0057a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:06:10 +0000

    Add package bank-statement-separator/2.1.3/bank_statement_separator-2.1.3.tar.gz

---
+++ packages/bank-statement-separator/bank_statement_separator-2.1.3.tar.gz/bank_statement_separator-2.1.3/src/bank_statement_separator/__init__.py
@@ -0,0 +1,8 @@
+"""
+Bank Statement Separator
+
+AI-powered tool for separating multi-statement PDF files using LangChain and LangGraph.
+"""
+
+__version__ = "2.1.3"
+__author__ = "Bank Statement Separator Team"
