caee3b629e4eb196ff904721a711004e2c4678c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:10 +0000

    Add package torch-snippets/0.560/torch_snippets-0.560-py3-none-any.whl

---
+++ packages/torch-snippets/torch_snippets-0.560-py3-none-any.whl/torch_snippets/__init__.py
@@ -0,0 +1,87 @@
+__version__ = "0.558"
+from .logger import *
+from .loader import *
+from .paths import *
+from .markup import *
+from .markdown import *
+
+# from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+from .markup2 import AD
+from .registry import *
+from .ipython import *
+from .decorators import *
+from .misc import *
+from .dates import *
+from .s3_loader import *
+from .zen import *
+from .torch_loader import *
+
+
+def init_torch():
+    try:
+        from .torch_loader import (
+            torch,
+            th,
+            torchvision,
+            T,
+            transforms,
+            nn,
+            np,
+            F,
+            Dataset,
+            DataLoader,
+            optim,
+            Report,
+            Reshape,
+            Permute,
+            device,
+            save_torch_model_weights_from,
+            load_torch_model_weights_to,
+            detach,
+            cat_with_padding,
+        )
+
+        import lovely_tensors
+
+        lovely_tensors.monkey_patch()
+    except ModuleNotFoundError:
+        raise ModuleNotFoundError(
+            "Unable to install torch dependencies. Please install them using `pip install torch-snippets[torch]`"
+        )
+
+    import builtins
+
+    # Define a dictionary mapping names to imported objects
+    modules = {
+        "torch": torch,
+        "th": th,
+        "torchvision": torchvision,
+        "T": T,
+        "transforms": transforms,
+        "nn": nn,
+        "np": np,
+        "F": F,
+        "Dataset": Dataset,
+        "DataLoader": DataLoader,
+        "optim": optim,
+        "Report": Report,
+        "Reshape": Reshape,
+        "Permute": Permute,
+        "device": device,
+        "save_torch_model_weights_from": save_torch_model_weights_from,
+        "load_torch_model_weights_to": load_torch_model_weights_to,
+        "detach": detach,
+        "cat_with_padding": cat_with_padding,
+    }
+
+    # Use a loop to set attributes in builtins
+    for name, module in modules.items():
+        setattr(builtins, name, module)
+
+
+try:
+    init_torch()
+except Exception:
+    ...
