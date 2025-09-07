023f10f35140e16f614f00d1d2423022cc3cac0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 05:06:31 +0000

    Add package common-details/1.0.0/common_details-1.0.0.tar.gz

---
+++ packages/common-details/common_details-1.0.0.tar.gz/common_details-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="common-details",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="sheshadri",
+    description="Common details library",
+    # license="MIT" 
+)
