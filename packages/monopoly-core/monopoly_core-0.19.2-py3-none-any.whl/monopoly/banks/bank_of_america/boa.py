dbc1bbbc3f79fd4bbb254b7418f2fe94b050465b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:28 +0000

    Add package monopoly-core/0.19.2/monopoly_core-0.19.2-py3-none-any.whl

---
+++ packages/monopoly-core/monopoly_core-0.19.2-py3-none-any.whl/monopoly/banks/bank_of_america/boa.py
@@ -0,0 +1,39 @@
+import logging
+import re
+
+from monopoly.banks.base import BankBase
+from monopoly.config import DateOrder, MultilineConfig, StatementConfig
+from monopoly.constants import BankNames, CreditTransactionPatterns, EntryType
+from monopoly.constants.date import ISO8601
+from monopoly.identifiers import MetadataIdentifier
+
+logger = logging.getLogger(__name__)
+
+
+class BankOfAmerica(BankBase):
+    name = BankNames.BANK_OF_AMERICA
+
+    credit = StatementConfig(
+        statement_type=EntryType.CREDIT,
+        statement_date_pattern=re.compile(rf"for .* to {ISO8601.MMMM_DD_YYYY}"),
+        statement_date_order=DateOrder("MDY"),
+        transaction_date_order=DateOrder("MDY"),
+        header_pattern=re.compile(r"(Date.*Description.*Amount)"),
+        transaction_pattern=CreditTransactionPatterns.BANK_OF_AMERICA,
+        transaction_date_format="%m/%d/%y",
+        multiline_config=MultilineConfig(multiline_descriptions=True),
+        safety_check=False,
+        transaction_auto_polarity=False,
+    )
+
+    identifiers = [
+        [
+            MetadataIdentifier(
+                format="PDF 1.5",
+                creator="Bank of America",
+                producer="TargetStream StreamEDS",
+            )
+        ]
+    ]
+
+    statement_configs = [credit]
