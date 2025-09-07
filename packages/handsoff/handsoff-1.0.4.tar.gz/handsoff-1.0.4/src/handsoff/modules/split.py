9da087a5e5a0d30c5425dff33790760807f1d9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:05:52 +0000

    Add package handsoff/1.0.4/handsoff-1.0.4.tar.gz

---
+++ packages/handsoff/handsoff-1.0.4.tar.gz/handsoff-1.0.4/src/handsoff/modules/split.py
@@ -0,0 +1,11 @@
+import re
+
+def split(commands: list[str]) -> dict[str, str]:
+    params = {}
+    for elem in commands[2:]:
+        if '=' not in elem:
+            continue
+        key, value = elem.split('=', 1)
+        params[key] = value
+    return params
+    
