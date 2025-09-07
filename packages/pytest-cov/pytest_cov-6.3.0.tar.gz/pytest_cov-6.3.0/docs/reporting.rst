3dc8152f702f08c18b14f15d787194cbe66ddcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:06:03 +0000

    Add package pytest-cov/6.3.0/pytest_cov-6.3.0.tar.gz

---
+++ packages/pytest-cov/pytest_cov-6.3.0.tar.gz/pytest_cov-6.3.0/docs/reporting.rst
@@ -0,0 +1,83 @@
+Reporting
+=========
+
+It is possible to generate any combination of the reports for a single test run.
+
+The available reports are terminal (with or without missing line numbers shown), HTML, XML, JSON, Markdown (either in 'write' or 'append' mode to file), LCOV and
+annotated source code.
+
+The terminal report without line numbers (default)::
+
+    pytest --cov-report term --cov=myproj tests/
+
+    -------------------- coverage: platform linux2, python 2.6.4-final-0 ---------------------
+    Name                 Stmts   Miss  Cover
+    ----------------------------------------
+    myproj/__init__          2      0   100%
+    myproj/myproj          257     13    94%
+    myproj/feature4286      94      7    92%
+    ----------------------------------------
+    TOTAL                  353     20    94%
+
+
+The terminal report with line numbers::
+
+    pytest --cov-report term-missing --cov=myproj tests/
+
+    -------------------- coverage: platform linux2, python 2.6.4-final-0 ---------------------
+    Name                 Stmts   Miss  Cover   Missing
+    --------------------------------------------------
+    myproj/__init__          2      0   100%
+    myproj/myproj          257     13    94%   24-26, 99, 149, 233-236, 297-298, 369-370
+    myproj/feature4286      94      7    92%   183-188, 197
+    --------------------------------------------------
+    TOTAL                  353     20    94%
+
+The terminal report with skip covered::
+
+    pytest --cov-report term:skip-covered --cov=myproj tests/
+
+    -------------------- coverage: platform linux2, python 2.6.4-final-0 ---------------------
+    Name                 Stmts   Miss  Cover
+    ----------------------------------------
+    myproj/myproj          257     13    94%
+    myproj/feature4286      94      7    92%
+    ----------------------------------------
+    TOTAL                  353     20    94%
+
+    1 files skipped due to complete coverage.
+
+You can use ``skip-covered`` with ``term-missing`` as well. e.g. ``--cov-report term-missing:skip-covered``
+
+The report options below output to files without showing anything on the terminal::
+
+    pytest --cov-report html
+            --cov-report xml
+            --cov-report json
+            --cov-report markdown
+            --cov-report markdown-append:cov-append.md
+            --cov-report lcov
+            --cov-report annotate
+            --cov=myproj tests/
+
+The output location for each of these reports can be specified. The output location for the XML, JSON, Markdown and LCOV
+report is a file. markdown-append option is specially useful for appending the report to an existing file. Example for GitHub Actions:
+--cov-report=markdown-append:${GITHUB_STEP_SUMMARY}. Where as the output location for the HTML and annotated source code reports are
+directories::
+
+    pytest --cov-report html:cov_html
+            --cov-report xml:cov.xml
+            --cov-report json:cov.json
+            --cov-report markdown:cov.md
+            --cov-report markdown-append:cov-append.md
+            --cov-report lcov:cov.info
+            --cov-report annotate:cov_annotate
+            --cov=myproj tests/
+
+The final report option can also suppress printing to the terminal::
+
+    pytest --cov-report= --cov=myproj tests/
+
+This mode can be especially useful on continuous integration servers, where a coverage file
+is needed for subsequent processing, but no local report needs to be viewed. For example,
+tests run on GitHub Actions could produce a .coverage file for use with Coveralls.
---
+++ packages/pytest-cov/pytest_cov-6.3.0.tar.gz/pytest_cov-6.3.0/setup.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+import re
+from itertools import chain
+from pathlib import Path
+
+from setuptools import Command
+from setuptools import find_packages
+from setuptools import setup
+
+try:
+    # https://setuptools.pypa.io/en/latest/deprecated/distutils-legacy.html
+    from setuptools.command.build import build
+except ImportError:
+    from distutils.command.build import build
+
+from setuptools.command.develop import develop
+from setuptools.command.easy_install import easy_install
+from setuptools.command.install_lib import install_lib
+
+
+def read(*names, **kwargs):
+    with Path(__file__).parent.joinpath(*names).open(encoding=kwargs.get('encoding', 'utf8')) as fh:
+        return fh.read()
+
+
+class BuildWithPTH(build):
+    def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+        path = str(Path(__file__).parent / 'src' / 'pytest-cov.pth')
+        dest = str(Path(self.build_lib) / Path(path).name)
+        self.copy_file(path, dest)
+
+
+class EasyInstallWithPTH(easy_install):
+    def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+        path = str(Path(__file__).parent / 'src' / 'pytest-cov.pth')
+        dest = str(Path(self.install_dir) / Path(path).name)
+        self.copy_file(path, dest)
+
+
+class InstallLibWithPTH(install_lib):
+    def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+        path = str(Path(__file__).parent / 'src' / 'pytest-cov.pth')
+        dest = str(Path(self.install_dir) / Path(path).name)
+        self.copy_file(path, dest)
+        self.outputs = [dest]
+
+    def get_outputs(self):
+        return chain(super().get_outputs(), self.outputs)
+
+
+class DevelopWithPTH(develop):
+    def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+        path = str(Path(__file__).parent / 'src' / 'pytest-cov.pth')
+        dest = str(Path(self.install_dir) / Path(path).name)
+        self.copy_file(path, dest)
+
+
+class GeneratePTH(Command):
+    user_options = ()
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        with Path(__file__).parent.joinpath('src', 'pytest-cov.pth').open('w') as fh:
+            with Path(__file__).parent.joinpath('src', 'pytest-cov.embed').open() as sh:
+                fh.write(f'import os, sys;exec({sh.read().replace("    ", " ")!r})')
+
+
+setup(
+    name='pytest-cov',
+    version='6.3.0',
+    license='MIT',
+    description='Pytest plugin for measuring coverage.',
+    long_description='{}\n{}'.format(read('README.rst'), re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))),
+    author='Marc Schlaich',
+    author_email='marc.schlaich@gmail.com',
+    url='https://github.com/pytest-dev/pytest-cov',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[path.stem for path in Path('src').glob('*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        'Documentation': 'https://pytest-cov.readthedocs.io/',
+        'Changelog': 'https://pytest-cov.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/pytest-dev/pytest-cov/issues',
+    },
+    keywords=[
+        'cover',
+        'coverage',
+        'pytest',
+        'py.test',
+        'distributed',
+        'parallel',
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'pytest>=6.2.5',
+        'coverage[toml]>=7.5',
+        'pluggy>=1.2',
+    ],
+    extras_require={
+        'testing': [
+            'fields',
+            'hunter',
+            'process-tests',
+            'pytest-xdist',
+            'virtualenv',
+        ]
+    },
+    entry_points={
+        'pytest11': [
+            'pytest_cov = pytest_cov.plugin',
+        ],
+    },
+    cmdclass={
+        'build': BuildWithPTH,
+        'easy_install': EasyInstallWithPTH,
+        'install_lib': InstallLibWithPTH,
+        'develop': DevelopWithPTH,
+        'genpth': GeneratePTH,
+    },
+)
