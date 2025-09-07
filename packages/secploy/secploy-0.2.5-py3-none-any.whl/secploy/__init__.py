da4eb70531881b1aa9158d357dbd7bf2b5f0843a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:38 +0000

    Add package secploy/0.2.5/secploy-0.2.5-py3-none-any.whl

---
+++ packages/secploy/secploy-0.2.5-py3-none-any.whl/secploy/__init__.py
@@ -0,0 +1,7 @@
+from .client import SecployClient
+from .schemas import SecployConfig
+from .enums import LogLevel
+
+__version__ = "0.2.5"
+__all__ = ["SecployClient", "SecployConfig", "LogLevel"]
+
