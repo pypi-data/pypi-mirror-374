6ca84db96df0d9c3170e8cd6a60fccceea490cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:32 +0000

    Add package signal-ICT-Krishraj-92510133023/0.1/signal_ict_krishraj_92510133023-0.1.tar.gz

---
+++ packages/signal-ICT-Krishraj-92510133023/signal_ict_krishraj_92510133023-0.1.tar.gz/signal_ict_krishraj_92510133023-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signal_ICT_Krishraj_92510133023",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib"],
+    description="Custom signal generation and operations package",
+    author="Your Name",
+    author_email="your-email@example.com",
+)
