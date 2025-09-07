85e353be03c66163ff8805638450f4b518189b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:21:30 +0000

    Add package hhi/6.21.0/hhi-6.21.0-cp312-cp312-win_amd64.whl

---
+++ packages/hhi/hhi-6.21.0-cp312-cp312-win_amd64.whl/hhi/__init__.py
@@ -0,0 +1,23 @@
+"""hhi - HHI photonics PDK"""
+
+from hhi import cells, config
+from hhi.pdk import PDK
+from hhi.tech import (
+    LAYER,
+    LAYER_STACK,
+    LAYER_VIEWS,
+    constants,
+    cross_sections,
+)
+
+__all__ = (
+    "cells",
+    "config",
+    "PDK",
+    "LAYER",
+    "LAYER_VIEWS",
+    "LAYER_STACK",
+    "cross_sections",
+    "constants",
+)
+__version__ = "6.21.0"
