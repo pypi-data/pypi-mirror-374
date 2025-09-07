9e7a40e40f13998b7e10cc902fcbf3163a4ef930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:06:22 +0000

    Add package playerpizarro05/0.0.1/playerpizarro05-0.0.1.tar.gz

---
+++ packages/playerpizarro05/playerpizarro05-0.0.1.tar.gz/playerpizarro05-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from pathlib import Path
+
+# Importamos el texto que hemos escrito en README.md y
+# la guardamos en un variable para luego utilizarla.
+long_desc = Path("README.md").read_text()
+
+setuptools.setup(
+    # El nombre del paquete que va tener dentro de pypi.
+    name="playerpizarro05",
+    # Indicamos la version que queremos utilizar
+    version="0.0.1",
+    # Aqui le vamos a dar una descripcion larga en pypi
+    long_description=long_desc,
+    # Donde se encuentran los paquetes
+    packages=setuptools.find_packages(
+        exclude=["mocks", "test"]
+    )
+)
