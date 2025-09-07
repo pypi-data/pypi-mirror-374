3eec8498682e5071cf95db55e064690ea2d78db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:19:51 +0000

    Add package haltonseq/0.1.0/haltonseq-0.1.0.tar.gz

---
+++ packages/haltonseq/haltonseq-0.1.0.tar.gz/haltonseq-0.1.0/haltonseq/core.py
@@ -0,0 +1,16 @@
+# haltonseq/core.py
+
+def halton_sequence(b: int):
+    """Generator function for Halton sequence with given base b."""
+    n, d = 0, 1
+    while True:
+        x = d - n
+        if x == 1:
+            n = 1
+            d *= b
+        else:
+            y = d // b
+            while x <= y:
+                y //= b
+            n = (b + 1) * y - x
+        yield n / d
