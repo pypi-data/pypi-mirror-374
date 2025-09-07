97205cae6667141adf5ec8bc2bd9f970f8d514ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:06:45 +0000

    Add package PyBACCHUS/1.0.0/pybacchus-1.0.0-py3-none-any.whl

---
+++ packages/PyBACCHUS/pybacchus-1.0.0-py3-none-any.whl/PyBACCHUS/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "1.0.0"
+
+# set Python env variable to keep track of example data dir
+DATADIR = os.path.dirname(__file__)
+
+print("You have successfully imported PyBACCHUS! Good luck!!!\n")
