90d3cb5da390f05d9f3053ca6e5af7f4d9f584b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 15:06:19 +0000

    Add package reme-ai/0.1.5/reme_ai-0.1.5.tar.gz

---
+++ packages/reme-ai/reme_ai-0.1.5.tar.gz/reme_ai-0.1.5/reme_ai/app.py
@@ -0,0 +1,17 @@
+import sys
+
+from flowllm.service.base_service import BaseService
+
+from reme_ai.config.config_parser import ConfigParser
+
+
+def main():
+    with BaseService.get_service(*sys.argv[1:], parser=ConfigParser) as service:
+        service(logo="ReMe")
+
+
+if __name__ == "__main__":
+    main()
+
+# python -m build
+# twine upload dist/*
