f5e09d9d35714034351163a1190783347e587deb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:48 +0000

    Add package pybeoplay/2.8.0/pybeoplay-2.8.0.tar.gz

---
+++ packages/pybeoplay/pybeoplay-2.8.0.tar.gz/pybeoplay-2.8.0/README.md
@@ -0,0 +1,45 @@
+# Bang & Olufsen BeoPlay devices Python integration module
+
+Beoplay Python wrapper to integrate B&amp;O speakers/TVs to Python that use the BeoPlay API. BeoPlay API is the 2nd generation B&O API, after [Masterlink Gateway](https://github.com/giachello/mlgw) and before [Mozart](https://github.com/bang-olufsen/mozart-open-api). It is supported by devices built from the early 2000s to the late 2010s, including several BeoVision TV, BeoLab speakers and the NL/ML Converter.
+
+The API is wrapped in an object that can be used  to read the state and control BeoPlay devices. It includes both methods for blocking calls, and async calls with callbacks using aiohttp.
+
+Reference information [is on this page.](https://documenter.getpostman.com/view/1053298/T1LTe4Lt)
+
+Some more information on the Notifications stream is in the [EVENTS.md](EVENTS.md) file.
+
+Currently gets the following attributes:
+- volume
+- mute
+- source
+- source list
+- primary experience
+- state
+- standby
+- track image (if available)
+- artist 
+- album
+- song(name) (if available)
+- description (if available)
+- stand positions
+- sound modes
+
+The following commands are available:
+- set volume
+- set mute
+- play
+- pause
+- stop
+- next
+- previous
+- standby
+- set source
+- join experience
+- leave experience
+- play queue item
+- set stand position
+- set sound mode
+
+The following key presses are available:
+```Cursor/Select, Cursor/Up, Cursor/Down, Cursor/Left, Cursor/Right, Cursor/Exit, Cursor/Back, Cursor/PageUp, Cursor/PageDown, Cursor/Clear, Stream/Play, Stream/Stop, Stream/Pause, Stream/Wind, Stream/Rewind, Stream/Forward, Stream/Backward, List/StepUp, List/StepDown, List/PreviousElement, List/Shuffle, List/Repeat, Menu/Root, Menu/Option, Menu/Setup, Menu/Contents, Menu/Favorites, Menu/ElectronicProgramGuide, Menu/VideoOnDemand, Menu/Text, Menu/HbbTV,Menu/HomeControl, Device/Information, Device/Eject, Device/TogglePower, Device/Languages, Device/Subtitles, Device/OneWayJoin, Device/Mots, Record/Record, Generic/Blue, Generic/Red, Generic/Green, Generic/Yellow,``` and the digits ```0-9```
+
---
+++ packages/pybeoplay/pybeoplay-2.8.0.tar.gz/pybeoplay-2.8.0/pybeoplay/const.py
@@ -0,0 +1,50 @@
+#
+# BeoPlay JSON interface for Bang & Olufsen Speakers, TVs and other NL devices
+# Reference: https://documenter.getpostman.com/view/1053298/T1LTe4Lt#intro
+# Reference: https://raw.githubusercontent.com/PolyPv/BeoRemote/master/BeoRemote.txt
+# Lifted a lot of code from marton borzak's ha-beoplay
+#
+#
+
+# Connection constants
+BASE_URL = 'http://{0}:8080/{1}'
+TIMEOUT = 5.0
+CONNFAILCOUNT = 5
+
+# BeoPlay constants
+BEOPLAY_URL_NOTIFICATIONS = 'BeoNotify/Notifications'
+
+BEOPLAY_URL_SET_VOLUME = 'BeoZone/Zone/Sound/Volume/Speaker/Level'
+BEOPLAY_URL_MUTE = 'BeoZone/Zone/Sound/Volume/Speaker/Muted'
+BEOPLAY_URL_PLAY = 'BeoZone/Zone/Stream/Play'
+BEOPLAY_URL_RELEASE = '/Release'
+BEOPLAY_URL_PAUSE = 'BeoZone/Zone/Stream/Pause'
+BEOPLAY_URL_STOP = 'BeoZone/Zone/Stream/Stop'
+BEOPLAY_URL_FORWARD = 'BeoZone/Zone/Stream/Forward'
+BEOPLAY_URL_BACKWARD = 'BeoZone/Zone/Stream/Backward'
+BEOPLAY_URL_STEPUP = 'BeoZone/Zone/List/StepUp'
+BEOPLAY_URL_STEPDOWN = 'BeoZone/Zone/List/StepDown'
+BEOPLAY_URL_SHUFFLE = 'BeoZone/Zone/List/Shuffle'
+BEOPLAY_URL_REPEAT = 'BeoZone/Zone/List/Repeat'
+BEOPLAY_URL_STANDBY = 'BeoDevice/powerManagement/standby'
+
+BEOPLAY_URL_GET_SOURCES = 'BeoZone/Zone/Sources'
+BEOPLAY_URL_ACTIVE_SOURCES = 'BeoZone/Zone/ActiveSources'
+
+BEOPLAY_URL_STAND_ACTIVE = 'BeoZone/Zone/Stand/Active'
+BEOPLAY_URL_STAND = 'BeoZone/Zone/Stand'
+BEOPLAY_URL_GET_SOUND_MODE = 'BeoZone/Zone/Sound/Mode'
+BEOPLAY_URL_SET_SOUND_MODE = 'BeoZone/Zone/Sound/Mode/Active'
+
+BEOPLAY_URL_JOIN_EXPERIENCE = 'BeoZone/Zone/Device/OneWayJoin'
+BEOPLAY_URL_LEAVE_EXPERIENCE = 'BeoZone/Zone/ActiveSources/primaryExperience'
+BEOPLAY_URL_PLAYQUEUE = 'BeoZone/Zone/PlayQueue'
+BEOPLAY_URL_PLAYQUEUE_INSTANT = '?instantplay'
+
+
+BEOPLAY_REMOTE_COMMANDS = ['Cursor/Select', 'Cursor/Up', 'Cursor/Down', 'Cursor/Left', 'Cursor/Right', 'Cursor/Exit', 'Cursor/Back', 'Cursor/PageUp', 'Cursor/PageDown', 'Cursor/Clear', 'Stream/Play', 'Stream/Stop', 'Stream/Pause', 'Stream/Wind', 'Stream/Rewind', 'Stream/Forward', 'Stream/Backward', 'List/StepUp', 'List/StepDown', 'List/PreviousElement', 'List/Shuffle', 'List/Repeat', 'Menu/Root', 'Menu/Option', 'Menu/Setup', 'Menu/Contents', 'Menu/Favorites', 'Menu/ElectronicProgramGuide', 'Menu/VideoOnDemand', 'Menu/Text', 'Menu/HbbTV,Menu/HomeControl', 'Device/Information', 'Device/Eject', 'Device/TogglePower', 'Device/Languages', 'Device/Subtitles', 'Device/OneWayJoin', 'Device/Mots', 'Record/Record', 'Generic/Blue', 'Generic/Red', 'Generic/Green', 'Generic/Yellow']
+BEOPLAY_REMOTE_PREFIX = 'BeoZone/Zone/'
+
+BEOPLAY_DIGITS = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+BEOPLAY_DIGITS_URL = 'BeoZone/Zone/Digits'
+BEOPLAY_DIGITS_KEY = 'digits'
