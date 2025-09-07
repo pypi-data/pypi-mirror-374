56ada0e7b39e72a3b3d4d4401ff41fcfc3c16ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:47 +0000

    Add package gracenote2epg/1.5.5/gracenote2epg-1.5.5.tar.gz

---
+++ packages/gracenote2epg/gracenote2epg-1.5.5.tar.gz/gracenote2epg-1.5.5/gracenote2epg/__init__.py
@@ -0,0 +1,45 @@
+"""
+gracenote2epg - North America TV Guide Grabber
+
+A modular Python implementation for downloading TV guide data from
+tvlistings.gracenote.com with intelligent caching and TVheadend integration.
+"""
+
+__version__ = "1.5.5"
+__author__ = "th0ma7"
+__license__ = "GPL-3.0"
+
+from .gracenote2epg_args import ArgumentParser
+from .gracenote2epg_config import ConfigManager
+from .gracenote2epg_downloader import OptimizedDownloader
+from .gracenote2epg_language import LanguageDetector
+from .gracenote2epg_parser import GuideParser
+from .gracenote2epg_tvheadend import TvheadendClient
+from .gracenote2epg_utils import CacheManager, TimeUtils
+from .gracenote2epg_xmltv import XmltvGenerator
+from .gracenote2epg_dictionaries import (
+    get_category_translation,
+    get_term_translation,
+    get_language_display_name,
+    get_available_languages,
+    get_translation_statistics,
+    reload_translations,
+)
+
+__all__ = [
+    "ArgumentParser",
+    "ConfigManager",
+    "OptimizedDownloader",
+    "LanguageDetector",
+    "GuideParser",
+    "TvheadendClient",
+    "CacheManager",
+    "TimeUtils",
+    "XmltvGenerator",
+    "get_category_translation",
+    "get_term_translation",
+    "get_language_display_name",
+    "get_available_languages",
+    "get_translation_statistics",
+    "reload_translations",
+]
