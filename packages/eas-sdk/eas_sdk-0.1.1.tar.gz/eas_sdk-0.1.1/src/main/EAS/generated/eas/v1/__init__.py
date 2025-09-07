3a5170dc761a8e9e2dc6b51b04e27a10c65d5e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:30 +0000

    Add package eas-sdk/0.1.1/eas_sdk-0.1.1.tar.gz

---
+++ packages/eas-sdk/eas_sdk-0.1.1.tar.gz/eas_sdk-0.1.1/src/main/EAS/generated/eas/v1/__init__.py
@@ -0,0 +1,24 @@
+# Generated protobuf files for EAS SDK v1
+# Type: ignore comments needed for generated protobuf code
+from . import messages_pb2, messages_pb2_grpc
+
+# Import the main message classes for easy access
+from .messages_pb2 import (  # type: ignore
+    Attestation,
+    AttestationResponse,
+    GraphQLError,
+    GraphQLResponse,
+    Schema,
+    SchemaResponse,
+)
+
+__all__ = [
+    "messages_pb2",
+    "messages_pb2_grpc",
+    "Schema",
+    "Attestation",
+    "SchemaResponse",
+    "AttestationResponse",
+    "GraphQLError",
+    "GraphQLResponse",
+]
