0eb46fcee401315ac2e7b2b3376484da06e7a4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:06:48 +0000

    Add package sdpmflut/0.6.2/sdpmflut-0.6.2-py3-none-any.whl

---
+++ packages/sdpmflut/sdpmflut-0.6.2-py3-none-any.whl/sdpmflut/__version__.py
@@ -0,0 +1,22 @@
+"""
+Version information for sdpmflut.
+
+This module defines the version details for the package.
+
+Notes
+-----
+Version is defined as a tuple and converted to string format.
+
+Examples
+--------
+>>> from sdpmflut.__version__ import __version__
+>>> print(__version__)
+'0.6.2'
+"""
+
+from typing import Tuple
+
+__version_info__: Tuple[int, int, int] = (0, 6, 2)
+__version__: str = ".".join(map(str, __version_info__))
+
+__major__, __minor__, __patch__ = __version_info__
