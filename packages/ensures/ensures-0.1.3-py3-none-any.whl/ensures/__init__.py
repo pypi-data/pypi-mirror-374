c1b1a58c45601b6b564204498590036eeaf881e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 05:06:32 +0000

    Add package ensures/0.1.3/ensures-0.1.3-py3-none-any.whl

---
+++ packages/ensures/ensures-0.1.3-py3-none-any.whl/ensures/__init__.py
@@ -0,0 +1,19 @@
+from .ensures import (
+    Error,
+    Result,
+    ensure,
+    invariant,
+    postcondition,
+    precondition,
+    require,
+)
+
+__all__ = [
+    "Error",
+    "Result",
+    "ensure",
+    "invariant",
+    "postcondition",
+    "precondition",
+    "require",
+]
