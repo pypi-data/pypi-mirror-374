c37ddabd37a0e174cfd1ff412b7e4f9a48d78516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:06:07 +0000

    Add package meshcore/2.1.6/meshcore-2.1.6-py3-none-any.whl

---
+++ packages/meshcore/meshcore-2.1.6-py3-none-any.whl/meshcore/__init__.py
@@ -0,0 +1,23 @@
+"""A library for communicating with meshcore devices."""
+import logging
+
+from .ble_cx import BLEConnection
+from .connection_manager import ConnectionManager
+from .events import EventType
+from .meshcore import MeshCore
+from .serial_cx import SerialConnection
+from .tcp_cx import TCPConnection
+
+# Setup default logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    "BLEConnection",
+    "ConnectionManager",
+    "EventType",
+    "MeshCore",
+    "SerialConnection",
+    "TCPConnection",
+    "logger",
+]
