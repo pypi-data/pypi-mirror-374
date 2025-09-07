207cae876ae0091cfd18d2adc537ee64f188432f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:37 +0000

    Add package signal-ict-snehfaldu-92510133030/0.1.0/signal_ict_snehfaldu_92510133030-0.1.0-py3-none-any.whl

---
+++ packages/signal-ict-snehfaldu-92510133030/signal_ict_snehfaldu_92510133030-0.1.0-py3-none-any.whl/signal_ICT_SnehFaldu_92510133030/__init__.py
@@ -0,0 +1,9 @@
+
+#signal_ICT_SnehFaldu_92510133030
+#A small  package for basic signals and operations.
+
+from . import unitary_signals
+from . import trigonometric_signals
+from . import operations
+
+__all__ = ["unitary_signals", "trigonometric_signals", "operations"]
