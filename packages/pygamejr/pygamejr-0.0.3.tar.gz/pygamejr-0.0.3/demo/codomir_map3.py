603431b789481147f123be8f8933fa334a0385bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:05:51 +0000

    Add package pygamejr/0.0.3/pygamejr-0.0.3.tar.gz

---
+++ packages/pygamejr/pygamejr-0.0.3.tar.gz/pygamejr-0.0.3/demo/codomir_map3.py
@@ -0,0 +1,13 @@
+from codomir import player, wait_quit, set_map, maps
+
+set_map(maps.linear.map3)
+
+player.move_forward()
+player.move_forward()
+player.turn_left()
+player.turn_left()
+player.move_forward()
+player.turn_right()
+player.move_forward()
+
+wait_quit()
---
+++ packages/pygamejr/pygamejr-0.0.3.tar.gz/pygamejr-0.0.3/src/codomir/maps/linear.py
@@ -0,0 +1,7 @@
+from ..resources.resolve_path import resolve_path
+
+map1 =  resolve_path('kenney_sokoban/map1.tmx')
+map2 = resolve_path('kenney_sokoban/map2.tmx')
+map3 = resolve_path('kenney_sokoban/map3.tmx')
+map4 = resolve_path('kenney_sokoban/map4.tmx')
+map5 = resolve_path('kenney_sokoban/map5.tmx')
