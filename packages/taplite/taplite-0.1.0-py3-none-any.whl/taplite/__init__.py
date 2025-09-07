7fd0792cbb5c057c76b4704ac9175be9252f1289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 05:06:26 +0000

    Add package taplite/0.1.0/taplite-0.1.0-py3-none-any.whl

---
+++ packages/taplite/taplite-0.1.0-py3-none-any.whl/taplite/__init__.py
@@ -0,0 +1,8 @@
+from .taplite import *
+
+
+__version__ = '0.1.0'
+
+
+# print out the current version
+print(f'taplite, version {__version__}\n')
---
+++ packages/taplite/taplite-0.1.0-py3-none-any.whl/taplite/taplite.py
@@ -0,0 +1,38 @@
+import ctypes
+import os
+import platform
+from multiprocessing import Process
+
+
+__all__ = ['assignment', 'simulation']
+
+
+_os = platform.system()
+if _os == 'Darwin':
+    if platform.machine().startswith('x86_64'):
+        lib_name = 'libtaplite_x64.dylib'
+    else:
+        lib_name = 'libtaplite_arm.dylib'
+elif _os == 'Windows':
+    lib_name = 'taplite.dll'
+elif _os == 'Linux':
+    lib_name = 'libtaplite.so'
+else:
+    raise OSError('Unsupported operating system')
+
+
+_lib = None
+try:
+    _lib = ctypes.CDLL(os.path.join(os.path.dirname(__file__), lib_name))
+except OSError:
+    print('failed to load TAPLite library.')
+
+
+def assignment():
+    proc_assignment = Process(target=_lib.DTA_AssignmentAPI())
+    proc_assignment.start()
+    proc_assignment.join()
+
+
+def simulation():
+    _lib.DTA_SimulationAPI()
