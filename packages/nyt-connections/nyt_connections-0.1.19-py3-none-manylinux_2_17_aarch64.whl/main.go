d5324eb8cd52432f23154f246b3cda3944613ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 14:07:25 +0000

    Add package nyt-connections/0.1.19/nyt_connections-0.1.19-py3-none-manylinux_2_17_aarch64.whl

---
+++ packages/nyt-connections/nyt_connections-0.1.19-py3-none-manylinux_2_17_aarch64.whl/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/jmelahman/connections/game"
+
+	"github.com/rivo/tview"
+)
+
+func main() {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	defer screen.Fini()
+
+	app := tview.NewApplication()
+	if err := game.Run(app, screen); err != nil {
+		panic(err)
+	}
+}
