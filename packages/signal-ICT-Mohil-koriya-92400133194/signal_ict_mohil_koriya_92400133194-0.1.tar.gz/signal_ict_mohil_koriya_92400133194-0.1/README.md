a512fafd2691fb58d12ed1bd2feea36cfb7879b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:19:51 +0000

    Add package signal-ICT-Mohil-koriya-92400133194/0.1/signal_ict_mohil_koriya_92400133194-0.1.tar.gz

---
+++ packages/signal-ICT-Mohil-koriya-92400133194/signal_ict_mohil_koriya_92400133194-0.1.tar.gz/signal_ict_mohil_koriya_92400133194-0.1/README.md
@@ -0,0 +1,2 @@
+```bash
+pip install signal_ICT_Mohil_koriya_92400133194
---
+++ packages/signal-ICT-Mohil-koriya-92400133194/signal_ict_mohil_koriya_92400133194-0.1.tar.gz/signal_ict_mohil_koriya_92400133194-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='signal_ICT_Mohil_koriya_92400133194',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+    ],
+    author='Mohil Koriya',
+    description='Python package for signal generation and operations',
+)
