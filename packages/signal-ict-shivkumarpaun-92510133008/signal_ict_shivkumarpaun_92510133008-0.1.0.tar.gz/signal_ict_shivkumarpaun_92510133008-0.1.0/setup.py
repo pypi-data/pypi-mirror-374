36795b31a314cafd1548f063949a96342ba3de66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:20:17 +0000

    Add package signal-ict-shivkumarpaun-92510133008/0.1.0/signal_ict_shivkumarpaun_92510133008-0.1.0.tar.gz

---
+++ packages/signal-ict-shivkumarpaun-92510133008/signal_ict_shivkumarpaun_92510133008-0.1.0.tar.gz/signal_ict_shivkumarpaun_92510133008-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="signal_ICT_Shivkumarpaun_92510133008",   # <-- replace this
+    version="0.1.0",
+    description="Basic signals and operations package for Signals & Systems demo",
+    author="Shivkumar Paun",   # <-- replace this
+    author_email="shivkumarpaun@example.com",
+    packages=find_packages(),
+    install_requires=["numpy", "matplotlib"],
+    python_requires=">=3.8",
+)
