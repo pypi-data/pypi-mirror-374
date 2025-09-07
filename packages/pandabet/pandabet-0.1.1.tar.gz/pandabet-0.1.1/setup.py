cee449d7642e4585d480d38b16e16b4ef11e1972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 15:06:12 +0000

    Add package pandabet/0.1.1/pandabet-0.1.1.tar.gz

---
+++ packages/pandabet/pandabet-0.1.1.tar.gz/pandabet-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="pandabet",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=1.0.0",
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
