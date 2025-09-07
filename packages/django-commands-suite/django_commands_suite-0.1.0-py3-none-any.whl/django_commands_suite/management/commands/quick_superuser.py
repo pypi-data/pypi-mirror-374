8ae8ec599d45c63b8a9aa6ee9988ed1314af4f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 03:19:54 +0000

    Add package django-commands-suite/0.1.0/django_commands_suite-0.1.0-py3-none-any.whl

---
+++ packages/django-commands-suite/django_commands_suite-0.1.0-py3-none-any.whl/django_commands_suite/management/commands/quick_superuser.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+   help = "Quickly create a default superuser (username=djadmin, password=djadmin, email=admin@dj.com)"
+
+   def handle(self, *args, **options):
+      User = get_user_model()
+      if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+               username="djadmin",
+               email="admin@dj.com",
+               password="djadmin"
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser 'djadmin' created successfully!"))
+      else:
+            self.stdout.write(self.style.WARNING("Superuser 'admin' already exists."))
