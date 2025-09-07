f3f0a65f60288c38245c590cbc7c1637e2797fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-07 04:05:58 +0000

    Add package starch/0.1.0a3/starch-0.1.0a3-py3-none-any.whl

---
+++ packages/starch/starch-0.1.0a3-py3-none-any.whl/starch/__init__.py
@@ -0,0 +1,55 @@
+"""
+Starch
+======
+
+A configurable line-formatter for visually distinguishing certain comments.
+
+"""
+from typing import Final
+
+from . import constants, formatter
+from .config import config
+from .constants import (
+    STARCH_CACHE_PATH,
+    STARCH_CONFIG_PATH,
+    STARCH_DATA_PATH,
+    STARCH_LOG_PATH,
+    STARCH_LOG_FILEPATH
+)
+from .formatter import CommentFormatter
+
+# ─── re-exporting metadata ──────────────────────────────────────────────────────── ✦ ─
+# __author__ = constants.__author__
+# __description__ = constants.__description__
+# __email__ = constants.__email__
+# __version__ = constants.__version__
+
+
+__all__ = [
+    # ─── metadata ─────────────────────────────────────────────────────────────────────
+    #
+    # "__author__",
+    # "__email__",
+    # "__license__",
+    # "__version__",
+    # "__package__",
+
+    # ─── constants ────────────────────────────────────────────────────────────────────
+    #
+    "STARCH_CACHE_PATH",
+    "STARCH_CONFIG_PATH",
+    "STARCH_DATA_PATH",
+    "STARCH_LOG_PATH",
+    "STARCH_LOG_FILEPATH",
+
+    # ─── modules ──────────────────────────────────────────────────────────────────────
+    #
+    "config",
+    "constants",
+    "formatter",
+    
+    # ─── classes ──────────────────────────────────────────────────────────────────────
+    #
+    "CommentFormatter",
+    "config"
+]
