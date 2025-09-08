ad81debac7ecc90dd94d3be05d835c6265b2a1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:05:35 +0000

    Add package neverlib/0.2.7/neverlib-0.2.7-py3-none-any.whl

---
+++ packages/neverlib/neverlib-0.2.7-py3-none-any.whl/neverlib/tests/test_imports.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: 凌逆战 | Never
+Date: 2025-09-07
+Description: 测试neverlib导入功能
+'''
+import sys
+import os
+import time
+
+# 确保当前目录在Python路径中，以便导入neverlib
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append("../..")
+print("开始测试neverlib导入功能...")
+
+from neverlib.audio_aug import limiter
