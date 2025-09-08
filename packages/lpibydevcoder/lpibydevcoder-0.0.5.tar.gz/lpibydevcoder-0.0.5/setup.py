4d2c1689179cd3f47b80a5de99d0e10cbff7eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 06:06:13 +0000

    Add package lpibydevcoder/0.0.5/lpibydevcoder-0.0.5.tar.gz

---
+++ packages/lpibydevcoder/lpibydevcoder-0.0.5.tar.gz/lpibydevcoder-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="lpibydevcoder",
+    version="0.0.5",
+    packages=find_packages(),
+    author="DevCooder",
+    author_email="zerowanlord@gmail.com",
+    description="My simple and easy programming language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZeroMurder/My-Programming-Language",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
+
+
+
