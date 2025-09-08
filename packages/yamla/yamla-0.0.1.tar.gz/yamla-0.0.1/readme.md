58505285394986e33c120ac4f235d22144392176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 06:06:06 +0000

    Add package yamla/0.0.1/yamla-0.0.1.tar.gz

---
+++ packages/yamla/yamla-0.0.1.tar.gz/yamla-0.0.1/readme.md
@@ -0,0 +1,8 @@
+<!--
+- SPDX-License-Identifier: Apache-2.0
+- Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+-->
+
+# yamla
+
+Yamla is a yaml parser and emitter for Python.
---
+++ packages/yamla/yamla-0.0.1.tar.gz/yamla-0.0.1/test/bin/unit/test_true.py
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 Jayesh Badwaik <j.badwaik@fz-juelich.de>
+# ------------------------------------------------------------------------------
+
+def test_true():
+    assert True
