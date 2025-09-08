4ece8f514612aa995bdbbb1871670ddcc91d5994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 06:05:59 +0000

    Add package pdfalyzer/1.16.12/pdfalyzer-1.16.12.tar.gz

---
+++ packages/pdfalyzer/pdfalyzer-1.16.12.tar.gz/pdfalyzer-1.16.12/pdfalyzer/detection/yaralyzer_helper.py
@@ -0,0 +1,53 @@
+"""
+Class to help with the pre-configured YARA rules in the /yara directory.
+"""
+from importlib.resources import as_file, files
+from sys import exit
+from typing import Optional, Union
+
+from yaralyzer.config import YaralyzerConfig
+from yaralyzer.yaralyzer import Yaralyzer
+
+YARA_RULES_DIR = files('pdfalyzer').joinpath('yara_rules')
+
+YARA_RULES_FILES = [
+    'didier_stevens.yara',
+    'lprat.static_file_analysis.yara',
+    'PDF.yara',
+    'PDF_binary_stream.yara',
+    'pdf_malware.yara',
+]
+
+
+def get_file_yaralyzer(file_path_to_scan: str) -> Yaralyzer:
+    """Get a yaralyzer for a file path"""
+    return _build_yaralyzer(file_path_to_scan)
+
+
+def get_bytes_yaralyzer(scannable: bytes, label: str) -> Yaralyzer:
+    return _build_yaralyzer(scannable, label)
+
+
+def _build_yaralyzer(scannable: Union[bytes, str], label: Optional[str] = None) -> Yaralyzer:
+    """Build a yaralyzer for .yara rules files stored in the yara_rules/ dir in this package."""
+    # TODO: ugh this sucks (handling to extract .yara files from a python pkg zip)
+    with as_file(YARA_RULES_DIR.joinpath(YARA_RULES_FILES[0])) as yara0:
+        with as_file(YARA_RULES_DIR.joinpath(YARA_RULES_FILES[1])) as yara1:
+            with as_file(YARA_RULES_DIR.joinpath(YARA_RULES_FILES[2])) as yara2:
+                with as_file(YARA_RULES_DIR.joinpath(YARA_RULES_FILES[3])) as yara3:
+                    with as_file(YARA_RULES_DIR.joinpath(YARA_RULES_FILES[4])) as yara4:
+                        # If there is a custom yara_rules arg, use that instead of the files in the yara_rules/ dir
+                        rules_paths = YaralyzerConfig.args.yara_rules_files or []
+
+                        if not YaralyzerConfig.args.no_default_yara_rules:
+                            rules_paths += [str(y) for y in [yara0, yara1, yara2, yara3, yara4]]
+
+                        try:
+                            return Yaralyzer.for_rules_files(rules_paths, scannable, label)
+                        except ValueError as e:
+                            # TODO: use YARA_FILE_DOES_NOT_EXIST_ERROR_MSG variable
+                            if "it doesn't exist" in str(e):
+                                print(str(e))
+                                exit(1)
+                            else:
+                                raise e
---
+++ packages/pdfalyzer/pdfalyzer-1.16.12.tar.gz/pdfalyzer-1.16.12/pdfalyzer/output/layout.py
@@ -0,0 +1,73 @@
+"""
+Methods to help with the formatting of the output tables, headers, panels, etc.
+"""
+from rich import box
+from rich.padding import Padding
+from rich.panel import Panel
+from rich.table import Table
+from yaralyzer.output.rich_console import console, console_width
+
+DEFAULT_SUBTABLE_COL_STYLES = ['white', 'bright_white']
+HEADER_PADDING = (1, 1)
+
+
+def generate_subtable(cols, header_style='subtable') -> Table:
+    """Suited for placement in larger tables."""
+    table = Table(
+        box=box.SIMPLE,
+        show_edge=False,
+        collapse_padding=True,
+        padding=(0, 0, 0, 0),
+        header_style=header_style,
+        show_lines=False,
+        border_style='grey.dark',
+        expand=True)
+
+    for i, col in enumerate(cols):
+        if i == 0:
+            table.add_column(col, style=DEFAULT_SUBTABLE_COL_STYLES[0], justify='left')
+        else:
+            table.add_column(col, style='bright_white', justify='right')
+
+    return table
+
+
+def subheading_width() -> int:
+    return int(console_width() * 0.75)
+
+
+def half_width() -> int:
+    return int(console_width() * 0.5)
+
+
+def pad_header(header: str) -> Padding:
+    """Would pad anything, not just headers"""
+    return Padding(header, HEADER_PADDING)
+
+
+def print_section_header(headline: str, style: str = '') -> None:
+    console.line(2)
+    _print_header_panel(headline, f"{style} reverse", True, console_width(), HEADER_PADDING)
+    console.line()
+
+
+def print_section_subheader(headline: str, style: str = '') -> None:
+    console.line()
+    _print_header_panel(headline, style, True, subheading_width(), HEADER_PADDING)
+
+
+def print_section_sub_subheader(headline: str, style: str = ''):
+    console.line()
+    _print_header_panel(headline, style, True, half_width())
+
+
+def print_headline_panel(headline, style: str = ''):
+    _print_header_panel(headline, style, False, console_width())
+
+
+def print_fatal_error_panel(headline):
+    print_headline_panel(headline, style='red blink')
+
+
+def _print_header_panel(headline: str, style: str, expand: bool, width: int, padding: tuple = (0,)) -> None:
+    console.print(Panel(headline, style=style, expand=expand, width=width or subheading_width(), padding=padding))
