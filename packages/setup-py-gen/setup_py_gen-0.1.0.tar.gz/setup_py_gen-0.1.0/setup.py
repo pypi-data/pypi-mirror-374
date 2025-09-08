1c9fb21aaac8e6dd1a8d0bfa21c2d9c479360d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:23:01 +0000

    Add package setup-py-gen/0.1.0/setup_py_gen-0.1.0.tar.gz

---
+++ packages/setup-py-gen/setup_py_gen-0.1.0.tar.gz/setup_py_gen-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='setup_py_gen',
+    version='0.1.0',
+    description='A package generated from provided source code and metadata.',
+    long_description='',
+    author='Eugene Evstafev',
+    author_email='hi@eugene.plus',
+    url='https://github.com/chigwell/setup_py_gen',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
