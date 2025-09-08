9a4f2f6082f03e9e17ca234d1040fcad64078956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 06:06:05 +0000

    Add package mathbyaarushlohit/0.7/mathbyaarushlohit-0.7.tar.gz

---
+++ packages/mathbyaarushlohit/mathbyaarushlohit-0.7.tar.gz/mathbyaarushlohit-0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mathbyaarushlohit",
+    version="0.7",
+    packages=find_packages(),
+    install_requires=[
+        "numpy"
+    ],
+    author="Lohit (A.K.A AarushLohit)",
+    description="A small package to help biology-background students and coding slow learners with this math module Contains all Math Practical program.",
+)
