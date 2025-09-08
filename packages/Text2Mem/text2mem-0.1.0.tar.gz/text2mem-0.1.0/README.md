489a817cca04bf85d04dc4ddb7c0c2210c70c64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:30 +0000

    Add package Text2Mem/0.1.0/text2mem-0.1.0.tar.gz

---
+++ packages/Text2Mem/text2mem-0.1.0.tar.gz/text2mem-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Text2Mem
+
+This is a placeholder package to reserve the name on PyPI.
---
+++ packages/Text2Mem/text2mem-0.1.0.tar.gz/text2mem-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Text2Mem",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A placeholder package for Text2Mem",
+    long_description="This is a placeholder package to reserve the name Text2Mem on PyPI.",
+    long_description_content_type="text/plain",
+    url="https://github.com/yourusername/Text2Mem",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
