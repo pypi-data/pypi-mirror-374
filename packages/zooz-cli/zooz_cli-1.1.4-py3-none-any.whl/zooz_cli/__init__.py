8e61abd2a0d62ce6551835e483e403305e1c0694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:12 +0000

    Add package zooz-cli/1.1.4/zooz_cli-1.1.4-py3-none-any.whl

---
+++ packages/zooz-cli/zooz_cli-1.1.4-py3-none-any.whl/zooz_cli/__init__.py
@@ -0,0 +1,10 @@
+"""
+Zooz CLI - Transfer Microsoft Power Pages sites between environments
+
+A command-line tool that simplifies the process of transferring Power Pages 
+sites between different Microsoft environments using the Power Platform CLI.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Zooz Tech"
+__email__ = "itzharm@zooz-tech.co.il"
