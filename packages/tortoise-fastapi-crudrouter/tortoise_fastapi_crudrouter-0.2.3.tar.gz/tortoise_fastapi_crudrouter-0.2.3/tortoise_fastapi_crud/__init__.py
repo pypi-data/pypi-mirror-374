49d3a523acca878442ce360aa158b042b185e268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:16 +0000

    Add package tortoise-fastapi-crudrouter/0.2.3/tortoise_fastapi_crudrouter-0.2.3.tar.gz

---
+++ packages/tortoise-fastapi-crudrouter/tortoise_fastapi_crudrouter-0.2.3.tar.gz/tortoise_fastapi_crudrouter-0.2.3/tortoise_fastapi_crud/__init__.py
@@ -0,0 +1,9 @@
+from .core import TortoiseCRUDRouter, SchemaConfig, RouteConfig
+
+from ._version import __version__
+
+__all__ = [
+    "SchemaConfig",
+    "RouteConfig",
+    "TortoiseCRUDRouter"
+]
