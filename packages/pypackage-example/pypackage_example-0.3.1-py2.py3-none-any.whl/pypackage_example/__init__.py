463cb1431610395b46cdc5c4ccd72056443f8340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:05:27 +0000

    Add package pypackage-example/0.3.1/pypackage_example-0.3.1-py2.py3-none-any.whl

---
+++ packages/pypackage-example/pypackage_example-0.3.1-py2.py3-none-any.whl/pypackage_example/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for pypackage_example."""
+
+__author__ = """Lorenzo Crecco"""
+__email__ = "lorenzo.crecco@hotmail.com"
+__version__ = "0.3.1"
+
+from .pypackage_example import *
