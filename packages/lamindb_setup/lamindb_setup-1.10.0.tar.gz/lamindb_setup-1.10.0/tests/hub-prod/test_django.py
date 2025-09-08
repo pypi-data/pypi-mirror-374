7e2c411e5f2448d1c7673c77a6d37611772e49fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:06:29 +0000

    Add package lamindb_setup/1.10.0/lamindb_setup-1.10.0.tar.gz

---
+++ packages/lamindb_setup/lamindb_setup-1.10.0.tar.gz/lamindb_setup-1.10.0/tests/hub-prod/test_django.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from lamindb_setup import django
+
+# test make migrations covered in test_migrate_create
+
+
+def test_django():
+    django("sqlsequencereset", "lamindb")
