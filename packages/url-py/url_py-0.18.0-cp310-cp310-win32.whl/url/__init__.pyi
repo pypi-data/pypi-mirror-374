0985ef6970e0c24d16bf4b0f0c65ee8d0e555f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:06:15 +0000

    Add package url-py/0.18.0/url_py-0.18.0-cp310-cp310-win32.whl

---
+++ packages/url-py/url_py-0.18.0-cp310-cp310-win32.whl/url/__init__.pyi
@@ -0,0 +1,33 @@
+from collections.abc import Iterable
+from typing import Self
+
+class URLError(Exception): ...
+class RelativeURLWithoutBase(Exception): ...
+class InvalidIPv6Address(Exception): ...
+
+class URL:
+    cannot_be_a_base: bool
+    host: Domain
+    host_str: str
+    password: str
+    path: str
+    path_segments: Iterable[str]
+    scheme: str
+    username: str
+    fragment: str | None
+
+    def __truediv__(self, other: str) -> Self: ...
+    @classmethod
+    def parse(cls, input: str) -> Self: ...
+    @classmethod
+    def parse_with_params(
+        cls,
+        input: str,
+        params: Iterable[tuple[str, str]],
+    ) -> Self: ...
+    def join(self, other: str) -> Self: ...
+    def make_relative(self, other: Self) -> str: ...
+    def with_fragment(self, fragment: str | None) -> Self: ...
+
+class Domain:
+    def __init__(self, value: str): ...
