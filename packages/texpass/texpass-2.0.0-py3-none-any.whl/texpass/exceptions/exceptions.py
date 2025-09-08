506b41173792e913ff60aa80bb973bb30d7b1720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 04:06:21 +0000

    Add package texpass/2.0.0/texpass-2.0.0-py3-none-any.whl

---
+++ packages/texpass/texpass-2.0.0-py3-none-any.whl/texpass/exceptions/exceptions.py
@@ -0,0 +1,11 @@
+class EntryAlreadyExists(Exception):
+    pass
+
+class UsernameAlreadyExists(Exception):
+    pass
+
+class UsernameDoesNotExist(Exception):
+    pass
+
+class WrongPassword(Exception):
+    pass
