f31b6c301910252b1453c0bac083d34e89abbf57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:06:20 +0000

    Add package text2mem-utils/0.1.0/text2mem_utils-0.1.0.tar.gz

---
+++ packages/text2mem-utils/text2mem_utils-0.1.0.tar.gz/text2mem_utils-0.1.0/README.md
@@ -0,0 +1,5 @@
+# text2mem-utils
+
+Utility functions for text2mem
+
+This is a placeholder package to reserve the name on PyPI.
---
+++ packages/text2mem-utils/text2mem_utils-0.1.0.tar.gz/text2mem_utils-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text2mem-utils",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Utility functions for text2mem",
+    long_description="This is a placeholder package for text2mem-utils",
+    long_description_content_type="text/plain",
+    url="https://github.com/yourusername/text2mem-utils",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
