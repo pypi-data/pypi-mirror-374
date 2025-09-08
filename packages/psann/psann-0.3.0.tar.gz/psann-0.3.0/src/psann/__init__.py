c28b00f6b5df3c7fe3d8c68dd522fb49a671ffd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:55 +0000

    Add package psann/0.3.0/psann-0.3.0.tar.gz

---
+++ packages/psann/psann-0.3.0.tar.gz/psann-0.3.0/src/psann/__init__.py
@@ -0,0 +1,12 @@
+"""PSANN: Parameterized Sine-Activated Neural Networks.
+
+Sklearn-style API with PyTorch backend.
+"""
+
+from .sklearn import PSANNRegressor
+
+__all__ = [
+    "PSANNRegressor",
+]
+
+__version__ = "0.2.0"
