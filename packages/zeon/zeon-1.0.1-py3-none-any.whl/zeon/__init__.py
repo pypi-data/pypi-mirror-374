40fa987b3eb0edad3346381aa93fd2946a3fb258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:05:38 +0000

    Add package zeon/1.0.1/zeon-1.0.1-py3-none-any.whl

---
+++ packages/zeon/zeon-1.0.1-py3-none-any.whl/zeon/__init__.py
@@ -0,0 +1,11 @@
+"""
+Zeon - A powerful CLI tool to scaffold and manage FastAPI projects
+"""
+
+__version__ = "1.0.1"
+__author__ = "Vinyas Bharadwaj"
+__email__ = "vinyasbharadwaj101@gmail.com"
+
+from .main import app
+
+__all__ = ["app"]
