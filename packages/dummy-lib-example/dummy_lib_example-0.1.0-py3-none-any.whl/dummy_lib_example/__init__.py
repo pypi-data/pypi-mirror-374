39e096c480f61872dfbc8783f3b2e1bcab3af314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:42 +0000

    Add package dummy-lib-example/0.1.0/dummy_lib_example-0.1.0-py3-none-any.whl

---
+++ packages/dummy-lib-example/dummy_lib_example-0.1.0-py3-none-any.whl/dummy_lib_example/__init__.py
@@ -0,0 +1,3 @@
+from .core import say_hello
+
+__all__ = ["say_hello"]
---
+++ packages/dummy-lib-example/dummy_lib_example-0.1.0-py3-none-any.whl/dummy_lib_example/core.py
@@ -0,0 +1,6 @@
+
+def say_hello(name: str = "world") -> str:
+    """
+    Returns a friendly greeting.
+    """
+    return f"Hello, {name}! This is your dummy library talking."
