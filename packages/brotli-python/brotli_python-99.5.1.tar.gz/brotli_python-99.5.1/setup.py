68faf455984f5ce775b8de054076c99646194771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:06:22 +0000

    Add package brotli-python/99.5.1/brotli_python-99.5.1.tar.gz

---
+++ packages/brotli-python/brotli_python-99.5.1.tar.gz/brotli_python-99.5.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="brotli-python",
+    version="99.5.1",
+    packages=find_packages(),
+    description="POC package (harmless beacon-only)",
+)
