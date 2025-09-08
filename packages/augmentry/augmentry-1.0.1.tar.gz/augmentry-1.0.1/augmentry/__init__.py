10a4b1e613e48fb270ea9ef9bb4bd1be6ecd21ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 04:05:35 +0000

    Add package augmentry/1.0.1/augmentry-1.0.1.tar.gz

---
+++ packages/augmentry/augmentry-1.0.1.tar.gz/augmentry-1.0.1/augmentry/__init__.py
@@ -0,0 +1,10 @@
+"""
+Augmentry Python SDK
+Official Python client for the Augmentry API
+"""
+
+from .client import AugmentryClient
+from .exceptions import AugmentryError, AuthenticationError, RateLimitError
+
+__version__ = "1.0.1"
+__all__ = ["AugmentryClient", "AugmentryError", "AuthenticationError", "RateLimitError"]
