452bd9627952cd9902bbdfc4b351c0e593404116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 05:06:24 +0000

    Add package bombhtml/0.4.5/bombhtml-0.4.5-py3-none-any.whl

---
+++ packages/bombhtml/bombhtml-0.4.5-py3-none-any.whl/bombhtml/__init__.py
@@ -0,0 +1,9 @@
+"""
+ Copyright (C) 2025  sophie (itsme@itssophi.ee)
+
+    Double licensed under MIT and GPLv3. See LICENSE_* for more info.
+"""
+
+from .bomb import Build
+
+__version__ = "0.4.5"
