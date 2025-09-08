2d6d2f24dd79652a59069233ec7d378d820c8272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 04:05:43 +0000

    Add package caskada/0.0.1/caskada-0.0.1.tar.gz

---
+++ packages/caskada/caskada-0.0.1.tar.gz/caskada-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="caskada",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Initial placeholder for the caskada package.",
+    author="zvictor",
+)
