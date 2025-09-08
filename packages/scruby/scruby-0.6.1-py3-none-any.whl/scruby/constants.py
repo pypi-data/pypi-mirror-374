15fb8118bc239873023153139e979bc10abf6218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-08 03:22:47 +0000

    Add package scruby/0.6.1/scruby-0.6.1-py3-none-any.whl

---
+++ packages/scruby/scruby-0.6.1-py3-none-any.whl/scruby/constants.py
@@ -0,0 +1,11 @@
+"""Constant variables.
+
+The module contains the following variables:
+
+- `DB_ROOT` - Path to root directory of database. By default = "ScrubyDB" (in root of project).
+"""
+
+from __future__ import annotations
+
+# Path to root directory of database. By default = "ScrubyDB" (in root of project).
+DB_ROOT: str = "ScrubyDB"
